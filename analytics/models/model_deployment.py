3944b219d1bc9e3a8d72a49e625244e3490c83f7
KOSASIH <kosasihg88@gmail.com>
2024-05-09 21:26:10 +0700

    Create model_deployment.py

---
+++ analytics/models/model_deployment.py
@@ -0,0 +1,8 @@
+class ModelDeployment:
+    def __init__(self, model, deployment_config):
+        self.model = model
+        self.deployment_config = deployment_config
+
+    def deploy(self):
+        # Code to deploy the model
+        pass
