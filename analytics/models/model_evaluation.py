8bf690175f46940325f73b9eef8fc2f102c61778
KOSASIH <kosasihg88@gmail.com>
2024-05-09 21:25:29 +0700

    Create model_evaluation.py

---
+++ analytics/models/model_evaluation.py
@@ -0,0 +1,8 @@
+class ModelEvaluation:
+    def __init__(self, model, evaluation_data):
+        self.model = model
+        self.evaluation_data = evaluation_data
+
+    def evaluate(self):
+        # Code to evaluate the model
+        pass
