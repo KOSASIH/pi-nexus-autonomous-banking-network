cf3551aab8dc621bb3750104ab3c4b3eb5a3672a
KOSASIH <kosasihg88@gmail.com>
2024-05-09 21:01:34 +0700

    Create __init__.py

---
+++ analytics/__init__.py
@@ -0,0 +1,6 @@
+from .data_ingestion import DataIngestion
+from .data_processing import DataProcessing
+from .data_analysis import DataAnalysis
+from .data_visualization import DataVisualization
+
+analytics = Analytics()
