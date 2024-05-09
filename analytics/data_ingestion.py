d4b380a836b6aa79b735707416480a0e7b570a23
KOSASIH <kosasihg88@gmail.com>
2024-05-09 21:02:30 +0700

    Create data_ingestion.py

---
+++ analytics/data_ingestion.py
@@ -0,0 +1,7 @@
+class DataIngestion:
+    def __init__(self, data_source):
+        self.data_source = data_source
+
+    def ingest(self):
+        # Code to ingest data from the data source
+        pass
