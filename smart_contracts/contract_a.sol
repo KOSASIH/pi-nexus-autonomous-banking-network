d48d7020243b754d01c5b5bcd22fb1843cee1d42
KOSASIH <kosasihg88@gmail.com>
2024-05-09 21:38:29 +0700

    Create contract_a.sol

---
+++ smart_contracts/contract_a.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+contract ContractA {
+    uint256 public storedData;
+
+    constructor() {
+        storedData = 0;
+    }
+
+    function set(uint256 data) public {
+        storedData = data;
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
