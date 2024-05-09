41d06527b6162f4f8283faef3ff1ace6edaa8a86
KOSASIH <kosasihg88@gmail.com>
2024-05-09 21:24:17 +0700

    Create model_training.py

---
+++ analytics/models/model_training.py
@@ -0,0 +1,8 @@
+class ModelTraining:
+    def __init__(self, model_name, training_data):
+        self.model_name = model_name
+        self.training_data = training_data
+
+    def train(self):
+        # Code to train the model
+        pass
