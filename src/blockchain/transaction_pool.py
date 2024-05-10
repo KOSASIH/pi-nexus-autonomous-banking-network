c819fbb78e07cd68e8ad25abbfd23b2fff31fdb8
KOSASIH <kosasihg88@gmail.com>
2024-05-10 10:26:07 +0700

    Create transaction_pool.py

---
+++ src/blockchain/transaction_pool.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+class TransactionPool:
+    def __init__(self):
+        self.transactions = []
+
+    def add_transaction(self, transaction: Transaction):
+        self.transactions.append(transaction)
+
+    def remove_transaction(self, transaction: Transaction):
+        self.transactions.remove(transaction)
+
+    def get_transactions(self) -> list[Transaction]:
+        return self.transactions
+
+    def validate_transaction(self, transaction: Transaction) -> bool:
+        # Validate the transaction signature
+        # This is a placeholder for the actual validation process
+        return True
+
+    def broadcast_transaction(self, transaction: Transaction):
+        # Broadcast the transaction to other nodes in the network
+        # This is a placeholder for the actual broadcasting process
+        pass
