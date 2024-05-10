c5cbe89d22fa5f1a7a103935699ac776d1063958
KOSASIH <kosasihg88@gmail.com>
2024-05-10 20:10:51 +0700

    Create block.py

---
+++ blockchain/block.py
@@ -0,0 +1,24 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, previous_hash, timestamp, data, hash):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.hash = hash
+
+    @staticmethod
+    def calculate_hash(index, previous_hash, timestamp, data):
+        value = str(index) + str(previous_hash) + str(timestamp) + str(data)
+        return hashlib.sha256(value.encode('utf-8')).hexdigest()
+
+    def __str__(self):
+        return 'Block: {index}, Previous Hash: {previous_hash}, Timestamp: {timestamp}, Data: {data}, Hash: {hash}'.format(
+            index=self.index,
+            previous_hash=self.previous_hash,
+            timestamp=self.timestamp,
+            data=self.data,
+            hash=self.hash
+        )
