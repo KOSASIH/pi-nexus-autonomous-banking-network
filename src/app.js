faa9ce641dee0fc18ee1ba86bc609c017873d950
KOSASIH <kosasihg88@gmail.com>
2024-05-10 14:42:37 +0700

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const { JWT_SECRET, MONGODB_URI } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const router = require("./routes");
+app.use("/api", router);
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+    process.exit(1);
+  }
+};
+
+connectToMongoDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,
