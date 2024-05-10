af2c73ce7c15667cd17109e90366f42cec11e1e2
KOSASIH <kosasihg88@gmail.com>
2024-05-10 19:58:47 +0700

    Create logging.py

---
+++ logging/logging.py
@@ -0,0 +1,16 @@
+import logging
+
+class Logging:
+    def __init__(self):
+        self.logger = logging.getLogger('banking_network')
+        self.logger.setLevel(logging.DEBUG)
+
+    def setup_logging(self):
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        console_handler.setFormatter(formatter)
+        self.logger.addHandler(console_handler)
+
+    def log(self, message):
+        self.logger.info(message)
