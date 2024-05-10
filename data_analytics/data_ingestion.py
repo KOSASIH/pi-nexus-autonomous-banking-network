607fe542de8262fa7a7ed813e31e0b82c04482d9
KOSASIH <kosasihg88@gmail.com>
2024-05-10 19:40:20 +0700

    Create data_ingestion.py

---
+++ data_analytics/data_ingestion.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+class DataIngestion:
+    def __init__(self, data_source):
+        self.data_source = data_source
+
+    def ingest_data(self):
+        data = pd.read_csv(self.data_source)
+        return data
