6d1b09613ea6ef1659b37bbede7a941fe991a9b9
KOSASIH <kosasihg88@gmail.com>
2024-05-10 10:42:17 +0700

    Create test_block.py

---
+++ src/tests/unit_tests/test_block.py
@@ -0,0 +1,25 @@
+import unittest
+from block import Block
+
+class TestBlock(unittest.TestCase):
+    def test_create_block(self):
+        block = Block(1, '0' * 64, 'test data', 100)
+        self.assertEqual(block.index, 1)
+        self.assertEqual(block.previous_hash, '0' * 64)
+        self.assertEqual(block.data, 'test data')
+        self.assertEqual(block.hash, '0' * 64)
+        self.assertEqual(block.nonce, 100)
+
+    def test_hash(self):
+        block = Block(1, '0' * 64, 'test data', 100)
+        block.hash = None
+        block.mine_hash()
+        self.assertNotEqual(block.hash, '0' * 64)
+
+    def test_mine_hash(self):
+        block = Block(1, '0' * 64, 'test data', 100)
+        block.mine_hash(difficulty=2)
+        self.assertEqual(block.hash[0:2], '00')
+
+if __name__ == '__main__':
+    unittest.main()
