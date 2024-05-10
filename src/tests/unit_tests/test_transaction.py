8013700d776c2f62838df302b2d0bcd492eb5355
KOSASIH <kosasihg88@gmail.com>
2024-05-10 14:16:13 +0700

    Create test_transaction.py

---
+++ src/tests/unit_tests/test_transaction.py
@@ -0,0 +1,24 @@
+import unittest
+from transaction import Transaction
+
+class TestTransaction(unittest.TestCase):
+    def test_create_transaction(self):
+        transaction = Transaction('sender', 'receiver', 100)
+        self.assertEqual(transaction.sender, 'sender')
+        self.assertEqual(transaction.receiver, 'receiver')
+        self.assertEqual(transaction.amount, 100)
+        self.assertEqual(transaction.hash, None)
+
+    def test_hash(self):
+        transaction = Transaction('sender', 'receiver', 100)
+        transaction.hash = None
+        transaction.mine_hash()
+        self.assertNotEqual(transaction.hash, None)
+
+    def test_mine_hash(self):
+        transaction = Transaction('sender', 'receiver', 100)
+        transaction.mine_hash(difficulty=2)
+        self.assertEqual(transaction.hash[0:2], '00')
+
+if __name__ == '__main__':
+    unittest.main()
