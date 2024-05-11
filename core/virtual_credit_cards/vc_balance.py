f1474c97abdf5efd4fd7f17a03c712b71b1fa920
KOSASIH <kosasihg88@gmail.com>
2024-05-11 18:20:08 +0700

    Create vc_balance.py

---
+++ core/virtual_credit_cards/vc_balance.py
@@ -0,0 +1,12 @@
+def get_vc_balance(vc_number):
+  # Get the balance of a virtual credit card
+  # ...
+  return 1000
+
+def set_vc_balance(vc_number, balance):
+  # Set the balance of a virtual credit card
+  # ...
+
+def add_vc_transaction(vc_number, amount, merchant):
+  # Add a transaction to the virtual credit card transaction history
+  # ...
