360912124ed02d8d9c5657acbcda07996c46b8c4
KOSASIH <kosasihg88@gmail.com>
2024-05-11 18:29:24 +0700

    Create translator.py

---
+++ translations/translator.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+class Translator:
+    def __init__(self, language):
+        # Load the translation data for the specified language
+        self.language = language
+        translations_file = os.path.join(os.path.dirname(__file__), 'translations', f'{language}.json')
+        with open(translations_file, 'r') as f:
+            self.translations = json.load(f)
+
+    def translate(self, key):
+        # Translate a given key into the specified language
+        return self.translations.get(key, key)
