9469e8c63e5529dee43debca623c79b3400779ea
KOSASIH <kosasihg88@gmail.com>
2024-05-11 18:43:44 +0700

    Create network_topology.py

---
+++ network/network_management/network_topology.py
@@ -0,0 +1,15 @@
+class NetworkTopology:
+    def __init__(self, network_management):
+        self.network_management = network_management
+
+    def generate_topology_map(self):
+        # generate network topology map
+        pass
+
+    def update_topology_map(self):
+        # update network topology map
+        pass
+
+    def optimize_topology(self):
+        # optimize network topology
+        pass
