392eacd9261e8cc5103c2fec3f25c87452dcc17c
KOSASIH <kosasihg88@gmail.com>
2024-05-11 18:20:41 +0700

    Create vc_limits.py

---
+++ core/virtual_credit_cards/vc_limits.py
@@ -0,0 +1,8 @@
+def set_vc_limit(vc_number, limit):
+  # Set the credit limit of a virtual credit card
+  # ...
+
+def get_vc_limit(vc_number):
+  # Get the credit limit of a virtual credit card
+  # ...
+  return 1000
