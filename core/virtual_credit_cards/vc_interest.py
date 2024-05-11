e9eac72316ac4b68dd9db8a2eb18e8a6f622140a
KOSASIH <kosasihg88@gmail.com>
2024-05-11 18:21:25 +0700

    Create vc_interest.py

---
+++ core/virtual_credit_cards/vc_interest.py
@@ -0,0 +1,8 @@
+def apply_vc_interest(vc_number):
+  # Apply the interest rate to a virtual credit card
+  # ...
+
+def get_vc_interest_rate(vc_number):
+  # Get the interest rate of a virtual credit card
+  # ...
+  return 0.1
