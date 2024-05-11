f607b8a7b451cfd4b188d269e1c07ac9b54125df
KOSASIH <kosasihg88@gmail.com>
2024-05-11 16:46:14 +0700

    Create routes.py

---
+++ api/routes.py
@@ -0,0 +1,9 @@
+# Use a RESTful API framework like Flask or FastAPI
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+@app.post("/transactions")
+async def create_transaction(transaction: Transaction):
+    """Create a new transaction"""
+    #...
