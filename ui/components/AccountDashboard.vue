c584489f9a56d92fcd10ce10d1fa64d25bd5cf74
KOSASIH <kosasihg88@gmail.com>
2024-05-11 16:48:27 +0700

    Create AccountDashboard.vue

---
+++ ui/components/AccountDashboard.vue
@@ -0,0 +1,21 @@
+<!-- Use a modern frontend framework like Vue.js or React -->
+<template>
+  <div>
+    <h1>Account Dashboard</h1>
+    <!-- Display account information and transactions -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      account: null,
+      transactions: []
+    }
+  },
+  async mounted() {
+    // Fetch account information and transactions
+  }
+}
+</script>
