07cf325a0fc197f9b439a9078f694c01e2692d25
KOSASIH <kosasihg88@gmail.com>
2024-05-11 18:44:21 +0700

    Create network_monitoring.py

---
+++ network/network_management/network_monitoring.py
@@ -0,0 +1,15 @@
+class NetworkMonitoring:
+    def __init__(self, network_management):
+        self.network_management = network_management
+
+    def monitor_traffic(self):
+        # monitor network traffic
+        pass
+
+    def monitor_latency(self):
+        # monitor network latency
+        pass
+
+    def monitor_availability(self):
+        # monitor network availability
+        pass
