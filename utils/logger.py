094c952f229c82cd1142651e21e6cbf2a84dedbf
KOSASIH <kosasihg88@gmail.com>
2024-05-13 23:14:51 +0700

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,29 @@
+import logging
+
+def get_logger(name):
+    """
+    Returns a logger object with the given name.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # Create a console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    # Create a file handler
+    file_handler = logging.FileHandler('app.log')
+    file_handler.setLevel(logging.INFO)
+
+    # Create a formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Add the formatter to the handlers
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    # Add the handlers to the logger
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    return logger
