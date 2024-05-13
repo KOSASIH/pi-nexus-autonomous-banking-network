12d59d04842606a60bda885af3d7aed6a84bfd43
KOSASIH <kosasihg88@gmail.com>
2024-05-13 22:56:50 +0700

    Create authentication_service.py

---
+++ services/authentication_service.py
@@ -0,0 +1,24 @@
+# services/authentication_service.py
+import jwt
+import datetime
+
+class AuthenticationService:
+    def __init__(self, secret_key):
+        self.secret_key = secret_key
+
+    def generate_token(self, user_id):
+        payload = {
+            "user_id": user_id,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1)
+        }
+        token = jwt.encode(payload, self.secret_key, algorithm="HS256")
+        return token
+
+    def decode_token(self, token):
+        try:
+            payload = jwt.decode(token, self.secret_key, algorithms=["HS256"])
+            return payload["user_id"]
+        except jwt.ExpiredSignatureError:
+            raise Exception("Token has expired")
+        except jwt.InvalidTokenError:
+            raise Exception("Invalid token")
