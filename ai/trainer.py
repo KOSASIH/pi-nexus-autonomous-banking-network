669aa3197fc280579299824c96b3d07649cc82c2
KOSASIH <kosasihg88@gmail.com>
2024-05-13 13:42:44 +0700

    Create trainer.py

---
+++ ai/trainer.py
@@ -0,0 +1,8 @@
+from models import NexusModel
+from data_loader import load_data
+
+def train_model(model: NexusModel, data: pd.DataFrame) -> None:
+    """Train the Nexus AI model."""
+    X, y = data.drop('target', axis=1), data['target']
+    model.compile()
+    model.model.fit(X, y, epochs=10, batch_size=32)
