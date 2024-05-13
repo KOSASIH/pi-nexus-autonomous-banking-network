482c8b597ca97046ddc8d197624a5ccf07ede3a8
KOSASIH <kosasihg88@gmail.com>
2024-05-13 11:46:18 +0700

    Create database.py

---
+++ utils/database.py
@@ -0,0 +1,13 @@
+# pi_nexus-autonomous-banking-network/utils/database.py
+import sqlite3
+
+def connect_to_db():
+    """Connect to the database"""
+    conn = sqlite3.connect('database.db')
+    return conn
+
+def execute_query(conn, query):
+    """Execute a query on the database"""
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
