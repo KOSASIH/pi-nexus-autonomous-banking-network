f3aaf6458251d5d40be0b83b77a250085f1d1ff5
KOSASIH <kosasihg88@gmail.com>
2024-05-13 22:33:08 +0700

    Create test.js

---
+++ scripts/test.js
@@ -0,0 +1,28 @@
+const { expect } = require("chai");
+
+const { ethers } = require("hardhat");
+
+describe("MyContract", function () {
+  let myContract;
+
+  beforeEach(async function () {
+    const MyContract = await ethers.getContractFactory("MyContract");
+    myContract = await MyContract.deploy();
+    await myContract.deployed();
+  });
+
+  describe("constructor", function () {
+    it("should set the initial value", async function () {
+      const initialValue = await myContract.getValue();
+      expect(initialValue).to.equal(42);
+    });
+  });
+
+  describe("setValue", function () {
+    it("should set the value", async function () {
+      await myContract.setValue(43);
+      const value = await myContract.getValue();
+      expect(value).to.equal(43);
+    });
+  });
+});
