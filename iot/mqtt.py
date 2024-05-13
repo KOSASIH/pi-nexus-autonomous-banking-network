5d1f6cd4f564026882d47ffb623a8e9a3d9dde1b
KOSASIH <kosasihg88@gmail.com>
2024-05-13 13:07:38 +0700

    Create mqtt.py

---
+++ iot/mqtt.py
@@ -0,0 +1,20 @@
+import paho.mqtt.client as mqtt
+
+class MQTT:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.client = mqtt.Client()
+
+    def connect(self):
+        self.client.connect(self.host, self.port)
+
+    def publish(self, topic, message):
+        self.client.publish(topic, message)
+
+    def subscribe(self, topic, callback):
+        self.client.subscribe(topic)
+        self.client.on_message = callback
+
+    def loop(self):
+        self.client.loop_forever()
