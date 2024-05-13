5f0f21c8433ca63ae913920b982f14113397469c
KOSASIH <kosasihg88@gmail.com>
2024-05-13 23:01:53 +0700

    Create fraud_detection_service.py

---
+++ services/fraud_detection_service.py
@@ -0,0 +1,14 @@
+# services/fraud_detection_service.py
+import tensorflow as tf
+
+class FraudDetectionService:
+    def __init__(self, model):
+        self.model = model
+
+    def detect_fraud(self, transaction):
+        input_data = tf.convert_to_tensor([transaction])
+        prediction = self.model.predict(input_data)
+        if prediction > 0.5:
+            return True
+        else:
+            return False
