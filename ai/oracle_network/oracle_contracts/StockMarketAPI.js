29bfe2038eb733d28cb23a912b27f79236ce4fbe
KOSASIH <kosasihg88@gmail.com>
2024-05-13 14:11:32 +0700

    Create StockMarketAPI.js

---
+++ ai/oracle_network/oracle_contracts/StockMarketAPI.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+class StockMarketAPI {
+    constructor(symbol) {
+        this.symbol = symbol;
+    }
+
+    async getData() {
+        const response = await axios.get(`https://financialmodelingprep.com/api/v3/quote/${this.symbol}?apikey=YOUR_API_KEY`);
+        return response.data[0].price;
+    }
+}
+
+module.exports = StockMarketAPI;
