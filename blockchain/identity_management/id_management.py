cb4fae3189003210fe021d491ea4f7e78645bbef
KOSASIH <kosasihg88@gmail.com>
2024-05-13 12:23:03 +0700

    Create id_management.py

---
+++ blockchain/identity_management/id_management.py
@@ -0,0 +1,21 @@
+import uuid
+
+class IDManagement:
+    def __init__(self):
+        self.ids = set()
+
+    def generate_id(self):
+        """
+        Generate a unique ID.
+        """
+        id = str(uuid.uuid4())
+        while id in self.ids:
+            id = str(uuid.uuid4())
+        self.ids.add(id)
+        return id
+
+    def validate_id(self, id):
+        """
+        Validate a given ID.
+        """
+        return id in self.ids
