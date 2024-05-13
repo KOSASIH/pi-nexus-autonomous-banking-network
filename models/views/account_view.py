c0a0553000a04a66d04ac1a03c532ba845b249e7
KOSASIH <kosasihg88@gmail.com>
2024-05-14 00:20:12 +0700

    Create account_view.py

---
+++ models/views/account_view.py
@@ -0,0 +1,12 @@
+from typing import List, Dict
+from jinja2 import Environment, FileSystemLoader
+
+def render_account_balance(account: Dict) -> str:
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('account_balance.html')
+    return template.render(account=account)
+
+def render_transaction_history(transactions: List[Dict]) -> str:
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('transaction_history.html')
+    return template.render(transactions=transactions)
