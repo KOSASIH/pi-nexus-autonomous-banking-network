64c6958be2615127ec4e8e2dfa8de6129accc60e
KOSASIH <kosasihg88@gmail.com>
2024-05-13 13:06:55 +0700

    Create device.py

---
+++ iot/device.py
@@ -0,0 +1,12 @@
+class Device:
+    def __init__(self, name, topic):
+        self.name = name
+        self.topic = topic
+
+    def read_sensor(self):
+        # Kode untuk membaca data sensor
+        pass
+
+    def send_command(self, command):
+        # Kode untuk mengirim perintah ke perangkat IoT
+        pass
