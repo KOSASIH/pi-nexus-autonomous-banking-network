24ccbc1f92540f498545d4c4f7b8d6275407f4c8
KOSASIH <kosasihg88@gmail.com>
2024-05-13 22:51:51 +0700

    Create account.py

---
+++ models/account.py
@@ -0,0 +1,13 @@
+# models/account.py
+class Account:
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise ValueError("Insufficient balance")
+        self.balance -= amount
