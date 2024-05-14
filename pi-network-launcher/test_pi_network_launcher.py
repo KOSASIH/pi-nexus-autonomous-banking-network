117d364db5234cdda7970579390f0b4db30fe2bd
KOSASIH <kosasihg88@gmail.com>
2024-05-15 04:12:54 +0700

    Create test_pi_network_launcher.py

---
+++ pi-network-launcher/test_pi_network_launcher.py
@@ -0,0 +1,12 @@
+import unittest
+from pi_network_launcher import PiNetworkLauncher
+
+class TestPiNetworkLauncher(unittest.TestCase):
+
+    def test_launcher(self):
+        launcher = PiNetworkLauncher()
+        output = launcher.launch()
+        self.assertEqual(output, "Expected output")
+
+if __name__ == '__main__':
+    unittest.main()
