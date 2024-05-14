bcda9c6925bd1ae2a7a5082738bd545cea402dbd
KOSASIH <kosasihg88@gmail.com>
2024-05-14 13:07:01 +0700

    Create humidity_sensor.py

---
+++ sensors/humidity_sensor.py
@@ -0,0 +1,14 @@
+class HumiditySensor:
+    def __init__(self, port):
+        # Initialize the humidity sensor
+        pass
+
+    def read_data(self):
+        """
+        Read data from the humidity sensor.
+
+        Returns:
+            dict: The humidity sensor data.
+        """
+        # Read data from humidity sensor
+        pass
