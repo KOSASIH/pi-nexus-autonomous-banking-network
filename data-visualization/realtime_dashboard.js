605c3a9c8a01d24a0e7555ebbd633da3f877bbdb
KOSASIH <kosasihg88@gmail.com>
2024-05-14 10:06:49 +0700

    Create realtime_dashboard.js

---
+++ data-visualization/realtime_dashboard.js
@@ -0,0 +1,25 @@
+// realtime_dashboard.js
+const React = require('react');
+const { useState, useEffect } = React;
+
+function RealtimeDashboard() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const dataIngestion = new DataIngestion();
+    dataIngestion.ingestData().then((data) => {
+      setData(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Real-time Dashboard</h1>
+      <ul>
+        {data.map((datum, index) => (
+          <li key={index}>{datum.timestamp}: {datum.value}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
