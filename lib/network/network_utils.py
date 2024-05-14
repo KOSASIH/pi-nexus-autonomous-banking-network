731fa1904db6301ae296c3be78ef4911de267f63
KOSASIH <kosasihg88@gmail.com>
2024-05-14 12:52:18 +0700

    Create network_utils.py

---
+++ lib/network/network_utils.py
@@ -0,0 +1,7 @@
+def send_data_to_server(data):
+    # Send data to a server using HTTP requests
+    pass
+
+def receive_data_from_server():
+    # Receive data from a server using socket programming
+    pass
