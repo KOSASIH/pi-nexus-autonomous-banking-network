b5f2a3b50f488dea582e257139f1abedf5440902
KOSASIH <kosasihg88@gmail.com>
2024-05-14 10:00:02 +0700

    Create leaderboard.js

---
+++ gamification/leaderboard.js
@@ -0,0 +1,18 @@
+// leaderboard.js
+const axios = require('axios');
+
+const LEADERBOARD_API_URL = 'https://api.leaderboard.com';
+
+const getLeaderboard = async () => {
+  try {
+    const response = await axios.get(`${LEADERBOARD_API_URL}/leaderboard`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching leaderboard: ${error.message}`);
+    throw error;
+  }
+};
+
+module.exports = {
+  getLeaderboard,
+};
