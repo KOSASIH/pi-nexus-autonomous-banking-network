177f800db30ac2bcc5ca36404a988a9b51700c84
KOSASIH <kosasihg88@gmail.com>
2024-05-14 09:52:02 +0700

    Create polkadot_protocol.js

---
+++ cross-chain-interoperability/polkadot_protocol.js
@@ -0,0 +1,23 @@
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+const { Keyring } = require('@polkadot/keyring');
+const { encodeAddress } = require('@polkadot/util-crypto');
+
+const wsProvider = new WsProvider('wss://polkadot.js.org/api');
+const api = await ApiPromise.create({ provider: wsProvider });
+const keyring = new Keyring({ type: 'sr25519' });
+
+const polkadotAddress = encodeAddress(keyring.addFromUri('your-mnemonic-phrase').address, 42);
+
+const sendCrossChainMessage = async (destinationChain, message) => {
+  const { data: { Xcm: xcm } } = await api.tx.xcmPallet
+    .send(destinationChain, message)
+    .signAndSend(polkadotAddress);
+
+  console.log('Cross-chain message sent:', xcm);
+};
+
+// Example usage
+const destinationChain = 1234; // Replace with the ID of the destination chain
+const message = 'Hello, cross-chain world!';
+
+sendCrossChainMessage(destinationChain, message);
