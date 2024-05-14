52846ba5ee800435f093f39cba966ee09937282a
KOSASIH <kosasihg88@gmail.com>
2024-05-14 14:08:56 +0700

    Update end_to_end.go

--- security/encryption/end_to_end.go
+++ security/encryption/end_to_end.go
@@ -26,4 +26,41 @@ func (e *endToEndEncryption) Encrypt(plaintext []byte) ([]byte, error) {
 
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-	
+	return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return ciphertext, nil
+}
+
+func (e *endToEndEncryption) Decrypt(ciphertext []byte) ([]byte, error) {
+	block, err := aes.NewCipher(e.key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, err
+	}
+
+	nonce := ciphertext[:nonceSize]
+	ciphertext = ciphertext[nonceSize:]
+
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
