ce8ee2908da4003333a2d491c221b6dc9818c5fa
KOSASIH <kosasihg88@gmail.com>
2024-05-14 08:18:33 +0700

    Create Proposal.sol

---
+++ blockchain/governance/Proposal.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/governance/extensions/GovernorProposal.sol";
+
+contract Proposal is GovernorProposal {
+    // Additional state variables and functions can be addedhere
+}
