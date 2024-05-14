ed757041799b902b5ee6cd8707eac2ca77cee9da
KOSASIH <kosasihg88@gmail.com>
2024-05-14 11:30:11 +0700

    Create test_transaction_processing.py

---
+++ tests/test_transaction_processing.py
@@ -0,0 +1,7 @@
+# tests/test_transaction_processing.py
+import unittest
+from transaction_processing.tasks import process_transaction
+
+class TestTransactionProcessing(unittest.TestCase):
+    def test_process_transaction(self):
+        # ...
