20194060cf4c2fa2ef26addc922c549c64e23e15
KOSASIH <kosasihg88@gmail.com>
2024-05-14 13:04:27 +0700

    Create sensor_utils.py

---
+++ sensor/sensor_utils.py
@@ -0,0 +1,14 @@
+class TemperatureSensor:
+    def __init__(self, port):
+        # Initialize the temperature sensor
+        pass
+
+    def read_data(self):
+        """
+        Read data from the temperature sensor.
+
+        Returns:
+            dict: The temperature sensor data.
+        """
+        # Read data from temperature sensor
+        pass
