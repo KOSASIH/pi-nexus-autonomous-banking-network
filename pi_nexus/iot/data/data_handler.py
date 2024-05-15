dc6c0b7805d90b2faf66f2c72e694669e9b55b71
KOSASIH <kosasihg88@gmail.com>
2024-05-15 10:58:58 +0700

    Create data_handler.py

---
+++ pi_nexus/iot/data/data_handler.py
@@ -0,0 +1,14 @@
+# iot/data/data_handler.py
+import pandas as pd
+
+class DataHandler:
+    def __init__(self):
+        self.data = pd.DataFrame()
+
+    def add_data(self, device_id, data):
+        # implementation
+        pass
+
+    def process_data(self):
+        # implementation
+        pass
