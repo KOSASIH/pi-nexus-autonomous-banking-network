6be915f4fa1589bc5a630948c07c41724c9e7bde
KOSASIH <kosasihg88@gmail.com>
2024-05-15 09:14:23 +0700

    Create models.py

---
+++ src/core/models.py
@@ -0,0 +1,14 @@
+# src/core/models.py
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class BankAccount(Base):
+    __tablename__ = 'bank_accounts'
+    id = Column(Integer, primary_key=True)
+    account_number = Column(String)
+    account_holder = Column(String)
+
+    def to_dict(self):
+        return {'id': self.id, 'account_number': self.account_number, 'account_holder': self.account_holder}
