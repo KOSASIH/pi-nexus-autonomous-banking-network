8780f95927cc99f74ab291ccae745fed5f4180aa
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:00:13 +0700

    Create shor.py

---
+++ pi_nexus/quantum/algorithms/shor.py
@@ -0,0 +1,14 @@
+# quantum/algorithms/shor.py
+from qiskit import QuantumCircuit, transpile, assemble, Aer, execute
+
+class Shor:
+    def __init__(self):
+        self.qc = QuantumCircuit(2, 2)
+
+    def create_circuit(self):
+        # implementation
+        pass
+
+    def run_circuit(self):
+        # implementation
+        pass
