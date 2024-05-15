544c599e3ded28e65bacc2ffcb54352d1aed71ae
KOSASIH <kosasihg88@gmail.com>
2024-05-15 10:59:36 +0700

    Create grover.py

---
+++ pi_nexus/quantum/algorithms/grover.py
@@ -0,0 +1,14 @@
+# quantum/algorithms/grover.py
+from qiskit import QuantumCircuit, transpile, assemble, Aer, execute
+
+class Grover:
+    def __init__(self):
+        self.qc = QuantumCircuit(2, 2)
+
+    def create_circuit(self):
+        # implementation
+        pass
+
+    def run_circuit(self):
+        # implementation
+        pass
