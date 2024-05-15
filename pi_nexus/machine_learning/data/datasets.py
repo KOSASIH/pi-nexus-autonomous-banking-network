0e97a816e747ac56c55824ecf8acb7fa11affca7
KOSASIH <kosasihg88@gmail.com>
2024-05-15 10:45:27 +0700

    Create datasets.py

---
+++ pi_nexus/machine_learning/data/datasets.py
@@ -0,0 +1,8 @@
+# machine_learning/data/datasets.py
+import pandas as pd
+
+class Datasets:
+    def __init__(self):
+        self.fraud_data = pd.read_csv("fraud_data.csv")
+        self.user_behavior_data = pd.read_csv("user_behavior_data.csv")
+        self.system_performance_data = pd.read_csv("system_performance_data.csv")
