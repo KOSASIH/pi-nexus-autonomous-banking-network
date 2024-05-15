30c8cf3dccae74e2700c98e7d1718fc236141b7f
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:25:45 +0700

    Create documentation_engine.py

---
+++ pi_nexus/documentation/documentation_engine.py
@@ -0,0 +1,18 @@
+# documentation/documentation_engine.py
+import inspect
+import ast
+
+class DocumentationEngine:
+    def __init__(self):
+        self.documentation_rules = DocumentationRules()
+
+    def generate_documentation(self, code):
+        tree = ast.parse(code)
+        documentation = self.apply_documentation_rules(tree)
+        return documentation
+
+    def apply_documentation_rules(self, tree):
+        documentation = {}
+        for rule in self.documentation_rules.rules:
+            documentation.update(rule.apply(tree))
+        return documentation
