339d9fa12c4cf86b9d200c150b730a6b3410e909
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:16:30 +0700

    Create generator_engine.py

---
+++ pi_nexus/code_generation/generator_engine.py
@@ -0,0 +1,17 @@
+# code_generation/generator_engine.py
+import inspect
+
+class GeneratorEngine:
+    def __init__(self):
+        self.generator_rules = GeneratorRules()
+
+    def generate_code(self, requirements):
+        templates = self.generator_rules.get_templates(requirements)
+        generated_code = self.generate_from_templates(templates)
+        return generated_code
+
+    def generate_from_templates(self, templates):
+        generated_code = ""
+        for template in templates:
+            generated_code += template.generate()
+        return generated_code
