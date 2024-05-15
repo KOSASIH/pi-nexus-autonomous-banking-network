016d49cb7ccde7074de55b84338e22f52b0537a7
KOSASIH <kosasihg88@gmail.com>
2024-05-15 10:57:13 +0700

    Create sentiment.py

---
+++ pi_nexus/ai_nlp/models/sentiment.py
@@ -0,0 +1,15 @@
+# ai_nlp/models/sentiment.py
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+class SentimentModel:
+    def __init__(self):
+        self.model = LogisticRegression()
+
+    def train(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X):
+        return self.model.predict(X)
