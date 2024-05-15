0ce6844f9f286a06c1ef73d1bf469c412e629cf6
KOSASIH <kosasihg88@gmail.com>
2024-05-15 07:48:53 +0700

    Create failover.py

---
+++ disaster_recovery/failover.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+
+def failover_data():
+    # Code for implementing failover
+    subprocess.run(["/path/to/failover/script.sh"])
+
+if __name__ == "__main__":
+    failover_data()
