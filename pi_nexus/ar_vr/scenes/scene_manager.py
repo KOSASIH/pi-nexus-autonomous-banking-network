6d09a7cd2ec82e00fa4ac2e477d87a5c475c0f6c
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:00:53 +0700

    Create scene_manager.py

---
+++ pi_nexus/ar_vr/scenes/scene_manager.py
@@ -0,0 +1,15 @@
+# ar_vr/scenes/scene_manager.py
+import pygame
+from .opengl import OpenGL
+
+class SceneManager:
+    def __init__(self):
+        self.opengl = OpenGL()
+
+    def create_scene(self):
+        # implementation
+        pass
+
+    def render_scene(self):
+        # implementation
+        pass
