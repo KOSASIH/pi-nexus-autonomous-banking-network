1a17b9af8a11f9e5cf16186d7d7961651b744284
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:08:23 +0700

    Create deploy_engine.py

---
+++ pi_nexus/deployment_cd/deploy_engine.py
@@ -0,0 +1,12 @@
+# deployment_cd/deploy_engine.py
+import subprocess
+
+class DeployEngine:
+    def __init__(self):
+        self.kubernetes = Kubernetes()
+
+    def deploy(self):
+        self.kubernetes.apply_configurations()
+
+    def rollback(self):
+        self.kubernetes.rollback_configurations()
