c3e2ab0f248d3a0158dfde906eded8b81130298e
KOSASIH <kosasihg88@gmail.com>
2024-05-15 20:50:21 +0700

    Create utils.py

---
+++ blockchain_integration/pi_network/utils.py
@@ -0,0 +1,19 @@
+# utils.py
+import os
+import logging
+
+def create_directory(directory: str) -> None:
+    """
+    Create a directory if it doesn't exist.
+
+    Args:
+        directory (str): The directory to create.
+    """
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def setup_logging() -> None:
+    """
+    Set up logging for the application.
+    """
+    logging.basicConfig(filename='app.log', level=logging.INFO)
