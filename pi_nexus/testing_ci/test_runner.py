ef1492eb0539775d60317308aa57579c10ecf740
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:07:07 +0700

    Create test_runner.py

---
+++ pi_nexus/testing_ci/test_runner.py
@@ -0,0 +1,15 @@
+# testing_ci/test_runner.py
+import unittest
+
+class TestRunner:
+    def __init__(self):
+        self.test_cases = [
+            # implementation
+        ]
+
+    def run_tests(self):
+        suite = unittest.TestSuite()
+        for test_case in self.test_cases:
+            suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(test_case))
+        runner = unittest.TextTestRunner()
+        runner.run(suite)
