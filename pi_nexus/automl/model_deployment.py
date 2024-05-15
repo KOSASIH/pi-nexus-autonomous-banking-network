3ff4028b1331f8be616433c58c5a53cc464eb88d
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:11:06 +0700

    Create model_deployment.py

---
+++ pi_nexus/automl/model_deployment.py
@@ -0,0 +1,11 @@
+# automl/model_deployment.py
+import joblib
+from .model_training import ModelTraining
+
+class ModelDeployment:
+    def __init__(self):
+        self.model_training = ModelTraining()
+
+    def deploy_model(self, X, y):
+        model = self.model_training.train_model(X, y)
+        joblib.dump(model, "model.joblib")
