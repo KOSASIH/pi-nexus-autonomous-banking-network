96b080f26ebecb57d1742d36d6b476723ca8fc76
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:06:13 +0700

    Create refactoring_rules.py

---
+++ pi_nexus/code_refactoring/refactoring_rules.py
@@ -0,0 +1,10 @@
+# code_refactoring/refactoring_rules.py
+class RefactoringRules:
+    def __init__(self):
+        self.rules = [
+            # implementation
+        ]
+
+    def apply_rules(self, tree):
+        for rule in self.rules:
+            rule.apply(tree)
