618065d1f61bd1d24134b2c3a103e9374672b6d1
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:01:29 +0700

    Create object_manager.py

---
+++ pi_nexus/ar_vr/objects/object_manager.py
@@ -0,0 +1,15 @@
+# ar_vr/objects/object_manager.py
+import pygame
+from .opengl import OpenGL
+
+class ObjectManager:
+    def __init__(self):
+        self.opengl = OpenGL()
+
+    def create_object(self):
+        # implementation
+        pass
+
+    def update_object(self):
+        # implementation
+        pass
