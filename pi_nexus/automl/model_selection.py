cb12a5d622331d48f33961b8e87475a17369314d
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:09:40 +0700

    Create model_selection.py

---
+++ pi_nexus/automl/model_selection.py
@@ -0,0 +1,12 @@
+# automl/model_selection.py
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+class ModelSelection:
+    def __init__(self):
+        self.data_handler = DataHandler()
+
+    def select_model(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        # implementation
+        pass
