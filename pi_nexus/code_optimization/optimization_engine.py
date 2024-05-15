efbbd01bd0fb836ce5d32082c2c73f263fdac3cb
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:19:27 +0700

    Create optimization_engine.py

---
+++ pi_nexus/code_optimization/optimization_engine.py
@@ -0,0 +1,18 @@
+# code_optimization/optimization_engine.py
+import ast
+
+class OptimizationEngine:
+    def __init__(self):
+        self.optimization_rules = OptimizationRules()
+
+    def optimize_code(self, code):
+        tree = ast.parse(code)
+        optimized_tree = self.apply_optimization_rules(tree)
+        optimized_code = compile(optimized_tree, "<string>", "exec")
+        return optimized_code
+
+    def apply_optimization_rules(self, tree):
+        optimized_tree = copy.deepcopy(tree)
+        for rule in self.optimization_rules.rules:
+            rule.apply(optimized_tree)
+        return optimized_tree
