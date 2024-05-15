71cb0e115e787a7b4bcdb0ed71a82d685b2afc84
KOSASIH <kosasihg88@gmail.com>
2024-05-15 09:22:44 +0700

    Update models.py

--- src/core/models.py
+++ src/core/models.py
@@ -1,6 +1,7 @@
 # src/core/models.py
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -12,3 +13,32 @@ class BankAccount(Base):
 
     def to_dict(self):
         return {'id': self.id, 'account_number': self.account_number, 'account_holder': self.account_holder}
+
+def create_bank_account(session, account_number, account_holder):
+    new_account = BankAccount(account_number=account_number, account_holder=account_holder)
+    session.add(new_account)
+    session.commit()
+    return new_account
+
+def update_bank_account(session, account_id, account_number, account_holder):
+    account = session.query(BankAccount).filter_by(id=account_id).first()
+    if account:
+        account.account_number = account_number
+        account.account_holder = account_holder
+        session.commit()
+        return account
+    return None
+
+def delete_bank_account(session, account_id):
+    account = session.query(BankAccount).filter_by(id=account_id).first()
+    if account:
+        session.delete(account)
+        session.commit()
+        return True
+    return False
+
+def get_bank_account_by_id(session, account_id):
+    return session.query(BankAccount).filter_by(id=account_id).first()
+
+def get_bank_accounts_by_query(session, query):
+    return session.query(BankAccount).filter(BankAccount.account_number.ilike(f"%{query}%") | BankAccount.account_holder.ilike(f"%{query}%")).all()
