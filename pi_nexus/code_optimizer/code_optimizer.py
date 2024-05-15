d2c990d8e52e7ee250dcaf4e3aabccb440faf6b5
KOSASIH <kosasihg88@gmail.com>
2024-05-15 12:53:37 +0700

    Create code_optimizer.py

---
+++ pi_nexus/code_optimizer/code_optimizer.py
@@ -0,0 +1,17 @@
+import random
+from deap import base, creator, tools
+
+class CodeOptimizer:
+    def __init__(self, code: str, fitness_function):
+        self.code = code
+        self.fitness_function = fitness_function
+        self.population = [code] * 100
+
+    def evolve_code(self, generations: int) -> str:
+        for _ in range(generations):
+            offspring = algorithms.varAnd(self.population, toolbox, cxpb=0.5, mutpb=0.1)
+            fits = [self.fitness_function(ind) for ind in offspring]
+            for fit, ind in zip(fits, offspring):
+                ind.fitness.values = fit
+            self.population = toolbox.select(offspring, k=len(self.population))
+        return max(self.population, key=self.fitness_function)
