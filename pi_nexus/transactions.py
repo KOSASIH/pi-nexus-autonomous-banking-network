baa4db946cc01c915db5d8d8d418488cc40177d4
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:37:31 +0700

    Create transactions.py

---
+++ pi_nexus/transactions.py
@@ -0,0 +1,10 @@
+# pi_nexus/transactions.py
+class Transaction:
+    def __init__(self, id: int, data: dict) -> None:
+        self.id = id
+        self.amount = data["amount"]
+
+class Account:
+    def __init__(self, id: int, data: dict) -> None:
+        self.id = id
+        self.balance = data["balance"]
