ba39e4320f3cab1839eafc8990d504f33ef158cd
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:02:45 +0700

    Create data_handler.py

---
+++ pi_nexus/edge_5g/data/data_handler.py
@@ -0,0 +1,14 @@
+# edge_5g/data/data_handler.py
+import pandas as pd
+
+class DataHandler:
+    def __init__(self):
+        self.data = pd.DataFrame()
+
+    def add_data(self, data):
+        # implementation
+        pass
+
+    def process_data(self):
+        # implementation
+        pass
