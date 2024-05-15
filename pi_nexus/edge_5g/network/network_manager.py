1da9453a947d13cba15702bc10626e3211d35047
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:02:09 +0700

    Create network_manager.py

---
+++ pi_nexus/edge_5g/network/network_manager.py
@@ -0,0 +1,18 @@
+# edge_5g/network/network_manager.py
+import time
+
+class NetworkManager:
+    def __init__(self):
+        self.edge_computing = EdgeComputing()
+
+    def connect_to_network(self):
+        # implementation
+        pass
+
+    def send_data(self, data):
+        # implementation
+        pass
+
+    def receive_data(self):
+        # implementation
+        pass
