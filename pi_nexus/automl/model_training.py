d251e540adb9004aa84c60e833b4e78d33bca5aa
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:10:23 +0700

    Create model_training.py

---
+++ pi_nexus/automl/model_training.py
@@ -0,0 +1,12 @@
+# automl/model_training.py
+import joblib
+from .model_selection import ModelSelection
+
+class ModelTraining:
+    def __init__(self):
+        self.model_selection = ModelSelection()
+
+    def train_model(self, X, y):
+        model = self.model_selection.select_model(X, y)
+        model.fit(X, y)
+        return model
