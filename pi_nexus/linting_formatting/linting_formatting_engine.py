7481028a42d70f1d3a3ab48ed62c78000ed9ce0b
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:27:10 +0700

    Create linting_formatting_engine.py

---
+++ pi_nexus/linting_formatting/linting_formatting_engine.py
@@ -0,0 +1,21 @@
+# linting_formatting/linting_formatting_engine.py
+import subprocess
+
+class LintingFormattingEngine:
+    def __init__(self):
+        self.linting_formatting_rules = LintingFormattingRules()
+
+    def lint_format_code(self, code):
+        issues = self.linting_formatting_rules.apply(code)
+        formatted_code = self.apply_formatting_rules(code, issues)
+        return formatted_code
+
+    def apply_formatting_rules(self, code, issues):
+        formatted_code = code
+        for issue in issues:
+            formatted_code = self.apply_formatting_rule(formatted_code, issue)
+        return formatted_code
+
+    def apply_formatting_rule(self, code, issue):
+        # implementation
+        pass
