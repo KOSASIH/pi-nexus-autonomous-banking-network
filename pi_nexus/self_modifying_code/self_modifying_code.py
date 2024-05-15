0bacacc6072b2f2e68a585fd4ecddeaef782ac42
KOSASIH <kosasihg88@gmail.com>
2024-05-15 12:52:10 +0700

    Create self_modifying_code.py

---
+++ pi_nexus/self_modifying_code/self_modifying_code.py
@@ -0,0 +1,10 @@
+import ast
+
+class SelfModifyingCode:
+    def __init__(self, code: str):
+        self.code = code
+        self.tree = ast.parse(code)
+
+    def modify_code(self, new_code: str) -> str:
+        self.tree.body[0].value = ast.parse(new_code).body[0].value
+        return ast.unparse(self.tree)
