17620c0df0d7399aa9f3f831c2adbcbc310c719a
KOSASIH <kosasihg88@gmail.com>
2024-05-15 10:48:51 +0700

    Create wallet.py

---
+++ pi_nexus/blockchain/wallet.py
@@ -0,0 +1,11 @@
+# blockchain/wallet.py
+from web3 import Web3
+
+class Wallet:
+    def __init__(self, web3: Web3):
+        self.web3 = web3
+        self.account_address = "0x..."
+
+    def send_transaction(self, to_address, value):
+        # implementation
+        pass
