8c839d4b6b95a285e8b334e2929e62ce8315ecc0
KOSASIH <kosasihg88@gmail.com>
2024-05-15 10:43:16 +0700

    Create fraud_detection.py

---
+++ pi_nexus/machine_learning/models/fraud_detection.py
@@ -0,0 +1,15 @@
+# machine_learning/models/fraud_detection.py
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+class FraudDetectionModel:
+    def __init__(self):
+        self.model = RandomForestClassifier()
+
+    def train(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X):
+        return self.model.predict(X)
