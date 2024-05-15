7740253beea7ef251708bc9ef666aaa3bec600b0
KOSASIH <kosasihg88@gmail.com>
2024-05-15 09:26:03 +0700

    Create app.py

---
+++ src/web/app.py
@@ -0,0 +1,18 @@
+# src/web/app.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db = SQLAlchemy(app)
+
+    from src.core.models import BankAccount
+    db.create_all()
+
+    from src.web.views import views
+    app.register_blueprint(views)
+
+    return app
