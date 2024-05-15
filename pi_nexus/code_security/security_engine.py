24e83147df33acbbdfda92e90a957647b6b12bac
KOSASIH <kosasihg88@gmail.com>
2024-05-15 11:20:48 +0700

    Create security_engine.py

---
+++ pi_nexus/code_security/security_engine.py
@@ -0,0 +1,17 @@
+# code_security/security_engine.py
+import ast
+
+class SecurityEngine:
+    def __init__(self):
+        self.security_rules = SecurityRules()
+
+    def audit_code(self, code):
+        tree = ast.parse(code)
+        vulnerabilities = self.apply_security_rules(tree)
+        return vulnerabilities
+
+    def apply_security_rules(self, tree):
+        vulnerabilities = []
+        for rule in self.security_rules.rules:
+            vulnerabilities += rule.apply(tree)
+        return vulnerabilities
