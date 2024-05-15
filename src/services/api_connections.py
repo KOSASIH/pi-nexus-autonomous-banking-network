1d3ea9da06e62733803f4bc09918649ce06cd712
KOSASIH <kosasihg88@gmail.com>
2024-05-15 09:17:16 +0700

    Create api_connections.py

---
+++ src/services/api_connections.py
@@ -0,0 +1,10 @@
+# src/services/api_connections.py
+import requests
+
+class PiNexusAPI:
+    def __init__(self):
+        self.base_url = "https://api.pi-nexus.com"
+
+    def get_data(self):
+        response = requests.get(self.base_url + "/data")
+        return response.json()
