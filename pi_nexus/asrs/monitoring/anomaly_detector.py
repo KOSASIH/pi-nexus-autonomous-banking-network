feb008f5fe6ec3ca93b720f069aa1e477bbd39fb
KOSASIH <kosasihg88@gmail.com>
2024-05-15 13:47:35 +0700

    Create anomaly_detector.py

---
+++ pi_nexus/asrs/monitoring/anomaly_detector.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import zscore
+
+class AnomalyDetector:
+    def __init__(self, config):
+        self.config = config
+        self.logger = logging.getLogger(__name__)
+
+    def detect_anomalies(self, data):
+        """Detect anomalies using statistical methods (e.g., z-score)."""
+        self.logger.info("Detecting anomalies...")
+        z_scores = zscore(data)
+        anomalies = np.where(np.abs(z_scores) > self.config.ANOMALY_THRESHOLD)[0]
+        return anomalies
