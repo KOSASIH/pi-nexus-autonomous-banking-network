3a1ff301efc2a03b2032d7f3b8111df211250e55
KOSASIH <kosasihg88@gmail.com>
2024-05-15 07:21:43 +0700

    Create banking_logic.py

---
+++ src/banking_network/banking_logic.py
@@ -0,0 +1,13 @@
+class AccountManager:
+    def __init__(self, db):
+        self.db = db
+
+    def create_account(self, account_data):
+        # Create a new account
+
+class TransactionProcessor:
+    def __init__(self, db):
+        self.db = db
+
+    def process_transaction(self, transaction_data):
+        # Process a transaction
