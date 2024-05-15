306dbf7b8f17493ed8fe5043bca79b64133d0aee
KOSASIH <kosasihg88@gmail.com>
2024-05-15 20:49:42 +0700

    Create config.py

---
+++ blockchain_integration/pi_network/config.py
@@ -0,0 +1,13 @@
+# config.py
+coins_file = 'coins.json'
+stable_value = 10.0
+exchanges = ['localhost:8080']
+
+# Load the configuration from a file
+with open('config.json', 'r') as file:
+    config = json.load(file)
+
+# Set the configuration variables
+coins_file = config['coins_file']
+stable_value = config['stable_value']
+exchanges = config['exchanges']
