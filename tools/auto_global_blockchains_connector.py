6cd7be9c3a2e0c7280d4a954eddbda39f0776d3e
KOSASIH <kosasihg88@gmail.com>
2024-05-16 18:06:13 +0700

    Create auto_global_blockchains_connector.py

---
+++ tools/auto_global_blockchains_connector.py
@@ -0,0 +1,81 @@
+import json
+import requests
+
+# Define the list of global blockchain networks to connect to
+BLOCKCHAIN_NETWORKS = [
+    {
+        'name': 'Bitcoin',
+        'rpc_url': 'https://bitcoin.rpc.ont.io',
+        'rpc_username': 'username',
+        'rpc_password': 'password',
+        'chain_id': '0000000000000000000000000000000000000000000000000000000000000000'
+    },
+    {
+        'name': 'Ethereum',
+        'rpc_url': 'https://mainnet.infura.io/v3/YOUR_PROJECT_ID',
+        'rpc_username': '',
+        'rpc_password': '',
+        'chain_id': '1'
+    },
+    # Add more blockchain networks as needed
+]
+
+# Define a function to send an RPC request to a blockchain network
+def send_rpc_request(rpc_url, rpc_username, rpc_password, method, params=None):
+    headers = {'Content-Type': 'application/json'}
+    if rpc_username and rpc_password:
+        auth = (rpc_username, rpc_password)
+    else:
+        auth = None
+    data = json.dumps({'jsonrpc': '2.0', 'method': method, 'params': params, 'id': 1})
+    response = requests.post(rpc_url, headers=headers, data=data, auth=auth)
+    response.raise_for_status()
+    return response.json()
+
+# Define a function to get the balance of an address on a blockchain network
+def get_balance(network, address):
+    if network['name'] == 'Bitcoin':
+        method = 'getbalance'
+        params = [address, '*']
+    elif network['name'] == 'Ethereum':
+        method = 'eth_getBalance'
+        params = [address, 'latest']
+    else:
+        raise ValueError(f'Unsupported blockchain network: {network["name"]}')
+    response = send_rpc_request(network['rpc_url'], network['rpc_username'], network['rpc_password'], method, params)
+    return response['result']
+
+# Define a function to send a transaction on a blockchain network
+def send_transaction(network, from_address, to_address, amount):
+    if network['name'] == 'Bitcoin':
+        method = 'sendtoaddress'
+        params = [to_address, amount]
+    elif network['name'] == 'Ethereum':
+        method = 'eth_sendTransaction'
+        params = {
+            'from': from_address,
+            'to': to_address,
+            'value': amount,
+            'gas': 21000,
+            'gasPrice': '20000000000'
+        }
+    else:
+        raise ValueError(f'Unsupported blockchain network: {network["name"]}')
+    response = send_rpc_request(network['rpc_url'], network['rpc_username'], network['rpc_password'], method, params)
+    return response['result']
+
+# Example usage
+if __name__ == '__main__':
+    # Connect to the Bitcoin network and get the balance of an address
+    network = BLOCKCHAIN_NETWORKS[0]
+    address = '1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2'
+    balance = get_balance(network, address)
+    print(f'Balance of address {address} on the {network["name"]} network: {balance}')
+
+    # Connect to the Ethereum network and send a transaction
+    network = BLOCKCHAIN_NETWORKS[1]
+    from_address = '0x742d35Cc6634C0532925a3b844Bc454e4438f44e'
+    to_address = '0x742d35Cc6634C0532925a3b844Bc454e4438f44e'
+    amount = 100000000000000000
+    tx_hash = send_transaction(network, from_address, to_address, amount)
+    print(f'Transaction hash on the {network["name"]} network: {tx_hash}')
