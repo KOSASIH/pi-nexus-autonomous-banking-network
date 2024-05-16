7b5cf21e43cc5aacc7a14bb9b910f5a1cff7b760
KOSASIH <kosasihg88@gmail.com>
2024-05-16 10:17:50 +0700

    Create consul.py

---
+++ core/consul/consul.py
@@ -0,0 +1,9 @@
+# consul.py
+import consul
+
+class ConsulConfig:
+    def __init__(self, url):
+        self.client = consul.Consul(url)
+
+    def get_config(self, key):
+        return self.client.kv.get(key)[1]['Value']
