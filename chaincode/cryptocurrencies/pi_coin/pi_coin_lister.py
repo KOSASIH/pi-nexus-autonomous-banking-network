7a1578c24fec98a984d009f700cd082d171a8246
KOSASIH <kosasihg88@gmail.com>
2024-05-16 16:08:55 +0700

    Update pi_coin_lister.py

--- chaincode/cryptocurrencies/pi_coin/pi_coin_lister.py
+++ chaincode/cryptocurrencies/pi_coin/pi_coin_lister.py
@@ -1,4 +1,3 @@
-# Import required libraries and frameworks
 import requests
 import json
 import pandas as pd
@@ -8,12 +7,14 @@ import os
 import threading
 from queue import Queue
 from sqlalchemy import create_engine
+import ccxt
 
 # Set API endpoints and credentials
 EXCHANGE_API_ENDPOINTS = {
-    'Binance': 'https://api.binance.com/api/v3/exchangeInfo',
-    'Coinbase': 'https://api.coinbase.com/v2/exchange-rates',
-    'Kraken': 'https://api.kraken.com/0/public/Ticker'
+    'Binance': 'https://api.binance.com',
+    'Coinbase': 'https://api.coinbase.com',
+    'Kraken': 'https://api.kraken.com',
+    'Indodax': 'https://api.indodax.com'
 }
 API_CREDENTIALS = {
     'Binance': {'api_key': os.environ['BINANCE_API_KEY'], 'api_secret': os.environ['BINANCE_API_SECRET']},
@@ -31,27 +32,41 @@ DB_ENGINE = create_engine('postgresql://user:password@host:port/dbname')
 # Function to fetch exchange data
 def fetch_exchange_data(exchange, queue):
     try:
-        endpoint = EXCHANGE_API_ENDPOINTS[exchange]
-        credentials = API_CREDENTIALS[exchange]
-        headers = {'X-MBX-APIKEY': credentials['api_key'], 'X-MBX-SECRET-KEY': credentials['api_secret']}
-        response = requests.get(endpoint, headers=headers, timeout=10)
-        response.raise_for_status()
-        queue.put((exchange, response.json()))
+        if exchange == 'Indodax':
+            exchange_obj = ccxt.indodax()
+            data = exchange_obj.fetch_ticker('PI_IDR')
+        else:
+            endpoint = EXCHANGE_API_ENDPOINTS[exchange]
+            credentials = API_CREDENTIALS[exchange]
+            headers = {'X-MBX-APIKEY': credentials['api_key'], 'X-MBX-SECRET-KEY': credentials['api_secret']}
+            response = requests.get(endpoint, headers=headers, timeout=10)
+            response.raise_for_status()
+            data = response.json()
+        queue.put((exchange, data))
     except requests.exceptions.RequestException as e:
         print(f'Error fetching data from {exchange}: {e}')
         queue.put((exchange, None))
 
 # Function to extract PI Coin data from exchange data
-def extract_pi_coin_data(exchange_data):
+def extract_pi_coin_data(exchange_data, exchange):
     pi_coin_data = {}
-    for symbol, info in exchange_data['symbols'].items():
-        if symbol == PI_COIN_SYMBOL:
-            pi_coin_data[exchange] = {
-                'symbol': symbol,
-                'price': info['price'],
-                'volume': info['volume'],
-                'listing_date': LISTING_DATE
-            }
+    if exchange == 'Indodax':
+        pi_coin_data[exchange] = {
+            'symbol': 'PI_IDR',
+            'price': exchange_data['last'],
+            'volume': exchange_data['volume'],
+            'listing_date': LISTING_DATE
+        }
+    else:
+        for symbol_info in exchange_data['symbols']:
+            if symbol_info['baseId'] == PI_COIN_SYMBOL:
+                pi_coin_data[exchange] = {
+                    'symbol': symbol_info['symbol'],
+                    'price': symbol_info['price'],
+                    'volume': symbol_info['volume'],
+                    'listing_date': LISTING_DATE
+                }
+                break
     return pi_coin_data
 
 # Function to list PI Coin on global exchanges
@@ -67,27 +82,7 @@ def list_pi_coin_on_exchanges(queue):
     while not queue.empty():
         exchange, data = queue.get()
         if data:
-            pi_coin_data = extract_pi_coin_data(data)
+            pi_coin_data = extract_pi_coin_data(data, exchange)
             if pi_coin_data:
                 pi_coin_list.append(pi_coin_data)
     return pi_coin_list
-
-# Function to store PI Coin data in database
-def store_pi_coin_data(pi_coin_list):
-    df = pd.DataFrame(pi_coin_list)
-    df.to_sql('pi_coin_list', con=DB_ENGINE, if_exists='replace', index=False)
-
-# Main function
-def main():
-    queue = Queue()
-    pi_coin_list = list_pi_coin_on_exchanges(queue)
-    if pi_coin_list:
-        print('PI Coin listed on the following exchanges:')
-        for exchange, pi_coin_data in pi_coin_list.items():
-            print(f'{exchange}: {pi_coin_data["symbol"]} - Price: {pi_coin_data["price"]} - Volume: {pi_coin_data["volume"]} - Listing Date: {pi_coin_data["listing_date"]}')
-        store_pi_coin_data(pi_coin_list)
-    else:
-        print('PI Coin not listed on any exchanges.')
-
-if __name__ == '__main__':
-    main()
