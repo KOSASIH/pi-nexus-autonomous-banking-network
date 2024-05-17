f133cbb4d57f14e85e7b2b75fd445b059ae2925f
KOSASIH <kosasihg88@gmail.com>
2024-05-17 11:26:12 +0700

    Create login.py

---
+++ web4/web4/modules/login.py
@@ -0,0 +1,13 @@
+# Login class for the web4 project
+
+class Login:
+    # Initialize the login object
+    def __init__(self, username, password):
+        # Initialize the username and password
+        self.username = username
+        self.password = password
+
+    # Authenticate the user login
+    def authenticate(self):
+        # Implement the logic for authenticating the user login
+        pass
