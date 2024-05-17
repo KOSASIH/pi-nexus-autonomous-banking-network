3fdedd5a1ca23cf0865e5a1b794a22dc69133fa9
KOSASIH <kosasihg88@gmail.com>
2024-05-17 11:25:35 +0700

    Create user_interface.py

---
+++ web4/web4/modules/user_interface.py
@@ -0,0 +1,12 @@
+# UserInterface class for the web4 project
+
+class UserInterface:
+    # Initialize the user interface object
+    def __init__(self):
+        # Initialize the list of messages to display to the user
+        self.messages = []
+
+    # Display real-time data about the car
+    def display_data(self, data):
+        # Implement the logic for displaying real-time data about the car
+        pass
