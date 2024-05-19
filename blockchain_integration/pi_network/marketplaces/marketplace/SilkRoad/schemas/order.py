bb7d41898c794a716602a635af0cd71637166ffd
KOSASIH <kosasihg88@gmail.com>
2024-05-19 11:32:01 +0700

    Create order.py

---
+++ blockchain_integration/pi_network/marketplaces/marketplace/SilkRoad/schemas/order.py
@@ -0,0 +1,21 @@
+# schemas/order.py
+
+from pydantic import BaseModel
+
+class OrderBase(BaseModel):
+    user_id: int
+    product_id: int
+    quantity: int
+    total_price: float
+
+class OrderCreate(OrderBase):
+    pass
+
+class OrderUpdate(OrderBase):
+    pass
+
+class Order(OrderBase):
+    id: int
+
+    class Config:
+        orm_mode = True
