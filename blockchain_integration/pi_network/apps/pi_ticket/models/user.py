b9492d9acb162ff85c215417b8bb71ebacd20eab
KOSASIH <kosasihg88@gmail.com>
2024-05-19 11:55:49 +0700

    Create user.py

---
+++ blockchain_integration/pi_network/apps/pi_ticket/models/user.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class User(models.Model):
+    """
+    User model
+    """
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ['username']
