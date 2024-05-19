6feab00aa6d20b75a5ee4550206195fe657e43ac
KOSASIH <kosasihg88@gmail.com>
2024-05-19 12:39:58 +0700

    Create permissions.py

---
+++ blockchain_integration/pi_network/apps/pi_ticket/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
+
+class IsAuthenticatedOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_authenticated
