51393120b88d702eca87662bbdc578a84407505f
KOSASIH <kosasihg88@gmail.com>
2024-05-19 12:02:40 +0700

    Create event_serializer.py

---
+++ blockchain_integration/pi_network/apps/pi_ticket/serializers/event_serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Event
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['id', 'event_name', 'event_location', 'event_date', 'description', 'image']
