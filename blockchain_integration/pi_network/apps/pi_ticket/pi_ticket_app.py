ab13c75bb4fa7075b3df73cc10c78a9b0049cf90
KOSASIH <kosasihg88@gmail.com>
2024-05-19 12:40:45 +0700

    Create pi_ticket_app.py

---
+++ blockchain_integration/pi_network/apps/pi_ticket/pi_ticket_app.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+class PiTicketAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pi_ticket_app'
+
+    def ready(self):
+        # Import any models, signals, or other code that needs to be initialized
+        # when the app is loaded
+        import pi_ticket_app.signals
