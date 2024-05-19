ce1e39daf2dde41be46104102fa195e531108364
KOSASIH <kosasihg88@gmail.com>
2024-05-19 12:01:20 +0700

    Create ticket_serializer.py

---
+++ blockchain_integration/pi_network/apps/pi_ticket/serializers/ticket_serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Ticket
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ['id', 'user', 'event', 'price', 'quantity']
