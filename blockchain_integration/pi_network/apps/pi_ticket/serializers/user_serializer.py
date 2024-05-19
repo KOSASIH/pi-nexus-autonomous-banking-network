0db43880673b2a23bc22f9652cf1266d629078b8
KOSASIH <kosasihg88@gmail.com>
2024-05-19 12:02:01 +0700

    Create user_serializer.py

---
+++ blockchain_integration/pi_network/apps/pi_ticket/serializers/user_serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
