8ea50188cfa971abcd13f58608ddd2d46b9e7bb7
KOSASIH <kosasihg88@gmail.com>
2024-05-19 11:19:44 +0700

    Create product.py

---
+++ blockchain_integration/pi_network/marketplaces/marketplace/SilkRoad/models/product.py
@@ -0,0 +1,19 @@
+# models/product.py
+
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+class Product(BaseModel):
+    id: int
+    marketplace_id: int
+    name: str
+    description: str
+    price: float
+    quantity: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
