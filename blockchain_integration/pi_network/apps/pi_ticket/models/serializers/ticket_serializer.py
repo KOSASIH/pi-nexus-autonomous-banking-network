323b160aa92d4f4affd5cc64c0cd2a6eee327701
KOSASIH <kosasihg88@gmail.com>
2024-05-19 12:00:08 +0700

    Delete blockchain_integration/pi_network/apps/pi_ticket/models/serializers directory

--- blockchain_integration/pi_network/apps/pi_ticket/models/serializers/ticket_serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Ticket
-
-class TicketSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ticket
-        fields = ['id', 'user', 'event', 'price', 'quantity']
