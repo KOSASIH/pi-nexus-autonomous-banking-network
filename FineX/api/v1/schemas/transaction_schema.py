aa96734625f712b29152ba95bd1c7462a1989e02
KOSASIH <kosasihg88@gmail.com>
2024-05-20 14:46:07 +0700

    Create transaction_schema.py

---
+++ FineX/api/v1/schemas/transaction_schema.py
@@ -0,0 +1,11 @@
+from marshmallow import fields, Schema
+
+class TransactionSchema(Schema):
+    """
+    A Transaction schema for the FineX project.
+    """
+    id = fields.Int(dump_only=True)
+    amount = fields.Float(required=True)
+    type = fields.Str(required=True)
+    timestamp = fields.DateTime(dump_only=True)
+    user_id = fields.Int(required=True)
