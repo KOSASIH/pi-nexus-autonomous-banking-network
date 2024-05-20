a0b47b2b037a0e05ae0aab4ed39e610a5db850f9
KOSASIH <kosasihg88@gmail.com>
2024-05-20 13:59:37 +0700

    Update verification.py

--- blockchain/verification.py
+++ blockchain/verification.py
@@ -1,17 +1,29 @@
 import hashlib
 
-def verify_transaction(sender, receiver, amount, timestamp, previous_hash):
-    transaction = {
-        'sender': sender,
-        'receiver': receiver,
-        'amount': amount,
-        'timestamp': timestamp,
-        'previous_hash': previous_hash
-    }
-    transaction_string = str(transaction)
-    transaction_hash = hashlib.sha256(transaction_string.encode()).hexdigest()
+def calculate_hash(block):
+    """Calculate the hash of a block"""
+    block_string = str(block)
+    return hashlib.sha256(block_string.encode()).hexdigest()
 
-    if transaction_hash == previous_hash:
-        return True
-    else:
-        return False
+def is_valid_proof(block, proof):
+    """Verify that a proof of work is valid"""
+    guess = f'{block["index"]}{block["previous_hash"]}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:4] == '0000'
+
+def verify_chain(chain):
+    """Verify that the chain is valid"""
+    for i in range(1, len(chain)):
+        current_block = chain[i]
+        previous_block = chain[i - 1]
+
+        if current_block['index'] != i:
+            return False
+
+        if current_block['previous_hash'] != calculate_hash(previous_block):
+            return False
+
+        if not is_valid_proof(current_block, current_block['proof']):
+            return False
+
+    return True
