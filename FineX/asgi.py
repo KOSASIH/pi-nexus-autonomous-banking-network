34dbcc0b0277e1f33c4df2808c7d3afe591af3ca
KOSASIH <kosasihg88@gmail.com>
2024-05-20 15:12:58 +0700

    Create asgi.py

---
+++ FineX/asgi.py
@@ -0,0 +1,6 @@
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '<your_project_name>.settings')
+
+application = get_asgi_application()
