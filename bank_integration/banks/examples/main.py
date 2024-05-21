72e662d133b93bf230439e930eda118a0092d30e
KOSASIH <kosasihg88@gmail.com>
2024-05-21 12:35:40 +0700

    Create main.py

---
+++ bank_integration/banks/examples/main.py
@@ -0,0 +1,21 @@
+# main.py
+import os
+import chase_bank
+import bank_of_america
+import capital_one
+
+api_key = os.getenv("API_KEY")
+
+chase = chase_bank.ChaseBank(api_key)
+boa = bank_of_america.BankOfAmerica(api_key)
+capitalone = capital_one.CapitalOne(api_key)
+
+chase_accounts = chase.get_accounts()
+boa_accounts = boa.get_accounts()
+capitalone_accounts = capitalone.get_accounts()
+
+chase_transactions = chase.get_transactions(chase_accounts[0]["id"])
+boa_transactions = boa.get_transactions(boa_accounts[0]["id"])
+capitalone_transactions = capitalone.get_transactions(capitalone_accounts[0]["id"])
+
+transfer_result = chase.transfer_funds(chase_accounts[0]["id"], boa_accounts[0]["id"], 100)
