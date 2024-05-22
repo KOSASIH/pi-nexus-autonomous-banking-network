7b58169aae289b143d2da93bc2bf7f32fc9bbdaa
KOSASIH <kosasihg88@gmail.com>
2024-05-22 19:47:37 +0700

    Update smart_contract_interface.sol

--- cross-chain-interoperability/smart_contract_interface.sol
+++ cross-chain-interoperability/smart_contract_interface.sol
@@ -1,2 +1,18 @@
 // smart_contract_interface.sol
 // A Solidity file to define a common interface for smart contracts across different chains
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface IBank {
+    // Function to get the current balance of an account
+    function getBalance(address account) external view returns (uint256);
+
+    // Function to deposit funds into an account
+    function deposit(address account, uint256 amount) external;
+
+    // Function to withdraw funds from an account
+    function withdraw(address account, uint256 amount) external;
+
+    // Function to transfer funds from one account to another
+    function transfer(address from, address to, uint256 amount) external;
+}
