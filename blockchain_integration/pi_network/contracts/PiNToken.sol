887dcd2fc7e5126967d5759485eeeea4e0ed89ea
KOSASIH <kosasihg88@gmail.com>
2024-05-23 13:23:50 +0700

    Create PiNToken.sol

---
+++ blockchain_integration/pi_network/contracts/PiNToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PiNToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("Pi Network Token", "PiN") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
