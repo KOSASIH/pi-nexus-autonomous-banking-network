055171ed0e160b35e05e5cbfde73238568961447
KOSASIH <kosasihg88@gmail.com>
2024-05-23 14:43:29 +0700

    Create pi-ethereum-adapter.js

---
+++ blockchain_integration/pi_network/contracts/PI-bank/pi-ethereum-adapter.js
@@ -0,0 +1,38 @@
+const Web3 = require('web3');
+const { ChainId, Token } = require('pi-bank-sdk');
+
+class PiEthereumAdapter {
+  constructor(ethereumUrl, piPrivateKey) {
+    this.ethereum = new Web3(ethereumUrl);
+    this.pi = new Token({
+      chainId: ChainId.PI_TESTNET,
+      privateKey: piPrivateKey,
+    });
+  }
+
+  async getPiBalance(piAddress) {
+    const ethBalance = await this.ethereum.eth.getBalance(piAddress);
+    const piBalance = await this.pi.getBalance(piAddress);
+    return { ethBalance, piBalance };
+  }
+
+  async sendPi(fromPiAddress, toPiAddress, amount) {
+    const txData = await this.pi.createSendTransaction(fromPiAddress, toPiAddress, amount);
+    const signedTx = await this.pi.signTransaction(txData);
+    const txHash = await this.pi.sendSignedTransaction(signedTx);
+    return txHash;
+  }
+
+  async sendEth(fromEthAddress, toEthAddress, amount) {
+    const txData = {
+      from: fromEthAddress,
+      to: toEthAddress,
+      value: amount,
+    };
+    const signedTx = await this.ethereum.eth.accounts.signTransaction(txData, fromEthAddress);
+    const txHash = await this.ethereum.eth.sendSignedTransaction(signedTx.rawTransaction);
+    return txHash;
+  }
+}
+
+module.exports = PiEthereumAdapter;
