1db21a9d4a1c612b69c0a71312bf42159d0738df
KOSASIH <kosasihg88@gmail.com>
2024-05-23 16:08:19 +0700

    Create PiToEthBridgeUtils.js

---
+++ utils/PiToEthBridgeUtils.js
@@ -0,0 +1,22 @@
+const Web3 = require('web3');
+
+class PiToEthBridgeUtils {
+
+    static getPiToEthereumRate(web3, contractAddress) {
+        const contract = new web3.eth.Contract(PiToEthBridge.abi, contractAddress);
+        return contract.methods.conversionRate().call();
+    }
+
+    static getPiTokenAddress(web3, contractAddress) {
+        const contract = new web3.eth.Contract(PiToEthBridge.abi, contractAddress);
+        return contract.methods.piNetworkContract().call();
+    }
+
+    static getEthereumTokenAddress(web3, contractAddress) {
+        const contract = new web3.eth.Contract(PiToEthBridge.abi, contractAddress);
+        return contract.methods.ethereumContract().call();
+    }
+
+}
+
+module.exports = PiToEthBridgeUtils;
