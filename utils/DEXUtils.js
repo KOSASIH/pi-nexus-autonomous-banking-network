3d2a4b63053ed2658b6f6b2624da6729838d52cc
KOSASIH <kosasihg88@gmail.com>
2024-05-23 16:25:04 +0700

    Create DEXUtils.js

---
+++ utils/DEXUtils.js
@@ -0,0 +1,17 @@
+class DEXUtils {
+
+    static calculatePrice(amount, reserve) {
+        return (amount * 100) / reserve;
+    }
+
+    static calculateReserve(amount, price) {
+        return (amount * price) / 100;
+    }
+
+    static calculateFee(amount, fee) {
+        return (amount * fee) / 100;
+    }
+
+}
+
+module.exports = DEXUtils;
