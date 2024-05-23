3f101dde28edd9add32079c9383c0db775ad1dc5
KOSASIH <kosasihg88@gmail.com>
2024-05-23 13:39:38 +0700

    Create deploy_contracts.js

---
+++ blockchain_integration/pi_network/scripts/deploy_contracts.js
@@ -0,0 +1,17 @@
+const { deployments, ethers } = require("hardhat");
+
+async function deployContracts() {
+  await deployments.fixture(["PiNToken", "PiNetworkBridge"]);
+  const piNToken = await ethers.getContract("PiNToken");
+  const piNetworkBridge = await ethers.getContract("PiNetworkBridge");
+
+  console.log("PiNToken address:", piNToken.address);
+  console.log("PiNetworkBridge address:", piNetworkBridge.address);
+}
+
+deployContracts()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
