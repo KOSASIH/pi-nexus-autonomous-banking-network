f6a944bafaa74319d1fad6d80ae8aef2f94f56e0
KOSASIH <kosasihg88@gmail.com>
2024-05-23 13:35:42 +0700

    Create test_helper.js

---
+++ blockchain_integration/pi_network/tests/test_helper.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require("dotenv").config();
+
+module.exports = {
+  networks: {
+    development: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:8545/"),
+      network_id: 1337,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
+};
