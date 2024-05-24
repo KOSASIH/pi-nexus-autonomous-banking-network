93f9eefb8f3f5b9871301b645ae03977e1dc5320
KOSASIH <kosasihg88@gmail.com>
2024-05-24 09:29:44 +0700

    Create LoginScreen.js

---
+++ mobile-app/screens/LoginScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import { Login } from '../components';
+
+const LoginScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Image source={require('../images/fingerprint.png')} />
+      <Text>Welcome to the most advanced high-tech mobile app!</Text>
+      <Login navigation={navigation} />
+    </View>
+  );
+};
+
+export default LoginScreen;
