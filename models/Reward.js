191ad0b5bcba459a7d731fea1505c66a37c418fe
KOSASIH <kosasihg88@gmail.com>
2024-05-24 11:05:56 +0700

    Create Reward.js

---
+++ models/Reward.js
@@ -0,0 +1,27 @@
+// models/Reward.js
+const mongoose = require('mongoose');
+
+const rewardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  pointsRequired: {
+    type: Number,
+    required: true,
+  },
+  discount: {
+    type: Number,
+    required: true,
+  },
+  partnerMerchant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'PartnerMerchant',
+  },
+});
+
+module.exports = mongoose.model('Reward', rewardSchema);
