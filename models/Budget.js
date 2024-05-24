e33cf47eb5f9e015c821561a6e96c7ef52ec2eb4
KOSASIH <kosasihg88@gmail.com>
2024-05-24 11:14:24 +0700

    Create Budget.js

---
+++ models/Budget.js
@@ -0,0 +1,31 @@
+// models/Budget.js
+const mongoose = require('mongoose');
+
+const budgetSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  income: {
+    type: Number,
+    required: true,
+  },
+  expenses: [
+    {
+      category: {
+        type: String,
+        required: true,
+      },
+      amount: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  forecast: {
+    type: Object,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Budget', budgetSchema);
