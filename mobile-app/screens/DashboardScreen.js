780bfb56a3d04ed82d268b4ecdf4f299ac301e01
KOSASIH <kosasihg88@gmail.com>
2024-05-24 09:30:17 +0700

    Create DashboardScreen.js

---
+++ mobile-app/screens/DashboardScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Dashboard } from '../components';
+
+const DashboardScreen = () => {
+  return (
+    <View>
+      <Text>Welcome to the most advanced high-tech dashboard!</Text>
+      <Dashboard />
+    </View>
+  );
+};
+
+export default DashboardScreen;
