3fe72d5f6acf36dd718e398b3cdd450fab90253d
KOSASIH <kosasihg88@gmail.com>
2024-05-24 10:17:19 +0700

    Create PushNotification.js

---
+++ services/PushNotification.js
@@ -0,0 +1,22 @@
+// services/PushNotification.js
+const Expo = require('expo-server-sdk');
+
+const pushNotification = async (user, message) => {
+  const expo = new Expo();
+  const tokens = await getPushTokensForUser(user);
+  const notifications = tokens.map((token) => ({
+    to: token,
+    sound: 'default',
+    title: 'Transaction Alert',
+    body: message,
+  }));
+  await expo.sendPushNotificationsAsync(notifications);
+};
+
+const getPushTokensForUser = async (user) => {
+  // Implement logic to retrieve push tokens for the user
+  // For example, using a database or a token storage service
+  return ['ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]'];
+};
+
+module.exports = { sendNotification: pushNotification };
