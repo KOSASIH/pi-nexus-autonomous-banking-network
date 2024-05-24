22d3f01595fbbf5590ac8d95e2d0570427c80801
KOSASIH <kosasihg88@gmail.com>
2024-05-24 11:17:21 +0700

    Create BudgetController.js

---
+++ controllers/BudgetController.js
@@ -0,0 +1,17 @@
+// controllers/BudgetController.js
+const BudgetService = require('../services/BudgetService');
+const Budget = require('../models/Budget');
+
+const createBudget = async (req, res) => {
+  const budget = new Budget(req.body);
+  await budget.save();
+  res.send({ message: 'Budget created successfully' });
+};
+
+const getBudgetRecommendations = async (req, res) => {
+  const budget = await Budget.findById(req.params.id);
+  const recommendations = await BudgetService.generateBudgetRecommendations(budget);
+  res.send(recommendations);
+};
+
+module.exports = { createBudget, getBudgetRecommendations };
