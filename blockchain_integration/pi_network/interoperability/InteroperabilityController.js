772d5e45a24a6b20154eb76ba6d568fd4d1dff25
KOSASIH <kosasihg88@gmail.com>
2024-05-25 08:00:35 +0700

    Update InteroperabilityController.js

--- blockchain_integration/pi_network/interoperability/InteroperabilityController.js
+++ blockchain_integration/pi_network/interoperability/InteroperabilityController.js
@@ -1,8 +1,8 @@
 class InteroperabilityController {
-  constructor(web3, polkadot, wallet) {
+  constructor(web3, ethers, polkadot) {
     this.web3 = web3;
+    this.ethers = ethers;
     this.polkadot = polkadot;
-    this.wallet = wallet;
   }
 
   async sendTransactionToPolkadot(transaction) {
@@ -16,19 +16,46 @@ class InteroperabilityController {
     return result;
   }
 
+  async sendTransactionToEthereum(transaction) {
+    // Convert the Polkadot transaction to an Ethereum transaction
+    const ethereumTransaction = this.convertTransaction(transaction);
+
+    // Send the transaction to the Ethereum network
+    const result = await this.web3.eth.sendTransaction(ethereumTransaction);
+
+    // Return the result of the transaction
+    return result;
+  }
+
+  async getBalanceOnPolkadot(address) {
+    // Get the balance of the address on the Polkadot network
+    const balance = await this.polkadot.getBalance(address);
+
+    // Return the balance
+    return balance;
+  }
+
+  async getBalanceOnEthereum(address) {
+    // Get the balance of the address on the Ethereum network
+    const balance = await this.web3.eth.getBalance(address);
+
+    // Return the balance
+    return balance;
+  }
+
   convertTransaction(transaction) {
-    // Convert the Ethereum transaction to a Polkadot transaction
-    // This will depend on the specific details of the transaction and the Polkadot network
+    // Convert the transaction to the desired format
+    // This will depend on the specific details of the transaction and the networks
     // You will need to implement this function based on the requirements of your project
     // For example, you may need to convert the transaction data, gas limit, gas price, etc.
     // You may also need to sign the transaction using the wallet's private key
     // This is just a placeholder implementation
     return {
       ...transaction,
-      from: this.wallet.address,
+      from: this.ethers.Wallet.address,
       gasLimit: '0x5208',
       gasPrice: '0x3b9aca00',
-      nonce: this.web3.eth.getTransactionCount(this.wallet.address),
+      nonce: this.web3.eth.getTransactionCount(this.ethers.Wallet.address),
       value: this.web3.utils.toWei(transaction.value, 'ether'),
     };
   }
