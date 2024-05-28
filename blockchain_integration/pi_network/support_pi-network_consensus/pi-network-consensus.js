a6991cd874d742121a47c1f8260c8c7094465e35
KOSASIH <kosasihg88@gmail.com>
2024-05-28 10:10:03 +0700

    Create pi-network-consensus.js

---
+++ blockchain_integration/pi_network/support_pi-network_consensus/pi-network-consensus.js
@@ -0,0 +1,42 @@
+// Import the necessary libraries
+const PiNetwork = require('pi-network-javascript');
+const express = require('express');
+const app = express();
+
+// Set up the Pi Network API connection
+const piNetwork = new PiNetwork({
+  network: 'mainnet', // or 'testnet'
+  apiKey: 'YOUR_API_KEY'
+});
+
+// Create a list of trusted nodes
+const trustedNodes = [
+  'node1.example.com',
+  'node2.example.com',
+  'node3.example.com'
+];
+
+// Implement the SCP algorithm
+async function participateInConsensus() {
+  // Create a list of quorum slices
+  const quorumSlices = trustedNodes.map(node => ({
+    validators: [node],
+    value: 1.0
+  }));
+
+  // Participate in the consensus process
+  const result = await piNetwork.consensus.propose({
+    quorumSlices,
+    value: 1.0
+  });
+
+  console.log('Consensus result:', result);
+}
+
+// Run the consensus algorithm every 10 seconds
+setInterval(participateInConsensus, 10000);
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
