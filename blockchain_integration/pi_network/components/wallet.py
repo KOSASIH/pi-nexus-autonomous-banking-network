23eee98f60c4a48542cf9ece349bb70bfd092047
KOSASIH <kosasihg88@gmail.com>
2024-05-28 08:15:45 +0700

    Create wallet.py

---
+++ blockchain_integration/pi_network/components/wallet.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+class Wallet:
+    def __init__(self, api_client):
+        self.api_client = api_client
+
+    def get_balance(self, address: str) -> Optional[float]:
+        # Implement Pi Network wallet API call to get balance
+        pass
+
+    def send_transaction(self, sender: str, receiver: str, amount: float) -> Optional[Transaction]:
+        # Implement Pi Network wallet API call to send transaction
+        pass
