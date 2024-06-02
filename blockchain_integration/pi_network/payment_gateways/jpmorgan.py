e225365f0181d4f2ae96dc441e50dd95f731b110
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:49:12 +0700

    Create jpmorgan.py

---
+++ blockchain_integration/pi_network/payment_gateways/jpmorgan.py
@@ -0,0 +1,14 @@
+import jpmorgan
+
+class JPMorganPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        jpmorgan.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = jpmorgan.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
