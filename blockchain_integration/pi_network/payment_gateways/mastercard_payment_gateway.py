34dbc76681358fa9fe112f3fc2e6404f6f191c21
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:44:26 +0700

    Create mastercard_payment_gateway.py

---
+++ blockchain_integration/pi_network/payment_gateways/mastercard_payment_gateway.py
@@ -0,0 +1,14 @@
+import mastercard_payment_gateway
+
+class MastercardPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        mastercard_payment_gateway.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = mastercard_payment_gateway.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
