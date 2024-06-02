19f6d6ad41935bcbc5392e1d9c2f89bc6c403386
KOSASIH <kosasihg88@gmail.com>
2024-06-02 15:00:45 +0700

    Create lloyds_bank.py

---
+++ blockchain_integration/pi_network/payment_gateways/lloyds_bank.py
@@ -0,0 +1,14 @@
+import lloyds_bank
+
+class LloydsBankPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        lloyds_bank.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = lloyds_bank.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
