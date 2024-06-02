d3394566137746cb196bdfb5a4c3892e75adb12c
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:42:26 +0700

    Create microsoft_pay.py

---
+++ blockchain_integration/pi_network/payment_gateways/microsoft_pay.py
@@ -0,0 +1,14 @@
+import microsoft_pay
+
+class MicrosoftPayPaymentGateway:
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        microsoft_pay.Configuration.configure(client_id, client_secret)
+
+    def create_payment(self, amount, currency):
+        payment = microsoft_pay.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
