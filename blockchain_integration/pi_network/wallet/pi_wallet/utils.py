54050e70cabe312755d569106f9d2b28a9a13ee9
KOSASIH <kosasihg88@gmail.com>
2024-06-02 17:41:58 +0700

    Create utils.py

---
+++ blockchain_integration/pi_network/wallet/pi_wallet/utils.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_data(data, title):
+    plt.figure(figsize=(12, 6))
+    plt.plot(data.index, data['close'])
+    plt.title(title)
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.show()
+
+def calculate_portfolio_performance(data):
+    portfolio_return = data['return'].mean() * 252
+    portfolio_volatility = data['return'].std() * np.sqrt(252)
+    sharpe_ratio = portfolio_return / portfolio_volatility
+    return portfolio_return, portfolio_volatility, sharpe_ratio
+
+def print_portfolio_performance(portfolio_return, portfolio_volatility, sharpe_ratio):
+    print("Portfolio Return: {:.2f}%".format(portfolio_return * 100))
+    print("Portfolio Volatility: {:.2f}%".format(portfolio_volatility * 100))
+    print("Sharpe Ratio: {:.2f}".format(sharpe_ratio))
