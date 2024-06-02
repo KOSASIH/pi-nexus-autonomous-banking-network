98106e7157a9b477de54933f8b7e2fd8fb6cf74f
KOSASIH <kosasihg88@gmail.com>
2024-06-02 18:33:37 +0700

    Create node_registry.py

---
+++ blockchain_integration/pi_network/node_management/node_registry.py
@@ -0,0 +1,15 @@
+class NodeRegistry:
+    def __init__(self):
+        self.nodes = {}
+
+    def add_node(self, node):
+        self.nodes[node.node_id] = node
+
+    def get_node(self, node_id):
+        return self.nodes.get(node_id)
+
+    def remove_node(self, node):
+        del self.nodes[node.node_id]
+
+# Example usage
+node_registry = NodeRegistry()
