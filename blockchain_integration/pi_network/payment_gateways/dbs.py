3b04449337ae915c1b4be9a511ccd5d3bc9fe2b2
KOSASIH <kosasihg88@gmail.com>
2024-06-02 15:05:49 +0700

    Create dbs.py

---
+++ blockchain_integration/pi_network/payment_gateways/dbs.py
@@ -0,0 +1,14 @@
+import dbs
+
+class DBSPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        dbs.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = dbs.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
