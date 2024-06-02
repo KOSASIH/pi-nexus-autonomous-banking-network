7f4a4a88344060f1b0aae87aee3dc038318371dd
KOSASIH <kosasihg88@gmail.com>
2024-06-02 15:03:13 +0700

    Create rabobank.py

---
+++ blockchain_integration/pi_network/payment_gateways/rabobank.py
@@ -0,0 +1,14 @@
+import rabobank
+
+class RabobankPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        rabobank.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = rabobank.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
