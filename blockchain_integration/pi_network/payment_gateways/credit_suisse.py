00657dcd52b1cf007393a8a074a30638556308b5
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:52:45 +0700

    Create credit_suisse.py

---
+++ blockchain_integration/pi_network/payment_gateways/credit_suisse.py
@@ -0,0 +1,14 @@
+import credit_suisse
+
+class CreditSuissePaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        credit_suisse.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = credit_suisse.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
