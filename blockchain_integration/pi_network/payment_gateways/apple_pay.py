1cfd37a5a0b8a3a7bfa2286f10123706485b645a
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:39:22 +0700

    Create apple_pay.py

---
+++ blockchain_integration/pi_network/payment_gateways/apple_pay.py
@@ -0,0 +1,14 @@
+import apple_pay
+
+class ApplePayPaymentGateway:
+    def __init__(self, merchant_id, public_key):
+        self.merchant_id = merchant_id
+        self.public_key = public_key
+        apple_pay.Configuration.configure(merchant_id, public_key)
+
+    def create_payment(self, amount, currency):
+        payment = apple_pay.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
