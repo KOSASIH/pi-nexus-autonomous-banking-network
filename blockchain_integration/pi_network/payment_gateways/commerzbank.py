fb8674efe698fdacdbe5267560e8f2af513b4399
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:57:27 +0700

    Create commerzbank.py

---
+++ blockchain_integration/pi_network/payment_gateways/commerzbank.py
@@ -0,0 +1,14 @@
+import commerzbank
+
+class CommerzbankPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        commerzbank.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = commerzbank.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
