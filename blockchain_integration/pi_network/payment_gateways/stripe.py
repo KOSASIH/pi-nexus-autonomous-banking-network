a686762153bfe45a0c2aa9fc2946f274c8e0dc7f
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:32:36 +0700

    Create stripe.py

---
+++ blockchain_integration/pi_network/payment_gateways/stripe.py
@@ -0,0 +1,14 @@
+import stripe
+
+class StripePaymentGateway:
+    def __init__(self, secret_key):
+        self.secret_key = secret_key
+        stripe.api_key = secret_key
+
+    def create_charge(self, amount, currency, source):
+        charge = stripe.Charge.create(
+            amount=int(amount * 100),
+            currency=currency,
+            source=source
+        )
+        return charge
