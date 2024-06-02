d6aa1a734c53ffabe8980c3942a031777175fb38
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:53:17 +0700

    Create deutsche_bank.py

---
+++ blockchain_integration/pi_network/payment_gateways/deutsche_bank.py
@@ -0,0 +1,14 @@
+import deutsche_bank
+
+class DeutscheBankPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        deutsche_bank.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = deutsche_bank.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
