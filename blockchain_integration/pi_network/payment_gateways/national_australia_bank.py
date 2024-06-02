0a6002628614f1cd942189fd45170b5628e8daaa
KOSASIH <kosasihg88@gmail.com>
2024-06-02 15:07:57 +0700

    Create national_australia_bank.py

---
+++ blockchain_integration/pi_network/payment_gateways/national_australia_bank.py
@@ -0,0 +1,14 @@
+import national_australia_bank
+
+class NationalAustraliaBankPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        national_australia_bank.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = national_australia_bank.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
