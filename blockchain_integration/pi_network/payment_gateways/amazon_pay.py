8f0a65cd9a477dbdf3f9b17cba7470bef08e4ff3
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:41:52 +0700

    Create amazon_pay.py

---
+++ blockchain_integration/pi_network/payment_gateways/amazon_pay.py
@@ -0,0 +1,15 @@
+import amazon_pay
+
+class AmazonPayPaymentGateway:
+    def __init__(self, seller_id, client_id, client_secret):
+        self.seller_id = seller_id
+        self.client_id = client_id
+        self.client_secret = client_secret
+        amazon_pay.Configuration.configure(seller_id, client_id, client_secret)
+
+    def create_payment(self, amount, currency):
+        payment = amazon_pay.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
