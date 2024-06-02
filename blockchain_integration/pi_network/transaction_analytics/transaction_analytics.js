e6d44f7587785dc8e69c9c89394813509c72669b
KOSASIH <kosasihg88@gmail.com>
2024-06-02 18:15:33 +0700

    Create transaction_analytics.js

---
+++ blockchain_integration/pi_network/transaction_analytics/transaction_analytics.js
@@ -0,0 +1,19 @@
+// transaction_analytics.js
+const { Kafka } = require('kafka-node');
+
+const kafka = new Kafka({
+  clientId: 'pi-network',
+  brokers: ['localhost:9092'],
+});
+
+async function processTransaction(transaction) {
+  // Process transaction data
+  const analyticsData = await processTransactionData(transaction);
+  // Send analytics data to Kafka topic
+  kafka.producer.send([
+    {
+      topic: 'pi-network-analytics',
+      messages: [analyticsData],
+    },
+  ]);
+}
