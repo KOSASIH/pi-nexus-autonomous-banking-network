f4c79e238a8d26656934fa13a3e8b7b3f4a1c192
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:36:53 +0700

    Create authorize_net.py

---
+++ blockchain_integration/pi_network/payment_gateways/authorize_net.py
@@ -0,0 +1,14 @@
+import authorize
+
+class AuthorizeNetPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        authorize.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = authorize.Payment()
+        payment.amount = amount
+        payment.currency = currency
+        payment.create()
+        return payment
