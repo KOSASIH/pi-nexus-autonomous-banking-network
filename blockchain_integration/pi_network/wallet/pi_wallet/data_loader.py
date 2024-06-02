55c7a00d37c89ae2216ec36dcdf1918b49ea226f
KOSASIH <kosasihg88@gmail.com>
2024-06-02 17:40:54 +0700

    Create data_loader.py

---
+++ blockchain_integration/pi_network/wallet/pi_wallet/data_loader.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import yfinance as yf
+
+class DataLoader:
+    def __init__(self, ticker, start_date, end_date):
+        self.ticker = ticker
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def load_data(self):
+        data = yf.download(self.ticker, start=self.start_date, end=self.end_date)
+        data.reset_index(inplace=True)
+        data.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
+        return data
+
+    def preprocess_data(self, data):
+        data['return'] = data['close'].pct_change()
+        data['log_return'] = np.log(data['close'] / data['close'].shift(1))
+        data.dropna(inplace=True)
+        return data
+
+    def split_data(self, data):
+        train_data, test_data = data.split(test_size=0.2, random_state=42)
+        return train_data, test_data
