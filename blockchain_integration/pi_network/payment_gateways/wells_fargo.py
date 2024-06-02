9eef4f064f750f30ed46129507a20f535b0ced77
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:50:50 +0700

    Create wells_fargo.py

---
+++ blockchain_integration/pi_network/payment_gateways/wells_fargo.py
@@ -0,0 +1,14 @@
+import wells_fargo
+
+class WellsFargoPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        wells_fargo.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = wells_fargo.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
