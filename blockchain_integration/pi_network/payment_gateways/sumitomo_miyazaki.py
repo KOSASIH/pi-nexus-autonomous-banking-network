d03f85a1251d24be6ff9745b3b532e6db28744a7
KOSASIH <kosasihg88@gmail.com>
2024-06-02 15:09:02 +0700

    Create sumitomo_miyazaki.py

---
+++ blockchain_integration/pi_network/payment_gateways/sumitomo_miyazaki.py
@@ -0,0 +1,14 @@
+import sumitomo_miyazaki
+
+class SumitomoMiyazakiPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        sumitomo_miyazaki.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = sumitomo_miyazaki.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
