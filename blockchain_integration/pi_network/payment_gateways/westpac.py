3947b44c05090de4ab0a36354d3e6d61d5e29ac7
KOSASIH <kosasihg88@gmail.com>
2024-06-02 15:09:46 +0700

    Create westpac.py

---
+++ blockchain_integration/pi_network/payment_gateways/westpac.py
@@ -0,0 +1,14 @@
+import westpac
+
+class WestpacPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        westpac.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = westpac.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
