92bebce64432dd5066c6a1e3ca3f6a4bbd3d5d32
KOSASIH <kosasihg88@gmail.com>
2024-06-02 15:06:55 +0700

    Create mizuho.py

---
+++ blockchain_integration/pi_network/payment_gateways/mizuho.py
@@ -0,0 +1,14 @@
+import mizuho
+
+class MizuhoPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        mizuho.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = mizuho.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
