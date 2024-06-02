7c856e63631da35723cb8cdaaff14cf7a0505db6
KOSASIH <kosasihg88@gmail.com>
2024-06-02 18:14:44 +0700

    Create decentralized_identity_verification.js

---
+++ blockchain_integration/pi_network/decentralized_identity_verification/decentralized_identity_verification.js
@@ -0,0 +1,15 @@
+// decentralized_identity_verification.js
+const { IdentityManager } = require('erc-725');
+
+const identityManager = new IdentityManager();
+
+async function verifyIdentity(userUid, metadata) {
+  const identity = await identityManager.getIdentity(userUid);
+  if (identity) {
+    // Verify identity using metadata
+    const isValid = await identityManager.verifyIdentity(identity, metadata);
+    return isValid;
+  } else {
+    return false;
+  }
+}
