ab9df6247bfe883841a48981847c89690e89bfa4
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:38:15 +0700

    Create coinbase.py

---
+++ blockchain_integration/pi_network/payment_gateways/coinbase.py
@@ -0,0 +1,14 @@
+import coinbase
+
+class CoinbasePaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        coinbase.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = coinbase.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
