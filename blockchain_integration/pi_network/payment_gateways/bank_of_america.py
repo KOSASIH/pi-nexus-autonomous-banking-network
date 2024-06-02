d68dc740d68ad269f5d0dfa59b5c4cc0ae715960
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:50:16 +0700

    Create bank_of_america.py

---
+++ blockchain_integration/pi_network/payment_gateways/bank_of_america.py
@@ -0,0 +1,14 @@
+import bank_of_america
+
+class BankOfAmericaPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        bank_of_america.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = bank_of_america.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
