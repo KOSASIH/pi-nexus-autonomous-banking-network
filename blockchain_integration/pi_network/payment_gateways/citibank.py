6c18dc564caadb85894ff1f9864e5623df0ac9dc
KOSASIH <kosasihg88@gmail.com>
2024-06-02 14:49:43 +0700

    Create citibank.py

---
+++ blockchain_integration/pi_network/payment_gateways/citibank.py
@@ -0,0 +1,14 @@
+import citibank
+
+class CitibankPaymentGateway:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        citibank.Configuration.configure(api_key, api_secret)
+
+    def create_payment(self, amount, currency):
+        payment = citibank.Payment.create({
+            'amount': amount,
+            'currency': currency
+        })
+        return payment
