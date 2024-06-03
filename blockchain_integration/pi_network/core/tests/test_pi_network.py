2a929fa7078c94c6d38b366e575319316df92a9a
KOSASIH <kosasihg88@gmail.com>
2024-06-03 12:53:38 +0700

    Create test_pi_network.py

---
+++ blockchain_integration/pi_network/core/tests/test_pi_network.py
@@ -0,0 +1,12 @@
+# test_pi_network.py
+import unittest
+from pi_network.core.pi_network import PiNetwork
+
+class TestPiNetwork(unittest.TestCase):
+    def test_start(self):
+        # Test the start method
+        pass
+
+    def test_stop(self):
+        # Test the stop method
+        pass
