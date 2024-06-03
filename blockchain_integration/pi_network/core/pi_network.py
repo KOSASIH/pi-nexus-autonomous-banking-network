7c63da8b5b285961ee17a4b3621674ea80aa5546
KOSASIH <kosasihg88@gmail.com>
2024-06-03 12:58:18 +0700

    Update pi_network.py

--- blockchain_integration/pi_network/core/pi_network.py
+++ blockchain_integration/pi_network/core/pi_network.py
@@ -8,6 +8,7 @@ class PiNetwork:
         self.config = config
         self.logger = Logger()
         self.error_handler = ErrorHandler()
+        self.cache = {}  # Add a cache to improve performance
 
     def start(self):
         # Initialize the Pi network
@@ -16,3 +17,16 @@ class PiNetwork:
     def stop(self):
         # Stop the Pi network
         pass
+
+    def get_transaction(self, transaction_id):
+        # Check if the transaction is in the cache
+        if transaction_id in self.cache:
+            return self.cache[transaction_id]
+
+        # Fetch the transaction from the blockchain
+        transaction = self.fetch_transaction_from_blockchain(transaction_id)
+
+        # Cache the transaction
+        self.cache[transaction_id] = transaction
+
+        return transaction
