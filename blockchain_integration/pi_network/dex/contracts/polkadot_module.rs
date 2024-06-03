6c5bb55ce54d7ae260db28887d4e7078cafe3d9a
KOSASIH <kosasihg88@gmail.com>
2024-06-03 18:45:41 +0700

    Create polkadot_module.rs

---
+++ blockchain_integration/pi_network/dex/contracts/polkadot_module.rs
@@ -0,0 +1,39 @@
+use substrate::{decl_module, decl_storage, decl_event, ensure};
+use substrate::traits::{OnInitialize, OnFinalize};
+use substrate::storage::{StorageValue, StorageMap};
+use substrate::dispatch::{DispatchResult, DispatchError};
+
+pub trait Trait: substrate::Trait {
+    type Event: From<Event<Self>> + Into<<Self as substrate::Trait>::Event>;
+}
+
+decl_module! {
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        fn deposit(origin, amount: u128) -> DispatchResult {
+            ensure!(origin == T::Origin::signed(T::AccountId::from([1; 32])), "Only the relay chain can deposit");
+            let who = T::AccountId::from([1; 32]);
+            <balances::Module<T>>::inc_balance(&who, amount);
+            Ok(())
+        }
+
+        fn withdraw(origin, amount: u128) -> DispatchResult {
+            ensure!(origin == T::Origin::signed(T::AccountId::from([1; 32])), "Only the relay chain can withdraw");
+            let who = T::AccountId::from([1; 32]);
+            <balances::Module<T>>::dec_balance(&who, amount);
+            Ok(())
+        }
+    }
+}
+
+decl_storage! {
+    trait Store for Module<T: Trait> as PolkadotStorage {
+        get balance: map [T::AccountId; u128];
+    }
+}
+
+decl_event! {
+    pub enum Event<T> where AccountId = <T as substrate::Trait>::AccountId {
+        Deposit(AccountId, u128),
+        Withdrawal(AccountId, u128),
+    }
+      }
