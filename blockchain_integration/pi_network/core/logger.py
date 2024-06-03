3c89506bb92a1ccd17ed519adb017e62d722f627
KOSASIH <kosasihg88@gmail.com>
2024-06-03 12:52:17 +0700

    Create logger.py

---
+++ blockchain_integration/pi_network/core/logger.py
@@ -0,0 +1,12 @@
+# logger.py
+import logging
+
+class Logger:
+    def __init__(self):
+        self.logger = logging.getLogger('pi_network')
+        self.logger.setLevel(logging.INFO)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
