8bbba9e45ed9a0aa3b8c6fe93e23e427ad0114e8
KOSASIH <kosasihg88@gmail.com>
2024-06-03 21:11:17 +0700

    Create IbcTransfer.go

---
+++ blockchain_integration/pi_network/DeFi/smart_contracts/IbcTransfer.go
@@ -0,0 +1,24 @@
+package xxx
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	ibc "github.com/cosmos/ibc-go (
+            uint80 roundID, 
+            int256 price,
+            uint256 startedAt,
+            uint256 updatedAt/modules/core/24-hour-fee"
+	"github.com/cosmos/ibc-go/modules/core/24-hour-fee/types"
+)
+
+const (
+	ModuleName = "ibc-transfer"
+	StoreKey   = ModuleName
+	RouterKey  = ModuleName
+),
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
