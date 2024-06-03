60a707bd4460f6becb6cf2bbd27c6ab8d7b821f9
KOSASIH <kosasihg88@gmail.com>
2024-06-03 20:58:11 +0700

    Create MyToken.sol

---
+++ blockchain_integration/pi_network/DeFi/MyToken.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.0;
+
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient funds");
+        require(_to != address(0), "Invalid address");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        return true;
+    }
+}
