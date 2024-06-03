565b85e1d74a6b00d02bfca2412ad880ce9759bf
KOSASIH <kosasihg88@gmail.com>
2024-06-03 12:51:40 +0700

    Create config.py

---
+++ blockchain_integration/pi_network/core/config.py
@@ -0,0 +1,8 @@
+# config.py
+import os
+
+class Config:
+    def __init__(self):
+        self.pi_network_port = int(os.environ.get('PI_NETWORK_PORT', 8080))
+        self.pi_network_host = os.environ.get('PI_NETWORK_HOST', 'localhost')
+        self.blockchain_node_url = os.environ.get('BLOCKCHAIN_NODE_URL', 'https://example.com/blockchain')
