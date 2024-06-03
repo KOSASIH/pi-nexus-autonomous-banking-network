13ffd0a7dbc01533ac37b6f2b486f4834c9251ca
KOSASIH <kosasihg88@gmail.com>
2024-06-03 17:06:31 +0700

    Create pi_network_smart_contract_interaction.py

---
+++ blockchain_integration/pi_network/pi_network_smart_contract_interaction.py
@@ -0,0 +1,12 @@
+from pi_network_blockchain_interface import BlockchainInterface
+
+class SmartContractInteraction:
+    def __init__(self, blockchain_interface, contract_address, abi):
+        self.blockchain_interface = blockchain_interface
+        self.contract = self.blockchain_interface.web3.eth.contract(address=contract_address, abi=abi)
+
+    def call_contract_function(self, function_name, *args):
+        return self.contract.functions[function_name](*args).call()
+
+    def send_transaction_to_contract(self, function_name, *args, transaction_value=0):
+        return self.contract.functions[function_name](*args).transact({'value': transaction_value})
