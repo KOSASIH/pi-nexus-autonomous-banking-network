a2683e6ffd629834ad477feb7c27dbf141fe5327
KOSASIH <kosasihg88@gmail.com>
2024-06-04 16:42:49 +0700

    Create pi_transaction.py

---
+++ blockchain_integration/pi_network/wallet/pi_wallet/models/pi_transaction.py
@@ -0,0 +1,14 @@
+class PiTransaction:
+    def __init__(self, sender, recipient, amount):
+        self.sender = sender
+        self.recipient = recipient
+        self.amount = amount
+        self.timestamp = int(time.time())
+
+    def to_dict(self):
+        return {
+            'sender': self.sender,
+            'recipient': self.recipient,
+            'amount': self.amount,
+            'timestamp': self.timestamp
+        }
