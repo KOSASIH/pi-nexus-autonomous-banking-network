56e33b80d0ef8ccfc4baf403eb162b39935398cb
KOSASIH <kosasihg88@gmail.com>
2024-06-04 16:45:03 +0700

    Create pi_block.py

---
+++ blockchain_integration/pi_network/wallet/pi_wallet/models/pi_block.py
@@ -0,0 +1,12 @@
+class PiBlock:
+    def __init__(self, block_number, transactions):
+        self.block_number = block_number
+        self.transactions = transactions
+        self.timestamp = int(time.time())
+
+    def to_dict(self):
+        return {
+            'block_number': self.block_number,
+            'transactions': self.transactions,
+            'timestamp': self.timestamp
+        }
