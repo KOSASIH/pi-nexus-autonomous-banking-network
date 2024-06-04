7bc694ef850316d0564d9ff5c39a27a46719019d
KOSASIH <kosasihg88@gmail.com>
2024-06-04 08:41:36 +0700

    Update PiNetworkIdentity.sol

--- blockchain_integration/pi_network/contracts/PiNetworkIdentity.sol
+++ blockchain_integration/pi_network/contracts/PiNetworkIdentity.sol
@@ -3,5 +3,29 @@ pragma solidity ^0.8.0;
 import "https://github.com/uport/uport-identity/blob/master/contracts/Identity.sol";
 
 contract PiNetworkIdentity is Identity {
-    //...
+    address private owner;
+    mapping (address => uint256) public balances;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function addIdentity(address _identity) public {
+        require(msg.sender == owner, "Only the owner can add identities");
+        identities[_identity] = true;
+    }
+
+    function removeIdentity(address _identity) public {
+        require(msg.sender == owner, "Only the owner can remove identities");
+        delete identities[_identity];
+    }
+
+    function getBalance(address _address) public view returns (uint256) {
+        return balances[_address];
+    }
+
+    function setBalance(address _address, uint256 _balance) public {
+        require(msg.sender == owner, "Only the owner can set balances");
+        balances[_address] = _balance;
+    }
 }
