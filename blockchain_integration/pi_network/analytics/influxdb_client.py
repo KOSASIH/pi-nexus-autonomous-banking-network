758a36ea24ea19b67d04511064702e3dd9a88b7f
KOSASIH <kosasihg88@gmail.com>
2024-06-04 08:18:49 +0700

    Create influxdb_client.py

---
+++ blockchain_integration/pi_network/analytics/influxdb_client.py
@@ -0,0 +1,16 @@
+import influxdb
+
+# Create an InfluxDB client
+client = influxdb.InfluxDBClient(host='localhost', port=8086)
+
+# Write data to InfluxDB
+client.write_points([
+    {
+        "measurement": "node_activity",
+        "tags": {"node_id": "node-1"},
+        "fields": {"activity": 10}
+    }
+])
+
+# Query data from InfluxDB
+result = client.query("SELECT * FROM node_activity")
