6c657e5e7c934a35b4c53eddddbbec8a08fc01eb
KOSASIH <kosasihg88@gmail.com>
2024-06-04 16:46:20 +0700

    Create pi_api_service.py

---
+++ blockchain_integration/pi_network/wallet/pi_wallet/services/pi_api_service.py
@@ -0,0 +1,38 @@
+import requests
+from .exceptions.pi_network_exceptions import PiNetworkApiException
+
+class PiApiService:
+    def __init__(self, api_endpoint, api_key, api_secret):
+        self.api_endpoint = api_endpoint
+        self.api_key = api_key
+        self.api_secret = api_secret
+
+    def get_blockchain_info(self):
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.api_key}'
+        }
+        response = requests.get(f'{self.api_endpoint}/blockchain', headers=headers)
+        if response.status_code != 200:
+            raise PiNetworkApiException(response.status_code, response.text)
+        return response.json()
+
+    def broadcast_transaction(self, transaction):
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.api_key}'
+        }
+        response = requests.post(f'{self.api_endpoint}/transaction', json=transaction, headers=headers)
+        if response.status_code != 201:
+            raise PiNetworkApiException(response.status_code, response.text)
+        return response.json()
+
+    def verify_transaction(self, transaction):
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.api_key}'
+        }
+        response = requests.get(f'{self.api_endpoint}/transaction/{transaction["hash"]}', headers=headers)
+        if response.status_code != 200:
+            raise PiNetworkApiException(response.status_code, response.text)
+        return response.json()
