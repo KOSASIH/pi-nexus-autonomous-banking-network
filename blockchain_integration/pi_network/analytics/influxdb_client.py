2e2b83d1323fd0d6f55819f49dc48dcc2321c0bf
KOSASIH <kosasihg88@gmail.com>
2024-06-04 09:10:01 +0700

    Update influxdb_client.py

--- blockchain_integration/pi_network/analytics/influxdb_client.py
+++ blockchain_integration/pi_network/analytics/influxdb_client.py
@@ -1,16 +1,29 @@
 import influxdb
 
-# Create an InfluxDB client
-client = influxdb.InfluxDBClient(host='localhost', port=8086)
+class InfluxDBClient:
+    def __init__(self, host, port, username, password, database):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.database = database
+        self.client = influxdb.InfluxDBClient(host, port, username, password, database)
 
-# Write data to InfluxDB
-client.write_points([
+    def write_points(self, points):
+        self.client.write_points(points)
+
+    def query(self, query):
+        return self.client.query(query)
+
+influxdb_client = InfluxDBClient("localhost", 8086, "root", "root", "pi_network")
+points = [
     {
         "measurement": "node_activity",
         "tags": {"node_id": "node-1"},
         "fields": {"activity": 10}
     }
-])
+]
+influxdb_client.write_points(points)
 
-# Query data from InfluxDB
-result = client.query("SELECT * FROM node_activity")
+result = influxdb_client.query("SELECT * FROM node_activity")
+print(result)
