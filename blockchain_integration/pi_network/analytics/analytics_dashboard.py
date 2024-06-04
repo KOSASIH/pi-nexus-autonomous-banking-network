8083d71641613719ba8cadc6d0a728e6b4916f79
KOSASIH <kosasihg88@gmail.com>
2024-06-04 10:31:26 +0700

    Create analytics_dashboard.py

---
+++ blockchain_integration/pi_network/analytics/analytics_dashboard.py
@@ -0,0 +1,16 @@
+# analytics_dashboard.py
+import matplotlib.pyplot as plt
+import pandas as pd
+
+class AnalyticsDashboard:
+    def __init__(self, data_source):
+        self.data_source = data_source
+
+    def update_dashboard(self):
+        data = self.data_source.get_data()
+        df = pd.DataFrame(data)
+        plt.plot(df['timestamp'], df['transaction_volume'])
+        plt.xlabel('Time')
+        plt.ylabel('Transaction Volume')
+        plt.title('Real-time Transaction Volume')
+        plt.show()
