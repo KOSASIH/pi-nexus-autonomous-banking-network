4ed5f3ff1d1b60cd0f3667f607a5ddab31a3a3b7
KOSASIH <kosasihg88@gmail.com>
2024-06-04 08:17:41 +0700

    Create PiNetworkContract.sol

---
+++ blockchain_integration/pi_network/contracts/PiNetworkContract.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/ownership/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/lifecycle/Pausable.sol";
+
+contract PiNetworkContract is Ownable, Pausable {
+    //...
+}
