de3a03bc12b087d853c5cd8b4dfed1897f9c376e
KOSASIH <kosasihg88@gmail.com>
2024-06-04 10:29:28 +0700

    Create interoperability_protocol.py

---
+++ blockchain_integration/pi_network/interoperability/interoperability_protocol.py
@@ -0,0 +1,15 @@
+# interoperability_protocol.py
+import hashlib
+
+class InteroperabilityProtocol:
+    def __init__(self, blockchain_networks):
+        self.blockchain_networks = blockchain_networks
+
+    def send_asset(self, asset, from_network, to_network):
+        # validate asset and networks
+        asset_hash = hashlib.sha256(asset.encode()).hexdigest()
+        self.blockchain_networks[from_network].send_transaction(asset_hash, to_network)
+
+    def receive_asset(self, asset_hash, from_network):
+        # validate asset hash and network
+        self.blockchain_networks[from_network].receive_transaction(asset_hash)
