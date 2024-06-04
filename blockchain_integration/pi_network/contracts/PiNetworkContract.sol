067741fb54c6d8750b6da768ce16d06568142597
KOSASIH <kosasihg88@gmail.com>
2024-06-04 08:47:08 +0700

    Update PiNetworkContract.sol

--- blockchain_integration/pi_network/contracts/PiNetworkContract.sol
+++ blockchain_integration/pi_network/contracts/PiNetworkContract.sol
@@ -4,5 +4,29 @@ import "https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contra
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/lifecycle/Pausable.sol";
 
 contract PiNetworkContract is Ownable, Pausable {
-    //...
+    address private owner;
+    mapping (address => uint256) public balances;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function addNode(address _node) public {
+        require(msg.sender == owner, "Only the owner can add nodes");
+        nodes[_node] = true;
+    }
+
+    function removeNode(address _node) public {
+        require(msg.sender == owner, "Only the owner can remove nodes");
+        delete nodes[_node];
+    }
+
+    function getBalance(address _address) public view returns (uint256) {
+        return balances[_address];
+    }
+
+    function setBalance(address _address, uint256 _balance) public {
+        require(msg.sender == owner, "Only the owner can set balances");
+        balances[_address] = _balance;
+    }
 }
