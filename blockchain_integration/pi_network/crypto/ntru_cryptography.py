f9cee2e7f72958750ecadb87e630acae33fe6eed
KOSASIH <kosasihg88@gmail.com>
2024-06-04 08:37:31 +0700

    Update ntru_cryptography.py

--- blockchain_integration/pi_network/crypto/ntru_cryptography.py
+++ blockchain_integration/pi_network/crypto/ntru_cryptography.py
@@ -1,10 +1,22 @@
 import ntru
 
-# Generate NTRU key pair
+class NTRUCryptography:
+    def __init__(self, private_key, public_key):
+        self.private_key = private_key
+        self.public_key = public_key
+
+    def encrypt(self, message):
+        return ntru.encrypt(message, self.public_key)
+
+    def decrypt(self, ciphertext):
+        return ntru.decrypt(ciphertext, self.private_key)
+
 private_key, public_key = ntru.keygen(701, 613, 11)
+ntru_cryptography = NTRUCryptography(private_key, public_key)
 
-# Encrypt a message
-ciphertext = ntru.encrypt("Hello, Pi Network!", public_key)
+message = "Hello, Pi Network!"
+ciphertext = ntru_cryptography.encrypt(message)
+print("Ciphertext:", ciphertext)
 
-# Decrypt the message
-plaintext = ntru.decrypt(ciphertext, private_key)
+plaintext = ntru_cryptography.decrypt(ciphertext)
+print("Plaintext:", plaintext)
