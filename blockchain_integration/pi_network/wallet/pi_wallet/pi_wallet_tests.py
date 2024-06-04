d5299e37198febaf6981cb7e38fa4a2add55c7f3
KOSASIH <kosasihg88@gmail.com>
2024-06-04 16:41:44 +0700

    Create pi_wallet_tests.py

---
+++ blockchain_integration/pi_network/wallet/pi_wallet/pi_wallet_tests.py
@@ -0,0 +1,26 @@
+import unittest
+from .pi_wallet import PiWallet
+
+class TestPiWallet(unittest.TestCase):
+    def setUp(self):
+        self.config = {'account_address': 'test_account_address'}
+        self.wallet = PiWallet(self.config)
+
+    def test_create_account(self):
+        account = self.wallet.create_account()
+        self.assertIsNotNone(account.get_private_key())
+        self.assertIsNotNone(account.get_public_key())
+
+    def test_send_pi(self):
+        recipient = 'recipient_account_address'
+        amount = 10
+        transaction = self.wallet.send_pi(recipient, amount)
+        self.assertIsNotNone(transaction)
+
+    def test_receive_pi(self):
+        transaction = {'amount': 10, 'sender': 'sender_account_address'}
+        self.wallet.receive_pi(transaction)
+        self.assertEqual(self.wallet.get_balance(), 10)
+
+if __name__ == '__main__':
+    unittest.main()
