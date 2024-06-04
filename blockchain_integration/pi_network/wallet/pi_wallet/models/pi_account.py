f706c73b3f28bc81d17d59dc5ee7ce731f267435
KOSASIH <kosasihg88@gmail.com>
2024-06-04 16:43:25 +0700

    Create pi_account.py

---
+++ blockchain_integration/pi_network/wallet/pi_wallet/models/pi_account.py
@@ -0,0 +1,18 @@
+class PiAccount:
+    def __init__(self, address):
+        self.address = address
+        self.private_key = None
+        self.public_key = None
+        self.balance = 0
+
+    def set_private_key(self, private_key):
+        self.private_key = private_key
+
+    def set_public_key(self, public_key):
+        self.public_key = public_key
+
+    def update_balance(self, amount):
+        self.balance += amount
+
+    def get_balance(self):
+        return self.balance
