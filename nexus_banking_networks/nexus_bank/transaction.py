15a1c0686de131fbc1521d6f32bc5839fbc40faf
KOSASIH <kosasihg88@gmail.com>
2024-06-05 19:46:18 +0700

    Create transaction.py

---
+++ nexus_banking_networks/nexus_bank/transaction.py
@@ -0,0 +1,12 @@
+class Transaction:
+    def __init__(self, transaction_id, account_id, amount, attributes):
+        self.transaction_id = transaction_id
+        self.account_id = account_id
+        self.amount = amount
+        self.attributes = attributes
+
+    def execute(self, account):
+        if self.amount > 0:
+            account.deposit(self.amount)
+        else:
+            account.withdraw(-self.amount)
