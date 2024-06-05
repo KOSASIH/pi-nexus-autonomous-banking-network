9ed75f18664992fe2878ef0537983f4ca19816e5
KOSASIH <kosasihg88@gmail.com>
2024-06-05 19:12:51 +0700

    Create AccountsView.vue

---
+++ nexus_banking_networks/web/src/views/AccountsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="account in accounts"
+        :key="account.account_id"
+        cols="12"
+        sm="6"
+        md="4"
+      >
+        <AccountCard :account="account" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import AccountCard from '../components/AccountCard.vue'
+
+export default {
+  components: {
+    AccountCard
+  },
+  computed: {
+    accounts() {
+      return this.$store.state.account.accounts
+    }
+  }
+}
+</script>
