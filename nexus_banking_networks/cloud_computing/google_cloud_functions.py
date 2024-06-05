4f40f78bb47779dac87a7447bedf27e719883068
KOSASIH <kosasihg88@gmail.com>
2024-06-05 20:13:21 +0700

    Create google_cloud_functions.py

---
+++ nexus_banking_networks/cloud_computing/google_cloud_functions.py
@@ -0,0 +1,17 @@
+import os
+from google.cloud import functions
+
+class GoogleCloudFunctions:
+    def __init__(self, function_name):
+        self.function_name = function_name
+        self.client = functions.CloudFunctionsServiceClient()
+
+    def invoke_function(self, event):
+        # Invoke Google Cloud Function
+        response = self.client.invoke_function(
+            request={
+                'name': self.function_name,
+                'data': json.dumps(event)
+            }
+        )
+        return response
