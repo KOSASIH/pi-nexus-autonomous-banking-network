e6713303adab901410723155f0ff7ed23ae2847e
KOSASIH <kosasihg88@gmail.com>
2024-06-05 19:14:22 +0700

    Create index.js

---
+++ nexus_banking_networks/web/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import AccountsView from '../views/AccountsView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Accounts',
+    component: AccountsView
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
