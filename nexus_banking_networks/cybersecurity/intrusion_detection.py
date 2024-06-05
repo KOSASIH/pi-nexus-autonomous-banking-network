17b8c1748fa660a250d7946becde18dcde8ff5f2
KOSASIH <kosasihg88@gmail.com>
2024-06-05 20:14:08 +0700

    Create intrusion_detection.py

---
+++ nexus_banking_networks/cybersecurity/intrusion_detection.py
@@ -0,0 +1,12 @@
+import scapy
+
+class IntrusionDetection:
+    def __init__(self, network_interface):
+        self.network_interface = network_interface
+        self.sniffer = scapy.sniff(iface=self.network_interface)
+
+    def detect_intrusions(self):
+        # Detect intrusions using Scapy
+        for packet in self.sniffer:
+            if packet.haslayer(scapy.TCP) and packet.dport == 22:
+                print("SSH connection detected!")
