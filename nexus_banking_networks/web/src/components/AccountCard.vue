2268f56e5c25da19df8925a2d90d1df478005cec
KOSASIH <kosasihg88@gmail.com>
2024-06-05 19:11:15 +0700

    Create AccountCard.vue

---
+++ nexus_banking_networks/web/src/components/AccountCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <h3 class="headline">{{ account.account_id }}</h3>
+    </v-card-title>
+    <v-card-text>
+      <p>Balance: {{ account.balance }}</p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    account: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
