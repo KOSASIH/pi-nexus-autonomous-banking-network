2f699401f1dadf52b1c0ae01a27895f2721a2cfa
KOSASIH <kosasihg88@gmail.com>
2024-06-05 18:39:47 +0700

    Create account.py

---
+++ nexus_banking_networks/account.py
@@ -0,0 +1,6 @@
+class NexusAccount:
+    def __init__(self, account_id: str, balance: float):
+        self.account_id = account_id
+        self.balance = balance
+
+    # Other methods for depositing, withdrawing, and transferring funds
