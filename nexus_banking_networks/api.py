24ea84555f43fd064f2780e0d8ee7f2a22ddf44e
KOSASIH <kosasihg88@gmail.com>
2024-06-05 18:41:41 +0700

    Create api.py

---
+++ nexus_banking_networks/api.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from nexus_banking_network import NexusAccount, NexusTransaction, NexusLedger
+
+app = Flask(__name__)
+api = Api(app)
+
+ledger = NexusLedger()
+
+class AccountResource(Resource):
+    def get(self, account_id):
+        account = next((acc for acc in ledger.accounts if acc.account_id == account_id), None)
+        if account:
+            return account.__dict__
+        else:
+            return {'error': 'Account not found'}, 404
+
+    def post(self, account_id):
+        # Implement creating a new account
+        pass
+
+class TransactionResource(Resource):
+    def post(self):
+        # Implement creating a new transaction
+        pass
+
+api.add_resource(AccountResource, '/accounts/<string:account_id>')
+api.add_resource(TransactionResource, '/transactions')
+
+if __name__ == '__main__':
+    app.run(debug=True)
