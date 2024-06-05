7054c2f6f44f7bc8b7eba5b1ee816e86999e3491
KOSASIH <kosasihg88@gmail.com>
2024-06-05 19:40:51 +0700

    Create node.py

---
+++ nexus_banking_networks/nexus_network/node.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self, node_id, node_type, attributes):
+        self.node_id = node_id
+        self.node_type = node_type
+        self.attributes = attributes
+
+    def __repr__(self):
+        return f"Node({self.node_id}, {self.node_type}, {self.attributes})"
