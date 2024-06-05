d2049883ea9448eb65da246b577555fc32b86e29
KOSASIH <kosasihg88@gmail.com>
2024-06-05 19:43:44 +0700

    Create bank.py

---
+++ nexus_banking_networks/nexus_bank/nexus_bank/bank.py
@@ -0,0 +1,16 @@
+class Bank:
+    def __init__(self, bank_id, name, attributes):
+        self.bank_id = bank_id
+        self.name = name
+        self.attributes = attributes
+        self.accounts = {}
+
+    def add_account(self, account_id, account_type, attributes):
+        account = Account(account_id, account_type, attributes)
+        self.accounts[account_id] = account
+
+    def get_accounts(self):
+        return list(self.accounts.values())
+
+    def get_account(self, account_id):
+        return self.accounts.get(account_id)
