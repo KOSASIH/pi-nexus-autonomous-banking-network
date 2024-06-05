686b8dffb0204eb91fe99b9db421e16d026bd250
KOSASIH <kosasihg88@gmail.com>
2024-06-05 20:06:04 +0700

    Create predictive_modeling.py

---
+++ nexus_banking_networks/neural_networks/predictive_modeling.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
+
+class PredictiveModeling:
+    def __init__(self, data):
+        self.data = data
+
+    def train_model(self):
+        # Train neural network model on historical data
+        X = self.data.drop(['target'], axis=1)
+        y = self.data['target']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        model = MLPRegressor(hidden_layer_sizes=(100, 50), max_iter=1000, random_state=42)
+        model.fit(X_train, y_train)
+        return model
+
+    def make_predictions(self, model):
+        # Make predictions on new data using trained model
+        predictions = model.predict(self.data)
+        return predictions
