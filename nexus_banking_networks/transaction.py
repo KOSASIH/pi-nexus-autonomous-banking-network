01f89ec5fb25434e23a8db18f93a3877cab15d3b
KOSASIH <kosasihg88@gmail.com>
2024-06-05 18:40:27 +0700

    Create transaction.py

---
+++ nexus_banking_networks/transaction.py
@@ -0,0 +1,7 @@
+class NexusTransaction:
+    def __init__(self, sender: NexusAccount, receiver: NexusAccount, amount: float):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+
+    # Other methods for processing and validating transactions
