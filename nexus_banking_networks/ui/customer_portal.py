fc95fbc16b05d8b2f864989be44a97a9195f5234
KOSASIH <kosasihg88@gmail.com>
2024-06-06 12:51:03 +0700

    Create customer_portal.py

---
+++ nexus_banking_networks/ui/customer_portal.py
@@ -0,0 +1,18 @@
+import flask
+from flask import request, jsonify
+
+class CustomerPortal:
+    def __init__(self, app):
+        self.app = app
+
+    def create_account(self, request):
+        # Create account for customer
+        pass
+
+    def login_account(self, request):
+        # Login account for customer
+        pass
+
+    def manage_account(self, request):
+        # Manage account for customer
+        pass
