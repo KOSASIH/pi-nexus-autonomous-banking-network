77035532178048dccb94376a8476b1d33078bb5c
KOSASIH <kosasihg88@gmail.com>
2024-06-07 10:17:42 +0700

    Update automated_model_selection.py

--- nexus_banking_networks/analytics/machine_learning/automated_model_selection.py
+++ nexus_banking_networks/analytics/machine_learning/automated_model_selection.py
@@ -30,4 +30,33 @@ class AutomatedModelSelector:
                 tf.keras.layers.Dense(1, activation='sigmoid')
             ])
 
-        X_train, X_test, y_train, y_test = train_test_split(self.data
+        X_train, X_test, y_train, y_test = train_test_split(self.data, self.target, test_size=0.2, random_state=42)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        return accuracy
+
+    def objective(self, params):
+        # Define the objective function for hyperparameter tuning
+        if params['model'] == 'random_forest':
+            model = RandomForestClassifier(n_estimators=params['random_forest__n_estimators'])
+        else:
+            model = tf.keras.models.Sequential([
+                tf.keras.layers.Dense(params['neural_network__hidden_units'], activation='relu'),
+                tf.keras.layers.Dense(1, activation='sigmoid')
+            ])
+
+        X_train, X_test, y_train, y_test = train_test_split(self.data, self.target, test_size=0.2, random_state=42)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        return -accuracy
+
+class AdvancedMachineLearning:
+    def __init__(self, automated_model_selector):
+        self.automated_model_selector = automated_model_selector
+
+    def perform_machine_learning(self, data, target):
+        # Perform machine learning using the automated model selector
+        accuracy = self.automated_model_selector.select_best_model()
+        return accuracy
