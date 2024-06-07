1663e0d9de05f125d4622de1908941928fd95559
KOSASIH <kosasihg88@gmail.com>
2024-06-07 13:56:43 +0700

    Create online_rl_anomaly_detection.py

---
+++ nexus_banking_networks/anomaly_detection/online_rl_anomaly_detection.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import IsolationForest
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
+import gym
+import torch
+from torch.nn import functional as F
+
+class OnlineRLAnomalyDetector:
+    def __init__(self, kafka_topic, bootstrap_servers, seq_len=100):
+        self.kafka_topic = kafka_topic
+        self.bootstrap_servers = bootstrap_servers
+        self.seq_len = seq_len
+        self.scaler = StandardScaler()
+        self.model = IsolationForest(contamination=0.1)
+        self.consumer = KafkaConsumer(self.kafka_topic, bootstrap_servers=self.bootstrap_servers)
+        self.producer = KafkaProducer(bootstrap_servers=self.bootstrap_servers)
+        self.env = gym.make('AnomalyDetection-v0')
+        self.agent = torch.nn.Linear(seq_len, 1)
+
+    def detect_anomalies(self, data):
+        data_scaled = self.scaler.transform(data)
+        X_pred = data_scaled[-self.seq_len:]
+        X_pred = X_pred.reshape(1, X_pred.shape[0], 1)
+        pred = self.model.predict(X_pred)
+        anomaly_score = pred[0]
+        if anomaly_score < 0:
+            self.producer.send(self.kafka_topic, value=b'anomaly_detected')
+        else:
+           self.producer.send(self.kafka_topic, value=b'no_anomaly')
+        return anomaly_score
+
+    def start_streaming(self):
+        for message in self.consumer:
+            data = pd.read_csv(message.value, header=None)
+            anomaly_score = self.detect_anomalies(data)
+            state = torch.tensor(data.values)
+            action = self.agent(state)
+            next_state, reward, done, _ = self.env.step(action)
+            self.agent.optimize(reward)
+
+    def update_model(self, data):
+        self.model.fit(data)
+
+class AnomalyDetectionEnv(gym.Env):
+    def __init__(self):
+        self.observation_space = gym.spaces.Box(low=-1, high=1, shape=(100,))
+        self.action_space = gym.spaces.Discrete(2)
+
+    def step(self, action):
+        if action == 0:
+            reward = -1
+        else:
+            reward = 1
+        next_state = np.random.rand(100)
+        done = False
+        return next_state, reward, done, {}
+
+    def reset(self):
+        return np.random.rand(100)
