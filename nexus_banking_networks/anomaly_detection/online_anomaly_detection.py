b36db2e58ac433e817b87d58beb1bde0ed1f8bd0
KOSASIH <kosasihg88@gmail.com>
2024-06-07 13:53:14 +0700

    Create online_anomaly_detection.py

---
+++ nexus_banking_networks/anomaly_detection/online_anomaly_detection.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import IsolationForest
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
+
+class OnlineAnomalyDetector:
+    def __init__(self, kafka_topic, bootstrap_servers, seq_len=100):
+        self.kafka_topic = kafka_topic
+        self.bootstrap_servers = bootstrap_servers
+        self.seq_len = seq_len
+        self.scaler = StandardScaler()
+        self.model = IsolationForest(contamination=0.1)
+        self.consumer = KafkaConsumer(self.kafka_topic, bootstrap_servers=self.bootstrap_servers)
+        self.producer = KafkaProducer(bootstrap_servers=self.bootstrap_servers)
+
+    def detect_anomalies(self, data):
+        data_scaled = self.scaler.transform(data)
+        X_pred = data_scaled[-self.seq_len:]
+        X_pred = X_pred.reshape(1, X_pred.shape[0], 1)
+        pred = self.model.predict(X_pred)
+        anomaly_score = pred[0]
+        if anomaly_score < 0:
+            self.producer.send(self.kafka_topic, value=b'anomaly_detected')
+        else:
+            self.producer.send(self.kafka_topic, value=b'no_anomaly')
+
+    def start_streaming(self):
+        for message in self.consumer:
+            data = pd.read_csv(message.value, header=None)
+            self.detect_anomalies(data)
+
+    def update_model(self, data):
+        self.model.fit(data)
