62bda3b27a0d31d8aaa71f9a359162c55e6801d3
KOSASIH <kosasihg88@gmail.com>
2024-06-08 11:43:48 +0700

    Create smpc_yao_garbled_circuits_ot_he.py

---
+++ nexus_banking_networks/cryptography/smpc_yao_garbled_circuits_ot_he.py
@@ -0,0 +1,31 @@
+import gmpy2
+
+class YaoGarbledCircuitsOTHE:
+    def __init__(self, parties, threshold):
+        self.parties = parties
+        self.threshold = threshold
+        self.keys = {}
+
+    def keygen(self):
+        # Generate shared keys for each party
+        pass
+
+    def garble(self, circuit):
+        # Garble circuit using Yao's protocol
+        pass
+
+    def oblivious_transfer(self, inputs):
+        # Perform oblivious transfer of inputs
+        pass
+
+    def homomorphic_encrypt(self, plaintext):
+        # Encrypt plaintext using homomorphic encryption scheme
+        pass
+
+    def evaluate(self, garbled_circuit, inputs):
+        # Evaluate garbled circuit with inputs
+        pass
+
+    def reconstruct(self, outputs):
+        # Reconstruct original output from shares
+        pass
