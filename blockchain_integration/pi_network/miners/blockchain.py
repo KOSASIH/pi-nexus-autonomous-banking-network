4c4b8df76720d599440a6c95da6ce750e235927a
KOSASIH <kosasihg88@gmail.com>
2024-06-08 22:16:55 +0700

    Create blockchain.py

---
+++ blockchain_integration/pi_network/miners/blockchain.py
@@ -0,0 +1,15 @@
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        # Create the genesis block
+        pass
+
+    def add_block(self, block):
+        # Add a new block to the blockchain
+        pass
+
+    def get_latest_block(self):
+        # Get the latest block in the blockchain
+        pass
