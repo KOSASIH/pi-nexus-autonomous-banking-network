a5c169baec2fb800f37b9179f8db5a8086bc8ff0
KOSASIH <kosasihg88@gmail.com>
2024-06-08 22:21:11 +0700

    Create message_handler.py

---
+++ blockchain_integration/pi_network/network_protocol/message_handler.py
@@ -0,0 +1,7 @@
+class MessageHandler:
+    def __init__(self, node):
+        self.node = node
+
+    def handle_message(self, message):
+        # Handle incoming messages (e.g. new block announcements)
+        pass
