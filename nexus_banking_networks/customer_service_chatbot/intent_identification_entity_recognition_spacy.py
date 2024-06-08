bd5e425fe4cd888b1cfc96bf2f8fff7e4c2bfb69
KOSASIH <kosasihg88@gmail.com>
2024-06-08 11:46:47 +0700

    Create intent_identification_entity_recognition_spacy.py

---
+++ nexus_banking_networks/customer_service_chatbot/intent_identification_entity_recognition_spacy.py
@@ -0,0 +1,13 @@
+import spacy
+
+class IntentIdentificationEntityRecognition:
+    def __init__(self, nlp):
+        self.nlp = nlp
+
+    def identify_intent(self, input_text):
+        # Identify intent of customer query using spaCy model
+        pass
+
+    def recognize_entities(self, input_text):
+        # Recognize entities in customer query using spaCy model
+        pass
