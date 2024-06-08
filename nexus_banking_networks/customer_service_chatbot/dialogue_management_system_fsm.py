aa54fd1c94eca353a5c32cfdf205cb3e3220c451
KOSASIH <kosasihg88@gmail.com>
2024-06-08 11:47:18 +0700

    Create dialogue_management_system_fsm.py

---
+++ nexus_banking_networks/customer_service_chatbot/dialogue_management_system_fsm.py
@@ -0,0 +1,9 @@
+import fsm
+
+class DialogueManagementSystem:
+    def __init__(self, fsm_model):
+        self.fsm_model = fsm_model
+
+    def manage_dialogue(self, input_text):
+        # Manage conversation with customer using finite state machine model
+        pass
