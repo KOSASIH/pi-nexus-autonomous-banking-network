cc3757ad0a32f6734980d713e7b616fa37132b60
KOSASIH <kosasihg88@gmail.com>
2024-06-09 23:19:17 +0700

    Create pios.py

---
+++ blockchain_integration/pi_network/technological_value/pios.py
@@ -0,0 +1,34 @@
+import hashlib
+
+class Pios:
+    def __init__(self):
+        self.blockchain = []
+
+    def add_block(self, block):
+        self.blockchain.append(block)
+
+    def verify_block(self, block):
+        previous_block = self.blockchain[-1]
+        if block['previous_hash']!= previous_block['hash']:
+            return False
+        if not self.validate_proof_of_work(block):
+            return False
+        return True
+
+    def validate_proof_of_work(self, block):
+        difficulty = block['difficulty']
+        nonce = block['nonce']
+        hash = hashlib.sha256(str(nonce).encode() + str(difficulty).encode()).hexdigest()
+        if hash[:difficulty]!= '0' * difficulty:
+            return False
+        return True
+
+if __name__ == '__main__':
+    pios = Pios()
+    block1 = {'previous_hash': '0' * 64, 'transactions': [], 'nonce': 0, 'difficulty': 4}
+    pios.add_block(block1)
+
+    block2 = {'previous_hash': block1['hash'], 'transactions': [], 'nonce': 1, 'difficulty': 4}
+    pios.add_block(block2)
+
+    print(pios.verify_block(block2))
