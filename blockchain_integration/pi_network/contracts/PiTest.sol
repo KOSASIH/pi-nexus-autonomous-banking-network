fc24c365302abca14f65393cea607f0b8b3c29e7
KOSASIH <kosasihg88@gmail.com>
2024-06-09 12:51:44 +0700

    Create PiTest.sol

---
+++ blockchain_integration/pi_network/contracts/PiTest.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+contract PiTest {
+    function testFunction() public {
+        // Implement test logic here
+        assert(true, "Test failed");
+    }
+
+    function debugFunction() public {
+        // Implement debug logic here
+        console.log("Debug message");
+    }
+}
