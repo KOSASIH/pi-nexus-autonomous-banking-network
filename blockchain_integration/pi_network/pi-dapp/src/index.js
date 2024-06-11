57d71bf66307311a3434ab6a77bd98a09218c202
KOSASIH <kosasihg88@gmail.com>
2024-06-11 10:07:04 +0700

    Update index.js

--- blockchain_integration/pi_network/pi-dapp/src/index.js
+++ blockchain_integration/pi_network/pi-dapp/src/index.js
@@ -1,14 +1,26 @@
-import express from 'express';
-import api from './api';
-import wallet from './wallet';
-import dex from './dex';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Web3Provider } from '@ethersproject/providers';
+import { Web3ReactProvider } from '@web3-react/core';
+import { PiNetworkProvider } from './PiNetworkProvider';
+import App from './App';
+import './index.css';
 
-const app = express();
+const web3Provider = new Web3Provider(window.ethereum);
+const piNetworkProvider = new PiNetworkProvider(web3Provider);
 
-app.use('/api', api);
-app.use('/wallet', wallet);
-app.use('/dex', dex);
-
-app.listen(3000, () => {
-    console.log('Pi DApp listening on port 3000');
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <Web3ReactProvider>
+      <PiNetworkProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={App} exact />
+          </Switch>
+        </BrowserRouter>
+      </PiNetworkProvider>
+    </Web3ReactProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
