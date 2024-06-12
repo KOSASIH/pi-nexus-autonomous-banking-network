4a5daed9363e11936136519faed692ed8fa2de62
KOSASIH <kosasihg88@gmail.com>
2024-06-12 21:29:55 +0700

    Create Footer.js

---
+++ blockchain_integration/pi_network/pi-browser/components/Footer.js
@@ -0,0 +1,24 @@
+// components/Footer.js
+import React from 'eact';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2023 Pi Browser</p>
+      <ul>
+        <li>
+          <a href="https://github.com/KOSASIH/pi-nexus-autonomous-banking-network" target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://pi-nexus-autonomous-banking-network.io" target="_blank" rel="noopener noreferrer">
+            Pi Nexus Autonomous Banking Network
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
