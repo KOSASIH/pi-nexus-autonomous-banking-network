5dc0b7d8230e695611d6e418df7bee78cd787172
KOSASIH <kosasihg88@gmail.com>
2024-06-12 22:11:45 +0700

    Create package.js

---
+++ blockchain_integration/pi_network/pi-browser/package.js
@@ -0,0 +1,23 @@
+{
+  "name": "pi-nexus-autonomous-banking-network",
+  "version": "1.0.0",
+  "description": "PI-Nexus Autonomous Banking Network",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo 'No tests yet!'"
+  },
+  "keywords": [],
+  "author": "KOSASIH",
+  "license": "MIT",
+  "dependencies": {
+    "socket.io": "^4.5.1",
+    "http": "^0.0.1-security",
+    "crypto-js": "^4.1.1",
+    "elliptic": "^6.5.4",
+    "hash-sum": "^2.0.0",
+    "js-sha3": "^0.8.0",
+    "merkle-tree": "^1.0.1",
+    "secp256k1": "^4.0.3"
+  }
+}
