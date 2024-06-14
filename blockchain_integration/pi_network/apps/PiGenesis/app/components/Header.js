2fc6cc462414d70e2cf90bff0897383d8421f9f7
KOSASIH <kosasihg88@gmail.com>
2024-06-14 16:27:09 +0700

    Create Header.js

---
+++ blockchain_integration/pi_network/apps/PiGenesis/app/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'eact';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Settings</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
