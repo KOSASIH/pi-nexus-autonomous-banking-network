6d41e63e8b03d6fb467245ebbc7a9f7832e2075d
KOSASIH <kosasihg88@gmail.com>
2024-06-15 22:01:21 +0700

    Create PIBankMath.sol

---
+++ blockchain_integration/pi_network/contracts/Lib/PIBankMath.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.0;
+
+library PIBankMath {
+    // Function to calculate the square root of a number
+    function sqrt(uint256 x) internal pure returns (uint256) {
+        uint256 z = (x + 1) / 2;
+        uint256 y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+        return y;
+    }
+}
