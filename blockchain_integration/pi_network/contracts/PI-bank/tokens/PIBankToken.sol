8a70faf3cd50596ff3633b470ae89c4be8087700
KOSASIH <kosasihg88@gmail.com>
2024-06-16 11:42:18 +0700

    Create PIBankToken.sol

---
+++ blockchain_integration/pi_network/contracts/PI-bank/tokens/PIBankToken.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.0;
+
+import "./IPIBankToken.sol";
+
+contract PIBankToken is IPIBankToken {
+    mapping(address => uint256) public balances;
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000 ether; // 1 million tokens
+        balances[msg.sender] = totalSupply;
+    }
+
+    function mint(address _owner, uint256 _amount) public {
+        totalSupply += _amount;
+        balances[_owner] += _amount;
+    }
+
+    function burn(uint256 _amount) public {
+        totalSupply -= _amount;
+        balances[msg.sender] -= _amount;
+    }
+
+    function transfer(address _from, address _to, uint256 _amount) public {
+        balances[_from] -= _amount;
+        balances[_to] += _amount;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return balances[_owner];
+    }
+}
