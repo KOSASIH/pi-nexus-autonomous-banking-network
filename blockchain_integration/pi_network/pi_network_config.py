b403bfdd7826f4353b6910f7f76cfadfcafb5938
KOSASIH <kosasihg88@gmail.com>
2024-06-16 18:45:36 +0700

    Create pi_network_config.py

---
+++ blockchain_integration/pi_network/pi_network_config.py
@@ -0,0 +1,9 @@
+config = {
+    'api_key': 'YOUR_API_KEY',
+    'api_secret': 'YOUR_API_SECRET',
+    'nodes': [
+        {'id': 'node1', 'name': 'Node 1', 'status': 'online'},
+        {'id': 'node2', 'name': 'Node 2', 'status': 'offline'},
+        # Add more nodes as needed
+    ]
+}
