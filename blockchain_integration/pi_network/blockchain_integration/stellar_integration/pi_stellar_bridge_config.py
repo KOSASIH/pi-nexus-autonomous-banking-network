88297de431d27b9b6d4c5562bea1c147ed607d1c
KOSASIH <kosasihg88@gmail.com>
2024-06-18 21:00:41 +0700

    Create pi_stellar_bridge_config.py

---
+++ blockchain_integration/pi_network/blockchain_integration/stellar_integration/pi_stellar_bridge_config.py
@@ -0,0 +1,6 @@
+class PiStellarBridgeConfig:
+    def __init__(self):
+        self.stellar_network = "testnet"  # or "mainnet"
+        self.pi_network_api_url = "https://api.pi.network"
+        self.pi_network_api_key = "YOUR_PI_NETWORK_API_KEY"
+        self.stellar_federation_server = "https://stellar-federation.io"
