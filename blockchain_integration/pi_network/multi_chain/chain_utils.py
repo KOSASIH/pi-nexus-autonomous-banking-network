c2faa57290c7956f89e1dba677eee0132eb987a0
KOSASIH <kosasihg88@gmail.com>
2024-06-19 20:48:57 +0700

    Create chain_utils.py

---
+++ blockchain_integration/pi_network/multi_chain/chain_utils.py
@@ -0,0 +1,9 @@
+def get_chain_symbol(chain_name):
+    # Return the symbol for the given chain name
+    # e.g. 'Ethereum' -> 'ETH'
+    pass
+
+def get_chain_rpc_url(chain_name):
+    # Return the RPC URL for the given chain name
+    # e.g. 'Ethereum' -> 'https://mainnet.infura.io/v3/YOUR_PROJECT_ID'
+    pass
