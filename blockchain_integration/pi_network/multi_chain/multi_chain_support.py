c42f65cf982e62e631ace78760778ef18e4df922
KOSASIH <kosasihg88@gmail.com>
2024-06-19 20:34:01 +0700

    Update multi_chain_support.py

--- blockchain_integration/pi_network/multi_chain/multi_chain_support.py
+++ blockchain_integration/pi_network/multi_chain/multi_chain_support.py
@@ -1,29 +1,115 @@
-import web3
+import requests
+from web3 import Web3
 
-# Define function to connect to multiple blockchain networks
-def connect_to_chains():
-    chains = [
-        {'name': 'Ethereum', 'provider': 'https://mainnet.infura.io/v3/YOUR_PROJECT_ID'},
-        {'name': 'Binance Smart Chain', 'provider': 'https://bsc-dataseed.binance.org/api/v1/'},
-        {'name': 'Polkadot', 'provider': 'https://polkadot.api.onfinality.io/public'}
-    ]
-    w3_chains = []
-    for chain in chains:
-        w3_chain = web3.Web3(web3.HTTPProvider(chain['provider']))
-        w3_chains.append(w3_chain)
-    return w3_chains
+class MultiChainSupport:
+    def __init__(self):
+        self.chains = {
+            'Ethereum': EthereumChain(),
+            'Binance Smart Chain': BinanceSmartChain(),
+            'Polygon': PolygonChain(),
+            'Solana': SolanaChain(),
+            'Polkadot': PolkadotChain(),
+            'Kusama': KusamaChain(),
+            'Avalanche': AvalancheChain(),
+            'Fantom': FantomChain(),
+            'Cronos': CronosChain(),
+            'Harmony': HarmonyChain(),
+            'OKC': OKCChain(),
+            'Arbitrum': ArbitrumChain(),
+            'Optimism': OptimismChain(),
+            'Moonbeam': MoonbeamChain(),
+            'Moonriver': MoonriverChain(),
+        }
 
-# Define function to execute transactions on multiple blockchain networks
-def execute_transactions(transactions):
-    w3_chains = connect_to_chains()
-    for transaction in transactions:
-        for w3_chain in w3_chains:
-            tx_hash = w3_chain.eth.send_transaction(transaction)
-            print(f"Transaction {tx_hash} executed on {w3_chain.provider}")
+    def get_chain(self, chain_name):
+        return self.chains.get(chain_name)
 
-# Integrate with blockchain integration
-def execute_all_transactions():
-    transactions = get_all_transactions()
-    execute_transactions(transactions)
+class EthereumChain:
+    def __init__(self):
+        self.rpc_url = 'https://mainnet.infura.io/v3/YOUR_PROJECT_ID'
+        self.chain_id = 1
+        self.symbol = 'ETH'
 
-execute_all_transactions()
+class BinanceSmartChain:
+    def __init__(self):
+        self.rpc_url = 'https://bsc-dataseed.binance.org/api/v1/'
+        self.chain_id = 56
+        self.symbol = 'BNB'
+
+class PolygonChain:
+    def __init__(self):
+        self.rpc_url = 'https://polygon-rpc.com/'
+        self.chain_id = 137
+        self.symbol = 'MATIC'
+
+class SolanaChain:
+    def __init__(self):
+        self.rpc_url = 'https://api.mainnet-beta.solana.io'
+        self.chain_id = 101
+        self.symbol = 'SOL'
+
+class PolkadotChain:
+    def __init__(self):
+        self.rpc_url = 'https://rpc.polkadot.io'
+        self.chain_id = 0
+        self.symbol = 'DOT'
+
+class KusamaChain:
+    def __init__(self):
+        self.rpc_url = 'https://rpc.kusama.network'
+        self.chain_id = 2
+        self.symbol = 'KSM'
+
+class AvalancheChain:
+    def __init__(self):
+        self.rpc_url = 'https://api.avax.network/ext/bc/C/rpc'
+        self.chain_id = 43114
+        self.symbol = 'AVAX'
+
+class FantomChain:
+    def __init__(self):
+        self.rpc_url = 'https://rpc.ftm.tools'
+        self.chain_id = 250
+        self.symbol = 'FTM'
+
+class CronosChain:
+    def __init__(self):
+        self.rpc_url = 'https://rpc.cronos.org'
+        self.chain_id = 25
+        self.symbol = 'CRO'
+
+class HarmonyChain:
+    def __init__(self):
+        self.rpc_url = 'https://api.harmony.one'
+        self.chain_id = 1666600000
+        self.symbol = 'ONE'
+
+class OKCChain:
+    def __init__(self):
+        self.rpc_url = 'https://exchaintestrpc.okex.org'
+        self.chain_id = 66
+        self.symbol = 'OKT'
+
+class ArbitrumChain:
+    def __init__(self):
+        self.rpc_url = 'https://arb1.arbitrum.io/rpc'
+        self.chain_id = 42161
+        self.symbol = 'ETH'
+
+class OptimismChain:
+    def __init__(self):
+        self.rpc_url = 'https://mainnet.optimism.io'
+        self.chain_id = 10
+        self.symbol = 'ETH'
+
+class MoonbeamChain:
+    def __init__(self):
+        self.rpc_url = 'https://rpc.api.moonbeam.network'
+        self.chain_id = 1284
+        self.symbol = 'GLMR'
+
+class MoonriverChain:
+    def __init__(self):
+        self.rpc_url = 'https://rpc.api.moonriver.moonbeam.network'
+        self.chain_id = 1285
+        self.symbol = 'MOVR'
