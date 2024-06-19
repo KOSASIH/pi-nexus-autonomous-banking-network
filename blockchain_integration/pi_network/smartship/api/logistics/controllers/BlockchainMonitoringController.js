ccc8d77abb4df8c75215e9aac601a893e0de79df
KOSASIH <kosasihg88@gmail.com>
2024-06-19 11:42:14 +0700

    Create BlockchainMonitoringController.js

---
+++ blockchain_integration/pi_network/smartship/api/logistics/controllers/BlockchainMonitoringController.js
@@ -0,0 +1,13 @@
+const { FabricClient } = require('fabric-client');
+
+class BlockchainMonitoringController {
+  async monitorTransactions(req, res) {
+    const { chaincodeId } = req.body;
+    const client = new FabricClient();
+    const channel = client.getChannel('mychannel');
+    const blockEvents = channel.watchBlockEvents();
+    blockEvents.on('data', (block) => {
+      const transactions = block.data.data;
+      const analyzedTransactions = analyzeTransactions(transactions);
+      res.json({ analyzedTransactions });
+    });
