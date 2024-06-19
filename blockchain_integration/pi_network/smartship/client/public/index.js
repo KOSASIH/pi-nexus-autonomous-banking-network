5fd84748d9c7f01f868c3ba45c7923801955f064
KOSASIH <kosasihg88@gmail.com>
2024-06-19 10:51:54 +0700

    Create index.js

---
+++ blockchain_integration/pi_network/smartship/client/public/index.js
@@ -0,0 +1,10 @@
+import React from 'eact';
+import ReactDOM from 'eact-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
