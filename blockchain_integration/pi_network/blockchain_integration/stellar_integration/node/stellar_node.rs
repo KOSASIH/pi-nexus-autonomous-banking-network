9dbc81349ec1372c147ef72ef125af4c91e751ce
KOSASIH <kosasihg88@gmail.com>
2024-06-21 08:40:07 +0700

    Create stellar_node.rs

---
+++ blockchain_integration/pi_network/blockchain_integration/stellar_integration/node/stellar_node.rs
@@ -0,0 +1,31 @@
+use stellar_sdk::types::{Transaction, Block};
+use stellar_sdk::xdr::{ScVal, ScVec};
+
+struct StellarNode {
+    // In-memory storage for transactions and blocks
+    transactions: ScVec<Transaction>,
+    blocks: ScVec<Block>,
+    // ...
+}
+
+impl StellarNode {
+    fn new() -> Self {
+        // ...
+    }
+
+    fn process_transaction(&mut self, tx: Transaction) -> Result<(), Error> {
+        // ...
+    }
+
+    fn process_block(&mut self, block: Block) -> Result<(), Error> {
+        // ...
+    }
+
+    fn get_transaction(&self, hash: &str) -> Option<Transaction> {
+        // ...
+    }
+
+    fn get_block(&self, hash: &str) -> Option<Block> {
+        // ...
+    }
+}
