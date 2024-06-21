c1d71363ec7bc2e7a029d6eb78d8c529d7c981ff
KOSASIH <kosasihg88@gmail.com>
2024-06-21 10:21:56 +0700

    Create incentive_mechanisms.py

---
+++ blockchain_integration/pi_network/blockchain_integration/stellar_integration/incentives/incentive_mechanisms.py
@@ -0,0 +1,7 @@
+def node_operator_incentive(node_id, block_height):
+    # Calculate incentive based on node ID and block height
+    return 10.0
+
+def validator_incentive(validator_id, block_height):
+    # Calculate incentive based on validator ID and block height
+    return 20.0
