9e4ee6032884c9a75e67dd79cc4005a925d0ff39
KOSASIH <kosasihg88@gmail.com>
2024-06-21 10:20:43 +0700

    Create tokenomics.py

---
+++ blockchain_integration/pi_network/blockchain_integration/stellar_integration/token_economics/tokenomics.py
@@ -0,0 +1,11 @@
+tokenomics_model = {
+    "token_name": "Pi Token",
+    "token_symbol": "PI",
+    "total_supply": 100000000,
+    "block_reward": 100,
+    "token_distribution": {
+        "node_operators": 30,
+        "validators": 20,
+        "users": 50
+    }
+}
