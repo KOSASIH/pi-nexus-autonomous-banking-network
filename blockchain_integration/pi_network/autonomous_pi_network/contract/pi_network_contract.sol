54491805845d2f74ef7aaf50052cd5a1aa9d02f0
KOSASIH <kosasihg88@gmail.com>
2024-06-25 17:12:50 +0700

    Create pi_network_contract.sol

---
+++ blockchain_integration/pi_network/autonomous_pi_network/contract/pi_network_contract.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+contract PINetwork {
+    //...
+    function registerNode(address _nodeAddress) public {
+        //...
+    }
+
+    function getNodeList() public view returns (address[] memory) {
+        //...
+    }
+
+    function transferFunds(address _from, address _to, uint256 _amount) public {
+        //...
+    }
+}
