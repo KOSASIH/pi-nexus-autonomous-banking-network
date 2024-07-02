56684fef5c189d2ac3ffc02912f3c6b3ae6ed75a
KOSASIH <kosasihg88@gmail.com>
2024-07-03 00:13:29 +0700

    Create OracleRepository.js

---
+++ nexus_core/oracle/OracleRepository.js
@@ -0,0 +1,21 @@
+class OracleRepository {
+  constructor() {
+    // Initialize oracle repository
+  }
+
+  createOracle(data) {
+    // Create oracle repository logic
+  }
+
+  getOracle(id) {
+    // Get oracle repository logic
+  }
+
+  updateOracle(id, data) {
+    // Update oracle repository logic
+  }
+
+  deleteOracle(id) {
+    // Delete oracle repository logic
+  }
+}
