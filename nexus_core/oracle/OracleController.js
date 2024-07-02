968325d39f728f47239ccdc58fbd7da92775f447
KOSASIH <kosasihg88@gmail.com>
2024-07-03 00:12:47 +0700

    Create OracleController.js

---
+++ nexus_core/oracle/OracleController.js
@@ -0,0 +1,21 @@
+class OracleController {
+  constructor() {
+    // Initialize oracle controller
+  }
+
+  createOracle(req, res) {
+    // Create oracle controller logic
+  }
+
+  getOracle(req, res) {
+    // Get oracle controller logic
+  }
+
+  updateOracle(req, res) {
+    // Update oracle controller logic
+  }
+
+  deleteOracle(req, res) {
+    // Delete oracle controller logic
+  }
+}
