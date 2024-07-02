c66fac61fd7cce56e2afd698fa48020bf51ddb4e
KOSASIH <kosasihg88@gmail.com>
2024-07-02 23:51:11 +0700

    Create TransactionModel.js

---
+++ nexus_core/models/TransactionModel.js
@@ -0,0 +1,21 @@
+class TransactionModel {
+  constructor() {
+    // Initialize transaction model
+  }
+
+  createTransaction(data) {
+    // Create transaction logic
+  }
+
+  getTransaction(id) {
+    // Get transaction logic
+  }
+
+  updateTransaction(id, data) {
+    // Update transaction logic
+  }
+
+  deleteTransaction(id) {
+    // Delete transaction logic
+  }
+}
