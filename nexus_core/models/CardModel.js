b2e3e9bac079cc24bd03d957301b5a05c786159c
KOSASIH <kosasihg88@gmail.com>
2024-07-02 23:53:50 +0700

    Create CardModel.js

---
+++ nexus_core/models/CardModel.js
@@ -0,0 +1,21 @@
+class CardModel {
+  constructor() {
+    // Initialize card model
+  }
+
+  createCard(data) {
+    // Create card logic
+  }
+
+  getCard(id) {
+    // Get card logic
+  }
+
+  updateCard(id, data) {
+    // Update card logic
+  }
+
+  deleteCard(id) {
+    // Delete card logic
+  }
+}
