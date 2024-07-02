e88becb943c968f3f04ed967d2060a51285b9113
KOSASIH <kosasihg88@gmail.com>
2024-07-02 22:19:38 +0700

    Create Footer.js

---
+++ nexus_core/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'eact';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 Pi Nexus Autonomous Banking Network</p>
+    </footer>
+  );
+};
+
+export default Footer;
