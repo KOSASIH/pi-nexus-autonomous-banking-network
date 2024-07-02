2c6c0e0599574d05e27700f5d1dbaa7ae65f6f0c
KOSASIH <kosasihg88@gmail.com>
2024-07-03 00:11:25 +0700

    Create OracleService.js

---
+++ nexus_core/oracle/OracleService.js
@@ -0,0 +1,21 @@
+class OracleService {
+  constructor() {
+    // Initialize oracle service
+  }
+
+  createOracle(data) {
+    // Create oracle service logic
+  }
+
+  getOracle(id) {
+    // Get oracle service logic
+  }
+
+  updateOracle(id, data) {
+    // Update oracle service logic
+  }
+
+  deleteOracle(id) {
+    // Delete oracle service logic
+  }
+}
