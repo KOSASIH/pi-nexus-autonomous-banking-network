27e40ccfeb59a178a9d6de7858c43009b948d2f4
KOSASIH <kosasihg88@gmail.com>
2024-07-02 22:20:08 +0700

    Create Sidebar.js

---
+++ nexus_core/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'eact';
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <ul>
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Transactions</a></li>
+        <li><a href="#">Accounts</a></li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
