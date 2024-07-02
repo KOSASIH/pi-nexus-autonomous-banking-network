6194a09d5c065a2baf06a2654e96e40274f8cc62
KOSASIH <kosasihg88@gmail.com>
2024-07-03 00:09:51 +0700

    Create OracleModel.js

---
+++ nexus_core/oracle/OracleModel.js
@@ -0,0 +1,21 @@
+class OracleModel {
+  constructor() {
+    // Initialize oracle model
+  }
+
+  createOracle(data) {
+    // Create oracle logic
+  }
+
+  getOracle(id) {
+    // Get oracle logic
+  }
+
+  updateOracle(id, data) {
+    // Update oracle logic
+  }
+
+  deleteOracle(id) {
+    // Delete oracle logic
+  }
+}
