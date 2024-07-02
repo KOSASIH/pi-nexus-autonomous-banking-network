5972a958065a16bf7078dcf47a3f996ed1100e38
KOSASIH <kosasihg88@gmail.com>
2024-07-02 21:57:33 +0700

    Create Navigation.js

---
+++ nexus_core/components/Navigation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/data">Data</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
