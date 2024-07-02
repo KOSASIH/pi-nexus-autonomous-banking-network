6184f9ef06d529617f4abe383b0c53c737b4d385
KOSASIH <kosasihg88@gmail.com>
2024-07-03 00:00:00 +0700

    Create LinkedListModel.js

---
+++ nexus_core/models/LinkedListModel.js
@@ -0,0 +1,25 @@
+class LinkedListModel {
+  constructor() {
+    // Initialize linked list model
+  }
+
+  insert(index, value) {
+    // Insert value at index logic
+  }
+
+  erase(index) {
+    // Erase node at given index logic
+  }
+
+  value_n_from_end(n) {
+    // Returns the value of the node at the nth position from the end of the list
+  }
+
+  reverse() {
+    // Reverses the list logic
+  }
+
+  remove_value(value) {
+    // Removes the first item in the list with this value logic
+  }
+}
