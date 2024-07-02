6510a7130b4853723b2cecf364a3ec189fcf61dc
KOSASIH <kosasihg88@gmail.com>
2024-07-02 23:52:52 +0700

    Create UserModel.js

---
+++ nexus_core/models/UserModel.js
@@ -0,0 +1,21 @@
+class UserModel {
+  constructor() {
+    // Initialize user model
+  }
+
+  createUser(data) {
+    // Create user logic
+  }
+
+  getUser(id) {
+    // Get user logic
+  }
+
+  updateUser(id, data) {
+    // Update user logic
+  }
+
+  deleteUser(id) {
+    // Delete user logic
+  }
+}
