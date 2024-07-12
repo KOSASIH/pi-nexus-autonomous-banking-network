52d83779f52955cdab81509dafe8e9677bea72b6
KOSASIH <kosasihg88@gmail.com>
2024-07-12 14:34:14 +0700

    Create augmented_reality.js

---
+++ nexus_banking_networks/nexus_network/advanced_banking_system/javascript/augmented_reality.js
@@ -0,0 +1,24 @@
+// augmented_reality.js
+import * as THREE from 'three';
+import * as ARJS from 'ar.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.Camera();
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.getElementById('ar-canvas'),
+  antialias: true,
+});
+
+const markerRoot = new ARJS.MarkerRoot(camera, {
+  patternUrl: 'pattern.patt',
+  markerSize: 1,
+});
+
+scene.add(markerRoot);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
