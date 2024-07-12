480562cfc56b15e444b6c21952a8fa832c5fe716
KOSASIH <kosasihg88@gmail.com>
2024-07-12 10:39:40 +0700

    Create nexus_bank_natural_language_processing.js

---
+++ nexus_banking_networks/nexus_bank/nexus_bank_natural_language_processing.js
@@ -0,0 +1,13 @@
+const natural = require('natural');
+
+const tokenizer = new natural.WordTokenizer();
+const text = "Hello, how are you?";
+const tokens = tokenizer.tokenize(text);
+console.log(tokens);
+
+const classifier = new natural.BayesClassifier();
+classifier.addDocument("I am happy.", "positive");
+classifier.addDocument("I am sad.", "negative");
+classifier.train();
+const sentiment = classifier.classify("I am happy today.");
+console.log(sentiment);
