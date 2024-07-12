b6090556da8d8f3ef876cd5ba6c6fb393e1f408b
KOSASIH <kosasihg88@gmail.com>
2024-07-12 11:02:23 +0700

    Create nexus_bank_machine_learning_tensorflow.js

---
+++ nexus_banking_networks/nexus_bank/nexus_bank_machine_learning_tensorflow.js
@@ -0,0 +1,25 @@
+const tf = require('@tensorflow/tfjs');
+
+class MachineLearning {
+    constructor() {
+        this.model = tf.sequential();
+        this.model.add(tf.layers.dense({ units: 10, inputShape: [784] }));
+        this.model.add(tf.layers.dense({ units: 10 }));
+        this.model.compile({ optimizer: tf.optimizers.adam(), loss: 'categoricalCrossentropy', metrics: ['accuracy'] });
+    }
+
+    train(X, y) {
+        this.model.fit(X, y, { epochs: 10 });
+    }
+
+    predict(X) {
+        return this.model.predict(X);
+    }
+}
+
+const ml = new MachineLearning();
+const X = tf.random.normal([100, 784]);
+const y = tf.random.uniform([100, 10]);
+ml.train(X, y);
+const predictions = ml.predict(X);
+console.log(predictions);
