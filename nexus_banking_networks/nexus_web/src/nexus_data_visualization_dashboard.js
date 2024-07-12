fa0d7605fe8031e66d5db3642e2d81c3c2e6e8af
KOSASIH <kosasihg88@gmail.com>
2024-07-12 16:14:00 +0700

    Create nexus_data_visualization_dashboard.js

---
+++ nexus_banking_networks/nexus_web/src/nexus_data_visualization_dashboard.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'eact';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'echarts';
+
+function NexusDataVisualizationDashboard() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.example.com/data')
+           .then(response => response.json())
+           .then(data => setData(data));
+    }, []);
+
+    return (
+        <LineChart width={500} height={300} data={data}>
+            <Line type="monotone" dataKey="value" stroke="#8884d8" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <Tooltip />
+        </LineChart>
+    );
+}
+
+export default NexusDataVisualizationDashboard;
