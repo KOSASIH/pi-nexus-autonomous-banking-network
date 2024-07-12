6a9497972033d220a7e0a65b781feb4d5230d546
KOSASIH <kosasihg88@gmail.com>
2024-07-12 18:59:54 +0700

    Create nexus_machine_learning_pipeline.py

---
+++ nexus_banking_networks/nexus_web/src/components/nexus_machine_learning_pipeline.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+
+class NexusMachineLearningPipeline:
+    def __init__(self):
+        self.data = pd.read_csv('data.csv')
+        self.X = self.data.drop('target', axis=1)
+        self.y = self.data['target']
+
+    def create_pipeline(self):
+        pipeline = Pipeline([
+            ('scaler', StandardScaler()),
+            ('classifier', RandomForestClassifier(n_estimators=100, random_state=42))
+        ])
+        return pipeline
+
+    def train_pipeline(self, pipeline):
+        pipeline.fit(self.X, self.y)
+
+    def predict(self, pipeline, input_data):
+        return pipeline.predict(input_data)
