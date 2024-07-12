eb4c57c67481e3d09115c09bbfeb8acb92ef91fb
KOSASIH <kosasihg88@gmail.com>
2024-07-12 10:25:58 +0700

    Create nexus_bank_security.rb

---
+++ nexus_banking_networks/nexus_bank/nexus_bank_security.rb
@@ -0,0 +1,11 @@
+require 'bcrypt'
+
+class Security
+  def self.hash_password(password)
+    BCrypt::Password.create(password)
+  end
+
+  def self.verify_password(password, hashed_password)
+    BCrypt::Password.new(hashed_password) == password
+  end
+end
