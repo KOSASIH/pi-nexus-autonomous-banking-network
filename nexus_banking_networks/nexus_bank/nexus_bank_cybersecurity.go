d6b1a302fb18167528793e4ca6a6864562a39154
KOSASIH <kosasihg88@gmail.com>
2024-07-12 10:29:01 +0700

    Create nexus_bank_cybersecurity.go

---
+++ nexus_banking_networks/nexus_bank/nexus_bank_cybersecurity.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+)
+
+func encrypt(plaintext []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return ciphertext, nil
+}
+
+func decrypt(ciphertext []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	return plaintext, err
+}
+
+func main() {
+	key := []byte("my_secret_key")
+	plaintext := []byte("Hello, World!")
+	ciphertext, err := encrypt(plaintext, key)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(base64.StdEncoding.EncodeToString(ciphertext))
+
+	decrypted, err := decrypt(ciphertext, key)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(decrypted))
+}
