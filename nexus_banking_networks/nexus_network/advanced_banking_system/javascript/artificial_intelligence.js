96ed6960d126b3d576a8babc897e4b13d423e897
KOSASIH <kosasihg88@gmail.com>
2024-07-12 14:34:42 +0700

    Create artificial_intelligence.js

---
+++ nexus_banking_networks/nexus_network/advanced_banking_system/javascript/artificial_intelligence.js
@@ -0,0 +1,14 @@
+// artificial_intelligence.js
+import * as brain from 'brain.js';
+
+const net = new brain.NeuralNetwork();
+
+net.train([
+  { input: [0, 0], output: [0] },
+  { input: [0, 1], output: [1] },
+  { input: [1, 0], output: [1] },
+  { input: [1, 1], output: [0] },
+]);
+
+const output = net.run([1, 1]);
+console.log(output);
