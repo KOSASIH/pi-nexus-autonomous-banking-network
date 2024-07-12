67760300ce66a3ac5ffb78bdad10e9294aaee020
KOSASIH <kosasihg88@gmail.com>
2024-07-12 10:35:24 +0700

    Create nexus_bank_augmented_reality.swift

---
+++ nexus_banking_networks/nexus_bank/nexus_bank_augmented_reality.swift
@@ -0,0 +1,18 @@
+import ARKit
+import UIKit
+
+class AugmentedRealityViewController: UIViewController, ARSCNViewDelegate {
+    @IBOutlet var sceneView: ARSCNView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // Add a 3D model to the scene
+        let model = SCNScene(named: "model.scn")!
+        node.addChildNode(model.rootNode)
+    }
+}
