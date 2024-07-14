52ffa74f0a2cc7491947fab90b5edcb98837b2ff
KOSASIH <kosasihg88@gmail.com>
2024-07-14 21:53:46 +0700

    Create multiparty_computation.java

---
+++ features/secure_multiparty_computation/multiparty_computation.java
@@ -0,0 +1,8 @@
+// File name: multiparty_computation.java
+import garbled_circuits.*;
+
+public class MultipartyComputation {
+    public static void main(String[] args) {
+        // Implement multiparty computation using garbled circuits here
+    }
+}
