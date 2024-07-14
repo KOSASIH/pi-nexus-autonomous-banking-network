b2806c683976e64f3cf75d87923eeb09347b56c0
KOSASIH <kosasihg88@gmail.com>
2024-07-14 21:56:11 +0700

    Create sentiment_analysis.py

---
+++ features/sentiment_analysis_market_prediction/sentiment_analysis.py
@@ -0,0 +1,9 @@
+# File name: sentiment_analysis.py
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+
+class SentimentAnalysis:
+    def __init__(self):
+        # Implement sentiment analysis using deep learning here
+        pass
