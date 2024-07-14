1f505d9d2c246be2b6472dea42f64fbca85e07a3
KOSASIH <kosasihg88@gmail.com>
2024-07-14 21:32:17 +0700

    Create decentralized_identity_management.js

---
+++ features/blockchain_identity_verification/decentralized_identity_management.js
@@ -0,0 +1,16 @@
+// File name: decentralized_identity_management.js
+const ipfs = require('ipfs-api');
+
+class DecentralizedIdentityManagement {
+    async storeIdentity(identity) {
+        const node = new ipfs({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+        const file = await node.add(identity);
+        return file.hash;
+    }
+
+    async retrieveIdentity(hash) {
+        const node = new ipfs({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+        const file = await node.cat(hash);
+        return file.toString();
+    }
+}
