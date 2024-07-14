d11a10316ddcad9f71e2468dc5d9ea6017c8b8a1
KOSASIH <kosasihg88@gmail.com>
2024-07-14 20:52:23 +0700

    Create FinancialAdvisory.js

---
+++ features/agi_financial_advisory/FinancialAdvisory.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function FinancialAdvisory() {
+  const [userInput, setUserInput] = useState('');
+  const [advice, setAdvice] = useState('');
+
+  useEffect(() => {
+    const fetchAdvice = async () => {
+      const response = await axios.post('/api/advice', { userInput });
+      setAdvice(response.data);
+    };
+    fetchAdvice();
+  }, [userInput]);
+
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Financial Advisory</h1>
+      <input type="text" value={userInput} onChange={handleInputChange} />
+      <p>{advice}</p>
+    </div>
+  );
+}
+
+export default FinancialAdvisory;
