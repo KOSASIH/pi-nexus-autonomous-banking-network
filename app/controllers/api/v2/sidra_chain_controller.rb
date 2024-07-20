918e9f2132962e1edece727ef97d9272cb3890fc
KOSASIH <kosasihg88@gmail.com>
2024-07-20 22:24:31 +0700

    Create sidra_chain_controller.rb

---
+++ app/controllers/api/v2/sidra_chain_controller.rb
@@ -0,0 +1,12 @@
+# app/controllers/api/v2/sidra_chain_controller.rb
+module Api
+  module V2
+    class SidraChainController < ApplicationController
+      def index
+        # Fetch Sidra Chain data using Sidra Chain API
+        sidra_chain_data = # ...
+        render json: sidra_chain_data
+      end
+    end
+  end
+end
