ca6c3afcdb7cd5b2184c02307bb8e98ac6da966b
KOSASIH <kosasihg88@gmail.com>
2024-07-20 22:25:19 +0700

    Create deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+# config/deploy.rb
+set :application, 'sidra_chain_integration'
+set :repo_url, 'https://github.com/KOSASIH/pi-nexus-autonomous-banking-network.git'
+
+set :deploy_to, '/var/www/sidra_chain_integration'
+set :deploy_via, :remote_cache
+
+set :rails_env, 'production'
+
+set :sidekiq_role, :app
+set :sidekiq_config, 'config/sidekiq.yml'
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :rake, 'cache:clear'
+    end
+  end
+end
