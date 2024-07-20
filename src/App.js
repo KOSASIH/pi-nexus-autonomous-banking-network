e2583f4015d08bd0aeb26487d42f1f70bd9ccabc
KOSASIH <kosasihg88@gmail.com>
2024-07-20 22:21:21 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'eact';
+import IdentityManager from './IdentityManager';
+
+function App() {
+  return (
+    <div>
+      <IdentityManager />
+    </div>
+  );
+}
+
+export default App;
