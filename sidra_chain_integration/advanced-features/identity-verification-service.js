2e69691ce2c5b1663df256e7f6221f0d0020b0eb
KOSASIH <kosasihg88@gmail.com>
2024-07-21 11:18:08 +0700

    Create identity-verification-service.js

---
+++ sidra_chain_integration/advanced-features/identity-verification-service.js
@@ -0,0 +1,31 @@
+// identity-verification-service.js
+const Web3 = require('web3');
+const identityVerificationContract = require('./identity-verification-contract');
+
+class IdentityVerificationService {
+  constructor() {
+    this.web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/YOUR_PROJECT_ID'));
+    this.contract = new this.web3.eth.Contract(identityVerificationContract.abi, identityVerificationContract.address);
+  }
+
+  async verifyUserIdentity(userId, name, email, password) {
+    const txCount = await this.web3.eth.getTransactionCount(userId);
+    const tx = {
+      from: userId,
+      to: this.contract.address,
+      value: '0',
+      gas: '20000',
+      gasPrice: '20',
+      data: this.contract.methods.verifyUserIdentity(name, email, password).encodeABI(),
+    };
+    const signedTx = await this.web3.eth.accounts.signTransaction(tx, '0x' + userId);
+    await this.web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+  }
+
+  async getUserIdentity(userId) {
+    const result = await this.contract.methods.getUserIdentity(userId).call();
+    return result;
+  }
+}
+
+module.exports = IdentityVerificationService;
