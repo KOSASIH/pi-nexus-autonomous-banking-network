eb1021ad4b691ade9817b341be9d2c388de58541
KOSASIH <kosasihg88@gmail.com>
2024-07-21 10:36:43 +0700

    Create LoanProcessingContract.py

---
+++ sidra_chain_integration/sidra_chain_sdk/contracts/LoanProcessingContract.py
@@ -0,0 +1,11 @@
+from sidra_chain_sdk.contract import Contract
+
+class LoanProcessingContract(Contract):
+    def __init__(self, network, api_key):
+        super().__init__(network, api_key)
+        self.contract_address = '0x...'
+
+    def evaluate_loan_application(self, credit_score, income, employment_history, loan_amount):
+        # Call the evaluateLoanApplication function on the smart contract
+        response = self.call_function('evaluateLoanApplication', [credit_score, income, employment_history, loan_amount])
+        return response
