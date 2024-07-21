fc525b2f20713604501e18fc4552e00304478b80
KOSASIH <kosasihg88@gmail.com>
2024-07-21 10:44:47 +0700

    Create sentiment_analysis.py

---
+++ sidra_chain_integration/natural_language_processing/sentiment_analysis.py
@@ -0,0 +1,9 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+class SentimentAnalyzer:
+    def __init__(self):
+        self.sia = SentimentIntensityAnalyzer()
+
+    def analyze(self, text):
+        return self.sia.polarity_scores(text)
