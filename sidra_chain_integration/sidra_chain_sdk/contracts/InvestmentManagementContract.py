54b37f0d7be2c2535c44154f6b8ef11ee227e39a
KOSASIH <kosasihg88@gmail.com>
2024-07-21 10:37:25 +0700

    Create InvestmentManagementContract.py

---
+++ sidra_chain_integration/sidra_chain_sdk/contracts/InvestmentManagementContract.py
@@ -0,0 +1,11 @@
+from sidra_chain_sdk.contract import Contract
+
+class InvestmentManagementContract(Contract):
+    def __init__(self, network, api_key):
+        super().__init__(network, api_key)
+        self.contract_address = '0x...'
+
+    def analyze_market_trends(self):
+        # Call the analyzeMarketTrends function on the smart contract
+        response = self.call_function('analyzeMarketTrends')
+        return response
