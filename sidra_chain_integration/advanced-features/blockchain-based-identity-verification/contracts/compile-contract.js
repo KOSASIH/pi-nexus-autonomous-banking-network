d861019b770af6dd10051ce4256d9986154444c3
KOSASIH <kosasihg88@gmail.com>
2024-07-21 11:21:38 +0700

    Create compile-contract.js

---
+++ sidra_chain_integration/advanced-features/blockchain-based-identity-verification/contracts/compile-contract.js
@@ -0,0 +1,7 @@
+// compile-contract.js
+const solc = require('solidity-compiler');
+
+const contractSource = fs.readFileSync('identity-verification-contract.sol', 'utf8');
+const compiledContract = solc.compile(contractSource, 'identityVerificationContract');
+
+fs.writeFileSync('identity-verification-contract.json', JSON.stringify(compiledContract));
