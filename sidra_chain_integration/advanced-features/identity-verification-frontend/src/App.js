e856551a896d7d2d9d0284a6b33ea10d948f5c83
KOSASIH <kosasihg88@gmail.com>
2024-07-21 11:52:30 +0700

    Create App.js

---
+++ sidra_chain_integration/advanced-features/identity-verification-frontend/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import VerifyUser from './VerifyUser';
+
+function App() {
+  return (
+    <div>
+      <VerifyUser />
+    </div>
+  );
+}
+
+export default App;
