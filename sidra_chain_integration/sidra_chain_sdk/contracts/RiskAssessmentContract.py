849c69807a1180afca59302806593d60e0465dce
KOSASIH <kosasihg88@gmail.com>
2024-07-21 10:38:10 +0700

    Create RiskAssessmentContract.py

---
+++ sidra_chain_integration/sidra_chain_sdk/contracts/RiskAssessmentContract.py
@@ -0,0 +1,11 @@
+from sidra_chain_sdk.contract import Contract
+
+class RiskAssessmentContract(Contract):
+    def __init__(self, network, api_key):
+        super().__init__(network, api_key)
+        self.contract_address = '0x...'
+
+    def identify_risks(self, loan_application):
+        # Call the identifyRisks function on the smart contract
+        response = self.call_function('identifyRisks', [loan_application])
+        return response
