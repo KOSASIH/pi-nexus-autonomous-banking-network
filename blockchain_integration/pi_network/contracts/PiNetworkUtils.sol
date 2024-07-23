486e6c0cd48a04bdc1e7db57163a08f02f8bcd44
KOSASIH <kosasihg88@gmail.com>
2024-07-23 10:24:35 +0700

    Create PiNetworkUtils.sol

---
+++ blockchain_integration/pi_network/contracts/PiNetworkUtils.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.0;
+
+library PiNetworkUtils {
+    function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+}
