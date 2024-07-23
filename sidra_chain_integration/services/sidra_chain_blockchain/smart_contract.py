bee125be777e599f8df2b1969e89e824081aabf1
KOSASIH <kosasihg88@gmail.com>
2024-07-23 14:59:46 +0700

    Create smart_contract.py

---
+++ sidra_chain_integration/services/sidra_chain_blockchain/smart_contract.py
@@ -0,0 +1,7 @@
+class SmartContract:
+    def __init__(self):
+        self.blockchain = Blockchain()
+
+    def execute_contract(self, transaction: dict):
+        # Execute smart contract logic here
+        pass
