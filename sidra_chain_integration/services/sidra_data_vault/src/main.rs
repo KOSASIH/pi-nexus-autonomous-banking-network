5a826db573763fb7ca59e6ba002784a0662a43f4
KOSASIH <kosasihg88@gmail.com>
2024-07-23 16:27:47 +0700

    Create main.rs

---
+++ sidra_chain_integration/services/sidra_data_vault/src/main.rs
@@ -0,0 +1,32 @@
+// sidra_data_vault/src/main.rs
+use ipfs::{Ipfs, IpfsApi};
+use sodium::{Key, Box_};
+
+struct DataVault {
+    ipfs: Ipfs,
+    key: Key,
+}
+
+impl DataVault {
+    fn new(ipfs: Ipfs, key: Key) -> Self {
+        DataVault { ipfs, key }
+    }
+
+    fn store_data(&self, data: Vec<u8>) -> String {
+        // Encrypt the data using the key
+        let encrypted_data = Box_::seal(data, &self.key);
+
+        // Store the encrypted data in IPFS
+        let cid = self.ipfs.add(encrypted_data).unwrap();
+        cid.to_string()
+    }
+
+    fn retrieve_data(&self, cid: String) -> Vec<u8> {
+        // Retrieve the encrypted data from IPFS
+        let encrypted_data = self.ipfs.get(cid).unwrap();
+
+        // Decrypt the data using the key
+        let decrypted_data = Box_::open(encrypted_data, &self.key).unwrap();
+        decrypted_data
+    }
+}
