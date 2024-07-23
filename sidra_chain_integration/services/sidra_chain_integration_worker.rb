c0363387f2329c2085875841c75c74a448bcb5d2
KOSASIH <kosasihg88@gmail.com>
2024-07-23 16:52:29 +0700

    Create sidra_chain_integration_worker.rb

---
+++ sidra_chain_integration/services/sidra_chain_integration_worker.rb
@@ -0,0 +1,17 @@
+require "sidekiq"
+
+class SidraChainIntegrationWorker
+  include Sidekiq::Worker
+
+  def perform(chain_id, chain_name)
+    # Integrate with Sidra Chain API
+    response = HTTParty.post("https://sidra-chain-api.com/integrate", 
+                             body: { chain_id: chain_id, chain_name: chain_name }.to_json, 
+                             headers: { "Content-Type" => "application/json" })
+    if response.success?
+      # Process successful response
+    else
+      # Handle error response
+    end
+  end
+end
