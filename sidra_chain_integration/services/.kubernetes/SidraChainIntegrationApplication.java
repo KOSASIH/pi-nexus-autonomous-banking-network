eb0c2cf5c3f94f7de061b536738ca534c41df34f
KOSASIH <kosasihg88@gmail.com>
2024-07-23 15:15:54 +0700

    Create SidraChainIntegrationApplication.java

---
+++ sidra_chain_integration/services/.kubernetes/SidraChainIntegrationApplication.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SidraChainIntegrationApplication {
+
+    @GetMapping("/sidra-chain-integration")
+    public String getSidraChainIntegration() {
+        return "Sidra Chain Integration Service";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SidraChainIntegrationApplication.class, args);
+    }
+}
