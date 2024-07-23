55ba6aef797a10c0aa5bca33510aeac1afc5844f
KOSASIH <kosasihg88@gmail.com>
2024-07-23 09:35:42 +0700

    Update network.js

--- blockchain_integration/pi_network/utils/network.js
+++ blockchain_integration/pi_network/utils/network.js
@@ -1,7 +1,32 @@
+const axios = require('axios');
+const { api_url, api_key, api_secret } = require('./config.json');
+
 const network = {
-  baseUrl: 'https://minepi.com',
-  nodeUrl: 'https://node1.pi.network',
-  walletUrl: 'https://wallet.pi.network'
+  async getApiUrl() {
+    return api_url;
+  },
+
+  async getApiKey() {
+    return api_key;
+  },
+
+  async getApiSecret() {
+    return api_secret;
+  },
+
+  async makeApiRequest(endpoint, method, data) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${api_key}`
+    };
+
+    try {
+      const response = await axios[method](`${api_url}/${endpoint}`, data, { headers });
+      return response.data;
+    } catch (error) {
+      throw new Error(`API request failed: ${error.message}`);
+    }
+  }
 };
 
 module.exports = network;
