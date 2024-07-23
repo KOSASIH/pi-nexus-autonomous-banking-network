2bb27429d36ebaa862f5418b6d629dec563a847e
KOSASIH <kosasihg88@gmail.com>
2024-07-23 10:33:47 +0700

    Create 5_deploy_oracle_contracts.js

---
+++ blockchain_integration/pi_network/migrations/5_deploy_oracle_contracts.js
@@ -0,0 +1,7 @@
+const PiNetworkOracle = artifacts.require("PiNetworkOracle");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(PiNetworkOracle);
+  const piNetworkOracle = await PiNetworkOracle.deployed();
+  console.log(`PiNetworkOracle deployed at ${piNetworkOracle.address}`);
+};
