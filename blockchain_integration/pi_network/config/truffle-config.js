e7366bfa4e489467c55127cf2814e8cd35b5b001
KOSASIH <kosasihg88@gmail.com>
2024-07-23 10:40:40 +0700

    Create truffle-config.js

---
+++ blockchain_integration/pi_network/config/truffle-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // ...
+  contracts: [
+    "PiNetwork.sol",
+    "PiNetworkFactory.sol",
+    "PiNetworkLogic.sol",
+    "PiNetworkOracle.sol",
+    "PiNetworkRouter.sol",
+    "PiNetworkData.sol",
+    "PiNetworkUI.sol"
+  ],
+  // ...
+};
