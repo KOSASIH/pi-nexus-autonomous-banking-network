4161857c01584b000afdc4a2ded0b863507132b8
KOSASIH <kosasihg88@gmail.com>
2024-07-23 10:52:42 +0700

    Create PiNetworkUI.stories.js

---
+++ blockchain_integration/pi_network/src/PiNetworkUI.stories.js
@@ -0,0 +1,8 @@
+import React from 'eact';
+import { storiesOf } from '@eact/eact-storybook-loader';
+import PiNetworkUI from './PiNetworkUI';
+
+storiesOf('PiNetworkUI', module)
+  .add('default', () => (
+    <PiNetworkUI />
+  ));
