df842923c9dbd7d4e9618272f925a43529f462ca
KOSASIH <kosasihg88@gmail.com>
2024-07-23 10:10:23 +0700

    Create 2_deploy_contracts.js

---
+++ blockchain_integration/pi_network/migrations/2_deploy_contracts.js
@@ -0,0 +1,8 @@
+const PiNetworkFactory = artifacts.require("PiNetworkFactory");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(PiNetworkFactory);
+  const piNetworkFactory = await PiNetworkFactory.deployed();
+  const piNetworkAddress = await piNetworkFactory.createPiNetwork();
+  console.log(`PiNetwork deployed at ${piNetworkAddress}`);
+};
