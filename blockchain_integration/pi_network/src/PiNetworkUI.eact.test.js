5059b4acd6af157561ee646b4d866972d018d009
KOSASIH <kosasihg88@gmail.com>
2024-07-23 10:57:16 +0700

    Create PiNetworkUI.eact.test.js

---
+++ blockchain_integration/pi_network/src/PiNetworkUI.eact.test.js
@@ -0,0 +1,9 @@
+import { shallow } from 'eact-testing-library';
+import PiNetworkUI from './PiNetworkUI';
+
+describe('PiNetworkUI', () => {
+  it('renders the Pi Network UI component', () => {
+    const wrapper = shallow(<PiNetworkUI />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
