3b86efc2afcb5cf8ee311d792022990fdcc029de
KOSASIH <kosasihg88@gmail.com>
2024-07-23 14:31:50 +0700

    Create security.py

---
+++ blockchain_integration/pi_network/core/security.py
@@ -0,0 +1,10 @@
+import hashlib
+
+def encrypt_data(data):
+    # Use SHA-256 encryption
+    encrypted_data = hashlib.sha256(data.encode()).hexdigest()
+    return encrypted_data
+
+def decrypt_data(encrypted_data):
+    # Use SHA-256 decryption (not possible, but you can use other encryption methods)
+    return encrypted_data
