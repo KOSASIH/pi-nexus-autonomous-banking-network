36f7f3aeaf297546ca62b4c33c862d77972cc1df
KOSASIH <kosasihg88@gmail.com>
2024-07-23 14:59:00 +0700

    Create blockchain.py

---
+++ sidra_chain_integration/services/sidra_chain_blockchain/blockchain.py
@@ -0,0 +1,12 @@
+import hashlib
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+
+    def add_block(self, block: dict):
+        self.chain.append(block)
+        self.chain[-1]["hash"] = self._calculate_hash(self.chain[-1])
+
+    def _calculate_hash(self, block: dict):
+        return hashlib.sha256(str(block).encode()).hexdigest()
