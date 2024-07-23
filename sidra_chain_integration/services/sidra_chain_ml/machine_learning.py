26680fc690fcc31ea5fd6c0bbd4e108f15c69db4
KOSASIH <kosasihg88@gmail.com>
2024-07-23 15:00:23 +0700

    Create machine_learning.py

---
+++ sidra_chain_integration/services/sidra_chain_ml/machine_learning.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+class MachineLearning:
+    def __init__(self):
+        self.model = RandomForestClassifier()
+
+    def train_model(self, transaction: dict):
+        # Train machine learning model here
+        pass
