b7440d3f4a43f2af440211262cae8509e333c3ff
KOSASIH <kosasihg88@gmail.com>
2024-07-23 15:13:57 +0700

    Create app.py

---
+++ sidra_chain_integration/services/.kubernetes/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+class SidraChainIntegration(Resource):
+    def get(self):
+        return jsonify({'message': 'Sidra Chain Integration Service'})
+
+api.add_resource(SidraChainIntegration, '/sidra-chain-integration')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
