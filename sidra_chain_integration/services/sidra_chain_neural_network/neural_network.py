bc0ddb004d02e1980de93f83e316d498817d8058
KOSASIH <kosasihg88@gmail.com>
2024-07-23 16:37:52 +0700

    Create neural_network.py

---
+++ sidra_chain_integration/services/sidra_chain_neural_network/neural_network.py
@@ -0,0 +1,21 @@
+# sidra_chain_neural_network/neural_network.py
+import tensorflow as tf
+from tensorflow import keras
+
+class NeuralNetwork:
+    def __init__(self):
+        self.model = keras.Sequential([
+            keras.layers.Dense(64, activation="relu", input_shape=(10,)),
+            keras.layers.Dense(32, activation="relu"),
+            keras.layers.Dense(1, activation="sigmoid")
+        ])
+        self.model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+    def train(self, data):
+        self.model.fit(data, epochs=10)
+
+    def predict(self, data):
+        return self.model.predict(data)
+
+    def evaluate(self, data):
+        return self.model.evaluate(data)
