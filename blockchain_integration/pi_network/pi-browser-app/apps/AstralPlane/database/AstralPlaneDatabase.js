a161452e72310deb2e45162a5cee5f335d192b89
KOSASIH <kosasihg88@gmail.com>
2024-07-25 02:46:10 +0700

    Update AstralPlaneDatabase.js

--- blockchain_integration/pi_network/pi-browser-app/apps/AstralPlane/database/AstralPlaneDatabase.js
+++ blockchain_integration/pi_network/pi-browser-app/apps/AstralPlane/database/AstralPlaneDatabase.js
@@ -1,32 +1,72 @@
-import * as MongoDB from 'ongodb';
+import * as MongoDB from 'mongodb';
 
 class AstralPlaneDatabase {
   constructor() {
     this.client = new MongoDB.MongoClient('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
     this.db = this.client.db('astralplane');
-    this.assetsCollection = this.db.collection('assets');
-  }
-
-  async getAssets() {
-    const assets = await this.assetsCollection.find().toArray();
-    return assets;
-  }
-
-  async getAsset(id) {
-    const asset = await this.assetsCollection.findOne({ _id: id });
-    return asset;
   }
 
   async createAsset(asset) {
-    await this.assetsCollection.insertOne(asset);
+    const collection = this.db.collection('assets');
+    await collection.insertOne(asset);
   }
 
-  async updateAsset(id, asset) {
-    await this.assetsCollection.updateOne({ _id: id }, { $set: asset });
+  async getAsset(assetId) {
+    const collection = this.db.collection('assets');
+    const asset = await collection.findOne({ _id: assetId });
+    return asset;
   }
 
-  async deleteAsset(id) {
-    await this.assetsCollection.deleteOne({ _id: id });
+  async updateAsset(assetId, updates) {
+    const collection = this.db.collection('assets');
+    await collection.updateOne({ _id: assetId }, { $set: updates });
+  }
+
+  async deleteAsset(assetId) {
+    const collection = this.db.collection('assets');
+    await collection.deleteOne({ _id: assetId });
+  }
+
+  async getAssets(filter = {}) {
+    const collection = this.db.collection('assets');
+    const assets = await collection.find(filter).toArray();
+    return assets;
+  }
+
+  async createUser(user) {
+    const collection = this.db.collection('users');
+    await collection.insertOne(user);
+  }
+
+  async getUser(userId) {
+    const collection = this.db.collection('users');
+    const user = await collection.findOne({ _id: userId });
+    return user;
+  }
+
+  async updateUser(userId, updates) {
+    const collection = this.db.collection('users');
+    await collection.updateOne({ _id: userId }, { $set: updates });
+  }
+
+  async deleteUser(userId) {
+    const collection = this.db.collection('users');
+    await collection.deleteOne({ _id: userId });
+  }
+
+  async getTransactions(filter = {}) {
+    const collection = this.db.collection('transactions');
+    const transactions = await collection.find(filter).toArray();
+    return transactions;
+  }
+
+  async createTransaction(transaction) {
+    const collection = this.db.collection('transactions');
+    await collection.insertOne(transaction);
+  }
+
+  async close() {
+    await this.client.close();
   }
 }
 
