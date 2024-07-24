6002370504961a706b9493bef02abe5802fff6e7
KOSASIH <kosasihg88@gmail.com>
2024-07-24 13:31:48 +0700

    Create ar.actions.js

---
+++ blockchain_integration/pi_network/pi_coin_voting_system/actions/ar.actions.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+export const getARDashboardData = () => {
+  return (dispatch) => {
+    dispatch({ type: 'AR_DATA_LOADING' });
+    axios.get('https://example.com/ar-dashboard-data')
+      .then((response) => {
+        const data = response.data;
+        dispatch({ type: 'AR_DATA_RECEIVED', data });
+      })
+      .catch((error) => {
+        dispatch({ type: 'AR_DATA_ERROR', error });
+      });
+  };
+};
+
+export const getARModel = (id) => {
+  return (dispatch) => {
+    dispatch({ type: 'AR_MODEL_LOADING' });
+    axios.get(`https://example.com/ar-models/${id}`)
+      .then((response) => {
+        const model = response.data;
+        dispatch({ type: 'AR_MODEL_RECEIVED', model });
+      })
+      .catch((error) => {
+        dispatch({ type: 'AR_MODEL_ERROR', error });
+      });
+  };
+};
+
+export const uploadARModel = (model) => {
+  return (dispatch) => {
+    dispatch({ type: 'AR_MODEL_UPLOADING' });
+    axios.post('https://example.com/ar-models', model)
+      .then((response) => {
+        const model = response.data;
+        dispatch({ type: 'AR_MODEL_UPLOADED', model });
+      })
+      .catch((error) => {
+        dispatch({ type: 'AR_MODEL_UPLOAD_ERROR', error });
+      });
+  };
+};
