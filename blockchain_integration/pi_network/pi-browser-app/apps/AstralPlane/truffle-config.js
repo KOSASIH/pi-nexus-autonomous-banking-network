91f2ebaf682cd8c36c59a9312befc556dcafe7ea
KOSASIH <kosasihg88@gmail.com>
2024-07-25 02:08:04 +0700

    Create truffle-config.js

---
+++ blockchain_integration/pi_network/pi-browser-app/apps/AstralPlane/truffle-config.js
@@ -0,0 +1,53 @@
+// truffle-config.js
+module.exports = {
+  // Network settings
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 6721975,
+      gasPrice: 20000000000,
+    },
+    test: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 6721975,
+      gasPrice: 20000000000,
+    },
+    mainnet: {
+      provider: () => new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/YOUR_PROJECT_ID"),
+      network_id: 1,
+      gas: 6721975,
+      gasPrice: 20000000000,
+    },
+  },
+
+  // Compiler settings
+  compilers: {
+    solc: {
+      version: "0.8.0",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
+
+  // Migrations settings
+  migrations: {
+    deployer: "truffle-deployer",
+  },
+
+  // Plugins settings
+  plugins: ["truffle-plugin-verify"],
+
+  // API settings
+  api: {
+    host: "127.0.0.1",
+    port: 8546,
+  },
+};
