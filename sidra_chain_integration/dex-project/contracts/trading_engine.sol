ca9c6d697c323725007417019d42612ad3899f1f
KOSASIH <kosasihg88@gmail.com>
2024-07-24 14:28:22 +0700

    Create trading_engine.sol

---
+++ sidra_chain_integration/dex-project/contracts/trading_engine.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract TradingEngine {
+    // Trading engine logic and functions
+}
