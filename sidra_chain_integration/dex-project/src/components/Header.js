78c0ebc86d7f5b6b21a634cfa55cb69c64c65b93
KOSASIH <kosasihg88@gmail.com>
2024-07-24 14:33:28 +0700

    Create Header.js

---
+++ sidra_chain_integration/dex-project/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">DEX</a>
+        </nav>
+    );
+};
+
+export default Header;
