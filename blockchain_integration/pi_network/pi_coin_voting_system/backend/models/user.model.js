ee83527e25e45b4caf36ee4e0157aef10380b541
KOSASIH <kosasihg88@gmail.com>
2024-07-24 10:45:30 +0700

    Create user.model.js

---
+++ blockchain_integration/pi_network/pi_coin_voting_system/backend/models/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  votes: [{ type: Number }]
+});
+
+userSchema.pre('save', function(next) {
+  const user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+});
+
+module.exports = mongoose.model('User', userSchema);
