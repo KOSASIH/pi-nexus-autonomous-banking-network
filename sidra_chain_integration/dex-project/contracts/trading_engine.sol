9b9b0152736d60dbc45b5224afa635454d8299f6
KOSASIH <kosasihg88@gmail.com>
2024-07-24 14:37:25 +0700

    Update trading_engine.sol

--- sidra_chain_integration/dex-project/contracts/trading_engine.sol
+++ sidra_chain_integration/dex-project/contracts/trading_engine.sol
@@ -2,7 +2,81 @@ pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/utils/ReentrancyGuard.sol";
 
-contract TradingEngine {
-    // Trading engine logic and functions
+contract TradingEngine is Ownable, ReentrancyGuard {
+    // Mapping of asset pairs to their respective liquidity pools
+    mapping(address => mapping(address => address)) public liquidityPools;
+
+    // Mapping of asset pairs to their respective order books
+    mapping(address => mapping(address => OrderBook)) public orderBooks;
+
+    // Event emitted when a new order is placed
+    event NewOrder(address indexed asset, address indexed maker, uint256 amount, uint256 price);
+
+    // Event emitted when an order is cancelled
+    event CancelOrder(address indexed asset, address indexed maker, uint256 amount);
+
+    // Event emitted when a trade is executed
+    event Trade(address indexed asset, address indexed maker, address indexed taker, uint256 amount, uint256 price);
+
+    // Function to place a new order
+    function placeOrder(address asset, uint256 amount, uint256 price) public {
+        // Check if the asset is supported
+        require(liquidityPools[asset] != address(0), "Asset not supported");
+
+        // Create a new order
+        Order memory order = Order(asset, msg.sender, amount, price);
+
+        // Add the order to the order book
+        orderBooks[asset][msg.sender].push(order);
+
+        // Emit the NewOrder event
+        emit NewOrder(asset, msg.sender, amount, price);
+    }
+
+    // Function to cancel an order
+    function cancelOrder(address asset, uint256 amount) public {
+        // Check if the asset is supported
+        require(liquidityPools[asset] != address(0), "Asset not supported");
+
+        // Find the order to cancel
+        Order memory order = orderBooks[asset][msg.sender][amount];
+
+        // Remove the order from the order book
+        orderBooks[asset][msg.sender].remove(order);
+
+        // Emit the CancelOrder event
+        emit CancelOrder(asset, msg.sender, amount);
+    }
+
+    // Function to execute a trade
+    function executeTrade(address asset, address maker, address taker, uint256 amount, uint256 price) public {
+        // Check if the asset is supported
+        require(liquidityPools[asset] != address(0), "Asset not supported");
+
+        // Check if the maker and taker have sufficient balance
+        require(maker.balance >= amount, "Maker does not have sufficient balance");
+        require(taker.balance >= amount, "Taker does not have sufficient balance");
+
+        // Execute the trade
+        maker.transfer(taker, amount);
+        taker.transfer(maker, amount * price);
+
+        // Emit the Trade event
+        emit Trade(asset, maker, taker, amount, price);
+    }
+}
+
+// Order struct
+struct Order {
+    address asset;
+    address maker;
+    uint256 amount;
+    uint256 price;
+}
+
+// OrderBook struct
+struct OrderBook {
+    Order[] orders;
 }
