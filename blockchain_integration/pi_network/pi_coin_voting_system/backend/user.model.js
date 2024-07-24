50c86d028baeebd59b1a27c1804f744752d72346
KOSASIH <kosasihg88@gmail.com>
2024-07-24 10:39:35 +0700

    Create user.model.js

---
+++ blockchain_integration/pi_network/pi_coin_voting_system/backend/user.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  votes: [{ type: Number }]
+});
+
+module.exports = mongoose.model('User', userSchema);
