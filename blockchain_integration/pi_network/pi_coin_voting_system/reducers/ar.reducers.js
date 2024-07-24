5794ec768e9441b518840c54aa75eb92e8bfd035
KOSASIH <kosasihg88@gmail.com>
2024-07-24 13:32:32 +0700

    Create ar.reducers.js

---
+++ blockchain_integration/pi_network/pi_coin_voting_system/reducers/ar.reducers.js
@@ -0,0 +1,31 @@
+const initialState = {
+  data: [],
+  model: null,
+  loading: false,
+  error: null
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'AR_DATA_LOADING':
+      return { ...state, loading: true };
+    case 'AR_DATA_RECEIVED':
+      return { ...state, data: action.data, loading: false };
+    case 'AR_DATA_ERROR':
+      return { ...state, error: action.error, loading: false };
+    case 'AR_MODEL_LOADING':
+      return { ...state, loading: true };
+    case 'AR_MODEL_RECEIVED':
+      return { ...state, model: action.model, loading: false };
+    case 'AR_MODEL_ERROR':
+      return { ...state, error: action.error, loading: false };
+    case 'AR_MODEL_UPLOADING':
+      return { ...state, loading: true };
+    case 'AR_MODEL_UPLOADED':
+      return { ...state, model: action.model, loading: false };
+    case 'AR_MODEL_UPLOAD_ERROR':
+      return { ...state, error: action.error, loading: false };
+    default:
+      return state;
+  }
+};
