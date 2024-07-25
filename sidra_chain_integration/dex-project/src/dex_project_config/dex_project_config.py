485c49fc4e10d32fe7d7dfa743c37f2b5c2a0148
KOSASIH <kosasihg88@gmail.com>
2024-07-25 21:28:25 +0700

    Create dex_project_config.py

---
+++ sidra_chain_integration/dex-project/src/dex_project_config/dex_project_config.py
@@ -0,0 +1,7 @@
+# dex_project_config.py
+class DexProjectConfig:
+    def __init__(self):
+        self.api_key = 'YOUR_API_KEY'
+        self.web3_provider = 'https://mainnet.infura.io/v3/YOUR_PROJECT_ID'
+        self.contract_address = '0x...SidraChainContractAddress...'
+        self.abi = [...]
