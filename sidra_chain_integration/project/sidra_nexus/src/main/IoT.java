5396da2bf625ca3f35a6758dccb85ec5e1b618a7
KOSASIH <kosasihg88@gmail.com>
2024-07-25 15:23:12 +0700

    Create IoT.java

---
+++ sidra_chain_integration/project/sidra_nexus/src/main/IoT.java
@@ -0,0 +1,13 @@
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class IoT {
+    public static void main(String[] args) {
+        // Set up an MQTT client
+        MqttClient client = new MqttClient("tcp://localhost:1883", "client");
+
+        // Publish a message
+        MqttMessage message = new MqttMessage("Hello, world!".getBytes());
+        client.publish("topic", message);
+    }
+}
