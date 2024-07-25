cb8a6639bd594ec8ceb04761dc517cc890e07b24
KOSASIH <kosasihg88@gmail.com>
2024-07-25 15:07:31 +0700

    Create UserRepository.java

---
+++ sidra_chain_integration/project/sidra_nexus/src/main/java/com/sidra/nexus/examples/UserRepository.java
@@ -0,0 +1,8 @@
+package com.sidra.nexus.repository;
+
+import com.sidra.nexus.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
