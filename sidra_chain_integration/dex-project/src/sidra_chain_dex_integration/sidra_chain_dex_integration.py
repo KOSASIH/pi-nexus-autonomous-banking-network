8e52b899e98bd20d4efae93e1a2198a3088be318
KOSASIH <kosasihg88@gmail.com>
2024-07-25 21:25:43 +0700

    Update sidra_chain_dex_integration.py

--- sidra_chain_integration/dex-project/src/sidra_chain_dex_integration/sidra_chain_dex_integration.py
+++ sidra_chain_integration/dex-project/src/sidra_chain_dex_integration/sidra_chain_dex_integration.py
@@ -1,9 +1,13 @@
 # sidra_chain_dex_integration.py
 import requests
+import pandas as pd
+from web3 import Web3
 
 class SidraChainDexIntegration:
-    def __init__(self, api_key):
+    def __init__(self, api_key, web3_provider):
         self.api_key = api_key
+        self.web3_provider = web3_provider
+        self.web3 = Web3(Web3.HTTPProvider(self.web3_provider))
 
     def get_dex_data(self):
         response = requests.get('https://api.sidrachain.com/dex/data', headers={'Authorization': f'Bearer {self.api_key}'})
@@ -11,4 +15,18 @@ class SidraChainDexIntegration:
 
     def integrate_dex_data(self, data):
         # Integrate the DEX data with the Sidra Chain
-        pass
+        dex_data_df = pd.DataFrame(data)
+        self.web3.eth.contract(address='0x...SidraChainContractAddress...', abi=[...]).functions.integrateDexData(dex_data_df.to_dict('records'))
+
+    def get_sidra_chain_data(self):
+        # Get data from the Sidra Chain
+        contract = self.web3.eth.contract(address='0x...SidraChainContractAddress...', abi=[...])
+        data = contract.functions.getData().call()
+        return data
+
+    def sync_dex_data_with_sidra_chain(self):
+        # Sync DEX data with the Sidra Chain
+        dex_data = self.get_dex_data()
+        self.integrate_dex_data(dex_data)
+        sidra_chain_data = self.get_sidra_chain_data()
+        return sidra_chain_data
