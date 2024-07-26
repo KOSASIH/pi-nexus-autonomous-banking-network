d54ccce1c944dfd3633de818ca3ea56702d014da
KOSASIH <kosasihg88@gmail.com>
2024-07-26 16:29:39 +0700

    Update index.js

--- sidra_chain_integration/dex-project/dex/index.js
+++ sidra_chain_integration/dex-project/dex/index.js
@@ -1,1 +1,30 @@
-console.log('Welcome to Sidra Chain DEX!');
+import Web3 from 'web3';
+import { SidraChainSDK } from 'sidra-chain-sdk';
+import { DEXContract } from './contracts/DEXContract';
+import { TokenContract } from './contracts/TokenContract';
+import { OrderBook } from './orderBook';
+import { MarketData } from './marketData';
+
+const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.sidra.chain'));
+const sidraChainSDK = new SidraChainSDK(web3);
+
+const dexContract = new DEXContract(sidraChainSDK, '0x...DEXContractAddress...');
+const tokenContract = new TokenContract(sidraChainSDK, '0x...TokenContractAddress...');
+
+const orderBook = new OrderBook(dexContract);
+const marketData = new MarketData(dexContract, tokenContract);
+
+async function start() {
+  await dexContract.deployed();
+  await tokenContract.deployed();
+
+  console.log('DEX Contract deployed at:', dexContract.address);
+  console.log('Token Contract deployed at:', tokenContract.address);
+
+  orderBook.start();
+  marketData.start();
+
+  console.log('Sidra Chain DEX started!');
+}
+
+start();
