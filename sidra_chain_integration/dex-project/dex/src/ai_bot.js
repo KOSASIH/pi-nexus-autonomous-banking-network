7361ab0312bc011aba8068bdb247bb4735d4d45a
KOSASIH <kosasihg88@gmail.com>
2024-07-26 21:28:41 +0700

    Update ai_bot.js

--- sidra_chain_integration/dex-project/dex/src/ai_bot.js
+++ sidra_chain_integration/dex-project/dex/src/ai_bot.js
@@ -1,10 +1,12 @@
 import { SidraChain } from '../sidra-chain';
 import { TensorFlow } from 'tensorflow';
+import { NaturalLanguageProcessing } from 'nlp-js';
 
 class AIBot {
   constructor(sidraChain) {
     this.sidraChain = sidraChain;
     this.tensorFlow = new TensorFlow();
+    this.nlp = new NaturalLanguageProcessing();
   }
 
   async start() {
@@ -12,6 +14,10 @@ class AIBot {
     const model = await this.tensorFlow.loadModel('ai-bot-model');
     // Start trading bot
     this.sidraChain.startTradingBot(model);
+    // Natural language processing-powered market analysis
+    const marketText = await this.sidraChain.getMarketText();
+    const sentiment = await this.nlp.analyzeSentiment(marketText);
+    console.log(`Market sentiment: ${sentiment}`);
   }
 }
 
