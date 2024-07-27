c717ce7b5a61821e199e2342bcdb7066d02b596c
KOSASIH <kosasihg88@gmail.com>
2024-07-27 09:49:41 +0700

    Create KosasihUniversalisCrypto.sol

---
+++ blockchain_integration/pi_network/pi-network-interoperability/bridge-contracts/kosasih-universalis/lib/KosasihUniversalisCrypto.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+library KosasihUniversalisCrypto {
+    function encrypt(bytes _data, bytes _key) internal pure returns (bytes) {
+        // Encrypt the data using the provided key
+        // ...
+    }
+
+    function decrypt(bytes _data, bytes _key) internal pure returns (bytes) {
+        // Decrypt the data using the provided key
+        // ...
+    }
+}
