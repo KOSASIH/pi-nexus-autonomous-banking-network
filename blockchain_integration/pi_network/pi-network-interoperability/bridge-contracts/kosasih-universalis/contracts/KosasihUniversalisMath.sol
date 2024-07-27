b42358fcbbf3441cd2b4d1fcb05e713b4be10b76
KOSASIH <kosasihg88@gmail.com>
2024-07-27 13:18:23 +0700

    Create KosasihUniversalisMath.sol

---
+++ blockchain_integration/pi_network/pi-network-interoperability/bridge-contracts/kosasih-universalis/contracts/KosasihUniversalisMath.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.0;
+
+library KosasihUniversalisMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a * b;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    }
+}
