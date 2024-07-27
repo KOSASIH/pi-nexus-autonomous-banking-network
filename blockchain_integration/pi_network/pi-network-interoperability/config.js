4d70b466c65e187ffd5451af176375e6b1fd73bd
KOSASIH <kosasihg88@gmail.com>
2024-07-27 07:02:01 +0700

    Create config.js

---
+++ blockchain_integration/pi_network/pi-network-interoperability/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  bridgeContractAddress: '0x...',
+  crossChainMessageContractAddress: '0x...',
+  atomicSwapContractAddress: '0x...',
+  infuraProjectId: 'YOUR_PROJECT_ID',
+  privateKey: 'YOUR_PRIVATE_KEY',
+};
