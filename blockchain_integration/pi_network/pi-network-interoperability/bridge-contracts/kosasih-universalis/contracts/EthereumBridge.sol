18e23a00620c916143ce790aeff9d65090d0a878
KOSASIH <kosasihg88@gmail.com>
2024-07-27 09:54:33 +0700

    Create EthereumBridge.sol

---
+++ blockchain_integration/pi_network/pi-network-interoperability/bridge-contracts/kosasih-universalis/contracts/EthereumBridge.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/utils/Address.sol";
+
+contract EthereumBridge {
+    address public kosasihUniversalisBridge;
+
+    constructor(address _kosasihUniversalisBridge) public {
+        kosasihUniversalisBridge = _kosasihUniversalisBridge;
+    }
+
+    function transferTokens(address _token, address _from, address _to, uint256 _amount) public {
+        // Call the Kosasih Universalis bridge to transfer tokens
+        KosasihUniversalisBridge(kosasihUniversalisBridge).transferTokens(_token, _from, _to, _amount);
+    }
+}
