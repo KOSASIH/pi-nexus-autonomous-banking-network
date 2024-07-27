f2f45e5b8daff4a3f1a18c302d6d554afd575273
KOSASIH <kosasihg88@gmail.com>
2024-07-27 13:21:53 +0700

    Update KosasihUniversalisGovernanceV2.sol

--- blockchain_integration/pi_network/pi-network-interoperability/bridge-contracts/kosasih-universalis/contracts/KosasihUniversalisGovernanceV2.sol
+++ blockchain_integration/pi_network/pi-network-interoperability/bridge-contracts/kosasih-universalis/contracts/KosasihUniversalisGovernanceV2.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/utils/Address.sol";
 import "./KosasihUniversalisUtils.sol";
 import "./KosasihUniversalisMath.sol";
+import "./KosasihUniversalisMath.sol";
 
 contract KosasihUniversalisGovernanceV2 {
     address public kosasihUniversalisNexus;
