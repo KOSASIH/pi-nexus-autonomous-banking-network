96d37501e3a55ed9bf460f3caa58bc59ed7d4e65
KOSASIH <kosasihg88@gmail.com>
2024-07-27 09:17:55 +0700

    Create index.js

---
+++ blockchain_integration/pi_network/pi-network-interoperability/bridge-contracts/nexus-smart-chain/index.js
@@ -0,0 +1,15 @@
+const NexusSmartChainBridgeFactory = require('./NexusSmartChainBridgeFactory');
+const NexusSmartChainRelayerFactory = require('./NexusSmartChainRelayerFactory');
+const NexusSmartChainTokenFactory = require('./NexusSmartChainTokenFactory');
+const NexusSmartChainBridge = require('./NexusSmartChainBridge');
+const NexusSmartChainRelayer = require('./NexusSmartChainRelayer');
+const NexusSmartChainToken = require('./NexusSmartChainToken');
+
+module.exports = {
+  NexusSmartChainBridgeFactory,
+  NexusSmartChainRelayerFactory,
+  NexusSmartChainTokenFactory,
+  NexusSmartChainBridge,
+  NexusSmartChainRelayer,
+  NexusSmartChainToken
+};
