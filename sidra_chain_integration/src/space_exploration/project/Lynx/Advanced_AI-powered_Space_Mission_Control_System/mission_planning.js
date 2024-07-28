baceccf28c2cca9d3a0c768ceb13e4b28a4a37a6
KOSASIH <kosasihg88@gmail.com>
2024-07-28 10:24:31 +0700

    Create mission_planning.js

---
+++ sidra_chain_integration/src/space_exploration/project/Lynx/Advanced_AI-powered_Space_Mission_Control_System/mission_planning.js
@@ -0,0 +1,15 @@
+import { MissionPlanning } from 'mission-planning-library';
+
+class MissionPlanning {
+  constructor() {
+    this.missionPlanning = new MissionPlanning();
+  }
+
+  planMission(missionParameters) {
+    // Use advanced algorithms to optimize mission parameters
+    const missionPlan = this.missionPlanning.planMission(missionParameters);
+    return missionPlan;
+  }
+}
+
+export default MissionPlanning;
