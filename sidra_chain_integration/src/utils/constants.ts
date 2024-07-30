b8abfb436746c9f8dcd9a9f8971fed42beb0d624
KOSASIH <kosasihg88@gmail.com>
2024-07-30 14:12:16 +0700

    Create constants.ts

---
+++ sidra_chain_integration/src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const SIDRA_CHAIN_CONTRACT_ADDRESS = '0x...SidraChainContractAddress...';
+export const TOKEN_CONTRACT_ADDRESS = '0x...TokenContractAddress...';
+export const PRIVATE_KEY = '0x...PrivateKey...';
+//...
