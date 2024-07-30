e4cccd6f06ee74ba49d990a456b78be7553bffd0
KOSASIH <kosasihg88@gmail.com>
2024-07-30 17:59:42 +0700

    Update eonix.py

--- coin/Eonix/src/eonix.py
+++ coin/Eonix/src/eonix.py
@@ -7,6 +7,9 @@ from database import EonixDatabase
 from ai import EonixAI
 from ml import EonixML
 from nlp import EonixNLP
+from qc import EonixQC
+from ar import EonixAR
+from agi import EonixAGI
 
 class Eonix:
     def __init__(self):
@@ -17,6 +20,9 @@ class Eonix:
         self.ai = EonixAI()
         self.ml = EonixML()
         self.nlp = EonixNLP()
+        self.qc = EonixQC()
+        self.ar = EonixAR()
+        self.agi = EonixAGI()
 
     def create_transaction(self, recipient, amount):
         transaction = Transaction(self.wallet.public_key, recipient, amount)
@@ -25,7 +31,7 @@ class Eonix:
     def mine_block(self):
         self.blockchain.mine_pending_transactions()
 
-    def get_balance(self):
+        def get_balance(self):
         balance = 0
         for block in self.blockchain.chain:
             for transaction in block.transactions:
@@ -56,3 +62,18 @@ class Eonix:
 
     def extract_entities(self, text):
         return self.nlp.extract_entities(text)
+
+    def simulate_quantum_computing(self, qubits, gates):
+        return self.qc.simulate_quantum_computing(qubits, gates)
+
+    def display_ar(self):
+        image = self.ar.capture_image()
+        markers = self.ar.detect_markers(image)
+        image = self.ar.track_markers(markers)
+        self.ar.display_ar(image)
+
+    def train_agi_model(self, data):
+        self.agi.train_model(data)
+
+    def evaluate_agi_model(self, data):
+        return self.agi.evaluate_model(data)
