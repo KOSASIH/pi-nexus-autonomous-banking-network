4eba989dbeb096e84247825391febe848b04ec54
KOSASIH <kosasihg88@gmail.com>
2024-07-30 14:21:25 +0700

    Update rabbitmq.ts

--- sidra_chain_integration/infrastructure/messaging/rabbitmq.ts
+++ sidra_chain_integration/infrastructure/messaging/rabbitmq.ts
@@ -2,9 +2,18 @@ import { RabbitMQ } from 'amqplib';
 
 const rabbitMQ = new RabbitMQ('amqp://localhost');
 
-export async function sendMessage(message: string) {
+export async function sendMessage(message: string, queue: string) {
   await rabbitMQ.connect();
-  await rabbitMQ.channel.assertQueue('sidra_chain_queue', { durable: true });
-  await rabbitMQ.channel.sendToQueue('sidra_chain_queue', Buffer.from(message));
+  await rabbitMQ.channel.assertQueue(queue, { durable: true });
+  await rabbitMQ.channel.sendToQueue(queue, Buffer.from(message));
+  await rabbitMQ.close();
+}
+
+export async function consumeMessage(queue: string, callback: (message: string) => void) {
+  await rabbitMQ.connect();
+  await rabbitMQ.channel.assertQueue(queue, { durable: true });
+  await rabbitMQ.channel.consume(queue, (message) => {
+    callback(message.content.toString());
+  });
   await rabbitMQ.close();
 }
