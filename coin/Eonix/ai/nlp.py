f91619df39349d31f77b5398d8c82f485f49e9f1
KOSASIH <kosasihg88@gmail.com>
2024-07-30 19:10:50 +0700

    Create nlp.py

---
+++ coin/Eonix/ai/nlp.py
@@ -0,0 +1,13 @@
+import nltk
+
+class EonixNLP:
+    def __init__(self):
+        self.tokenizer = nltk.word_tokenize
+
+    def analyze_sentiment(self, text):
+        # analyze sentiment of the given text
+        pass
+
+    def extract_entities(self, text):
+        # extract entities from the given text
+        pass
