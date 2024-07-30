66f31f7c5e03492754e3ea2726a63825c7be6f5d
KOSASIH <kosasihg88@gmail.com>
2024-07-30 14:08:37 +0700

    Create sidra_chain.sol

---
+++ sidra_chain_integration/contracts/sidra_chain.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+
+contract SidraChain {
+    //...
+    function transfer(address recipient, uint256 amount) public {
+        //...
+    }
+
+    function balanceOf(address owner) public view returns (uint256) {
+        //...
+    }
+
+    //...
+}
