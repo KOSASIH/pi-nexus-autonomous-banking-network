541159564fe0e63ef43039e49517bf8c62ce60db
KOSASIH <kosasihg88@gmail.com>
2024-07-30 19:10:16 +0700

    Create ml.py

---
+++ coin/Eonix/ai/ml.py
@@ -0,0 +1,13 @@
+import sklearn
+
+class EonixML:
+    def __init__(self):
+        self.model = sklearn.ensemble.RandomForestClassifier()
+
+    def train_model(self, data):
+        # train the ML model with the given data
+        pass
+
+    def predict(self, data):
+        # make predictions using the ML model
+        pass
