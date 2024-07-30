f638ad2f65f1069d01f143afaae88af1c55671d7
KOSASIH <kosasihg88@gmail.com>
2024-07-30 14:54:47 +0700

    Create sidra-dex.sol

---
+++ sidra_chain_integration/dex-project/dex/contracts/sidra-dex.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+import "./erc20-token.sol";
+
+contract SidraDEX {
+    // Sidra DEX implementation
+    // including functions for trading, liquidity provision, and more
+}
