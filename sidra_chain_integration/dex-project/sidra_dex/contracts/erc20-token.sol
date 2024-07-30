63094837515b0e76c6bdd512d4bd7984fba0b6b3
KOSASIH <kosasihg88@gmail.com>
2024-07-30 14:50:47 +0700

    Delete sidra_chain_integration/dex-project/sidra_dex/contracts directory

--- sidra_chain_integration/dex-project/sidra_dex/contracts/erc20-token.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.8.0;
-
-import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
-
-contract ERC20Token {
-    // ERC20 token implementation
-}
--- sidra_chain_integration/dex-project/sidra_dex/contracts/sidra-dex.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.0;
-
-import "./erc20-token.sol";
-
-contract SidraDEX {
-    // Sidra DEX implementation
-    // including functions for trading, liquidity provision, and more
-}
