26bdaa9664af32c9287ac8802f0022422c6e8594
KOSASIH <kosasihg88@gmail.com>
2024-08-01 14:36:17 +0700

    Create kyc_aml_manager.py

---
+++ blockchain_integration/pi_network/fiat_integration/advanced_features/kyc_aml_manager.py
@@ -0,0 +1,8 @@
+class KYCAMLManager:
+    def perform_kyc_check(self, user_id):
+        # implementation
+        pass
+
+    def perform_aml_check(self, transaction_id):
+        # implementation
+        pass
