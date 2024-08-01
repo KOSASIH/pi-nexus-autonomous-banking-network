4ae531da9ea77ac4504e1e7eebf0be4b4c419496
KOSASIH <kosasihg88@gmail.com>
2024-08-01 16:28:21 +0700

    Update constants.py

--- blockchain_integration/pi_network/fiat_integration/utils/constants.py
+++ blockchain_integration/pi_network/fiat_integration/utils/constants.py
@@ -1,1 +1,47 @@
-FIAT_CURRENCIES = ['USD', 'EUR', 'GBP', 'JPY', `IDR`, `VND`]
+# Fiat currencies
+FIAT_CURRENCIES = [
+    'USD',  # United States Dollar
+    'EUR',  # Euro
+    'GBP',  # British Pound
+    'JPY',  # Japanese Yen
+    'CHF',  # Swiss Franc
+    'CAD',  # Canadian Dollar
+    'AUD',  # Australian Dollar
+    'NZD',  # New Zealand Dollar
+    'CNY',  # Chinese Renminbi (RMB)
+    'RUB',  # Russian Ruble
+    'INR',  # Indian Rupee
+    'BRL',  # Brazilian Real
+    'ZAR',  # South African Rand
+    'MXN',  # Mexican Peso
+    'KRW',  # South Korean Won
+    'TRY',  # Turkish Lira
+    'SGD',  # Singapore Dollar
+    'HKD',  # Hong Kong Dollar
+    'SEK',  # Swedish Krona
+    'NOK',  # Norwegian Krone
+    'DKK',  # Danish Krone
+    'PLN',  # Polish Złoty
+    'CZK',  # Czech Koruna
+    'HUF',  # Hungarian Forint
+    'ILS',  # Israeli Shekel
+    'IDR',  # Indonesian Rupiah
+    'VND',  # Vietnamese Dong
+]
+
+# Cryptocurrencies
+CRYPTO_CURRENCIES = [
+    'BTC',  # Bitcoin
+    'ETH',  # Ethereum
+    'LTC',  # Litecoin
+    'BCH',  # Bitcoin Cash
+    'XRP',  # Ripple
+    'XLM',  # Stellar
+    'EOS',  # EOS
+    'BNB',  # Binance Coin
+    'USDT',  # Tether
+]
+
+# Other constants
+MIN_FIAT_TRANSACTION_AMOUNT = 10.0  # Minimum fiat transaction amount
+MAX_FIAT_TRANSACTION_AMOUNT = 10000.0  # Maximum fiat transaction amount
