542dd84b15bd1f558faa65459e91f17cbf428b05
KOSASIH <kosasihg88@gmail.com>
2024-08-01 10:52:43 +0700

    Update banking_platform.js

--- blockchain_integration/banking_platform.js
+++ blockchain_integration/banking_platform.js
@@ -4,33 +4,23 @@ const smartContract = require('./smart_contracts/smart_contract');
 
 class BankingPlatform {
   async createAccount(userIdentity) {
+    // Generate a new Ethereum wallet address
+    const walletAddress = await smartContract.generateWalletAddress();
+
     // Create a new account on the Pi Network
-    const account = await piNetwork.createAccount(userIdentity);
+    const account = await piNetwork.createAccount(userIdentity, walletAddress);
+
     // Create a new wallet on the Ethereum blockchain
-    const wallet = await smartContract.createWallet(account.address);
+    await smartContract.createWallet(walletAddress, account.address);
+
+    // Map the Pi Network account to the Ethereum wallet address
+    await smartContract.mapAccountToWallet(account.address, walletAddress);
+
     // Return the account and wallet information
-    return { account, wallet };
+    return { account, walletAddress };
   }
 
-  async deposit(amount, accountAddress) {
-    // Deposit funds into the account on the Pi Network
-    await piNetwork.deposit(amount, accountAddress);
-    // Update the account balance on the Ethereum blockchain
-    await smartContract.updateBalance(accountAddress, amount);
-  }
-
-  async withdraw(amount, accountAddress) {
-    // Withdraw funds from the account on the Pi Network
-    await piNetwork.withdraw(amount, accountAddress);
-    // Update the account balance on the Ethereum blockchain
-    await smartContract.updateBalance(accountAddress, -amount);
-  }
-
-  async getAccountBalance(accountAddress) {
-    // Get the account balance from the Ethereum blockchain
-    const balance = await smartContract.getBalance(accountAddress);
-    return balance;
-  }
+  // ... other methods ...
 }
 
 module.exports = BankingPlatform;
