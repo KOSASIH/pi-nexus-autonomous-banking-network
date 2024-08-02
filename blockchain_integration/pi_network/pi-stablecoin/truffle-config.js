1b4dfb5bf5d1cd41e0a1262e70e7f54dfd1c26a2
KOSASIH <kosasihg88@gmail.com>
2024-08-02 10:34:26 +0700

    Create truffle-config.js

---
+++ blockchain_integration/pi_network/pi-stablecoin/truffle-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0"
+    }
+  }
+};
