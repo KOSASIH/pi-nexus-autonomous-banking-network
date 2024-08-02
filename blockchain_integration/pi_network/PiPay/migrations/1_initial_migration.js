8a97340be9fcc9b2dd029fc1bc34d736c3e4a8a9
KOSASIH <kosasihg88@gmail.com>
2024-08-02 11:16:58 +0700

    Create 1_initial_migration.js

---
+++ blockchain_integration/pi_network/PiPay/migrations/1_initial_migration.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up: async (deployer) => {
+    // Initialize the migration
+    console.log("Running initial migration...");
+  },
+
+  down: async (deployer) => {
+    // Roll back the migration
+    console.log("Rolling back initial migration...");
+  }
+};
