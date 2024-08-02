ab1d5bf01258546832ba7b21ccd790b8e2b5b976
KOSASIH <kosasihg88@gmail.com>
2024-08-02 10:31:04 +0700

    Update Token.test.js

--- blockchain_integration/pi_network/pi-stablecoin/test/Token.test.js
+++ blockchain_integration/pi_network/pi-stablecoin/test/Token.test.js
@@ -12,5 +12,27 @@ contract('Token', () => {
         token = await Token.new();
     });
 
-    it('should mint tokens', async () => {
-        const
+        it('should mint tokens', async () => {
+        const initialBalance = await token.balanceOf('0x...AuthorizedAddress...');
+        await token.mint('0x...AuthorizedAddress...', 100);
+        const newBalance = await token.balanceOf('0x...AuthorizedAddress...');
+        expect(newBalance).to.be.equal(initialBalance.add(100));
+    });
+
+    it('should transfer tokens', async () => {
+        const initialBalanceFrom = await token.balanceOf('0x...AuthorizedAddress...');
+        const initialBalanceTo = await token.balanceOf('0x...RecipientAddress...');
+        await token.transfer('0x...AuthorizedAddress...', '0x...RecipientAddress...', 50);
+        const newBalanceFrom = await token.balanceOf('0x...AuthorizedAddress...');
+        const newBalanceTo = await token.balanceOf('0x...RecipientAddress...');
+        expect(newBalanceFrom).to.be.equal(initialBalanceFrom.sub(50));
+        expect(newBalanceTo).to.be.equal(initialBalanceTo.add(50));
+    });
+
+    it('should burn tokens', async () => {
+        const initialBalance = await token.balanceOf('0x...AuthorizedAddress...');
+        await token.burn(50);
+        const newBalance = await token.balanceOf('0x...AuthorizedAddress...');
+        expect(newBalance).to.be.equal(initialBalance.sub(50));
+    });
+});
