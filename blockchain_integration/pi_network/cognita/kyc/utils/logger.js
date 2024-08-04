032f4a9659098bc4248140901568c90a837d33a3
KOSASIH <kosasihg88@gmail.com>
2024-08-04 22:49:13 +0700

    Create logger.js

---
+++ blockchain_integration/pi_network/cognita/kyc/utils/logger.js
@@ -0,0 +1,21 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+    }),
+    new winston.transports.File({
+      filename: 'logs/info.log',
+      level: 'info',
+    }),
+  ],
+});
+
+module.exports = logger;
