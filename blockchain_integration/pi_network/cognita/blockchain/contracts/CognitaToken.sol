217c64de5291b55989c7f0a4ee238d7ec9349595
KOSASIH <kosasihg88@gmail.com>
2024-08-04 18:56:12 +0700

    Update CognitaToken.sol

--- blockchain_integration/pi_network/cognita/blockchain/contracts/CognitaToken.sol
+++ blockchain_integration/pi_network/cognita/blockchain/contracts/CognitaToken.sol
@@ -9,23 +9,50 @@ contract CognitaToken {
     // Mapping of token balances
     mapping (address => uint) public balances;
 
-    // Events
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
+    // Mapping of token allowances
+    mapping (address => mapping (address => uint)) public allowances;
 
-    // Constructor
+    // Event emitted when tokens are transferred
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    // Event emitted when tokens are burned
+    event Burn(address indexed from, uint value);
+
+    // Event emitted when dividends are distributed
+    event Dividend(address indexed from, uint value);
+
+    // Constructor function
     constructor() public {
         name = "Cognita Token";
         symbol = "COG";
         totalSupply = 100000000;
+        balances[msg.sender] = totalSupply;
     }
 
-    // Functions
+    // Function to transfer tokens
     function transfer(address to, uint value) public {
-        // Transfer logic
+        require(balances[msg.sender] >= value, "Insufficient balance");
+        balances[msg.sender] -= value;
+        balances[to] += value;
+        emit Transfer(msg.sender, to, value);
     }
 
-    function approve(address spender, uint value) public {
-        // Approval logic
+    // Function to burn tokens
+    function burn(uint value) public {
+        require(balances[msg.sender] >= value, "Insufficient balance");
+        balances[msg.sender] -= value;
+        totalSupply -= value;
+        emit Burn(msg.sender, value);
+    }
+
+    // Function to distribute dividends
+    function distributeDividend(uint value) public {
+        require(balances[msg.sender] >= value, "Insufficient balance");
+        balances[msg.sender] -= value;
+        // Distribute dividend to all token holders
+        for (address holder in balances) {
+            balances[holder] += value / totalSupply;
+        }
+        emit Dividend(msg.sender, value);
     }
 }
