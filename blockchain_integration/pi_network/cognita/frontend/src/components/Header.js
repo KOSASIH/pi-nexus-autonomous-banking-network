b0fd55ed2cf9328571fefec95503d41b8b60cc6f
KOSASIH <kosasihg88@gmail.com>
2024-08-04 18:45:33 +0700

    Create Header.js

---
+++ blockchain_integration/pi_network/cognita/frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
