592699d9aef389e2198e513b70737fc8b33e3397
KOSASIH <kosasihg88@gmail.com>
2024-08-04 22:53:22 +0700

    Update kyc.module.js

--- blockchain_integration/pi_network/cognita/kyc/kyc.module.js
+++ blockchain_integration/pi_network/cognita/kyc/kyc.module.js
@@ -1,10 +1,19 @@
-// kyc/kyc.module.js
-import { Module } from '@cognita/core';
-import KycController from './kyc.controller';
-import KycService from './kyc.service';
+import { Module } from '@nestjs/common';
+import { KycController } from './kyc.controller';
+import { KycService } from './kyc.service';
+import { KycModel } from './kyc.model';
+import { KycValidation } from './kyc.validation';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/passport-local';
+import { JwtStrategy } from './strategies/passport-jwt';
+import { PiNetworkStrategy } from './strategies/passport-pi-network';
 
 @Module({
+  imports: [
+    PassportModule,
+  ],
   controllers: [KycController],
-  providers: [KycService],
+  providers: [KycService, KycModel, KycValidation, LocalStrategy, JwtStrategy, PiNetworkStrategy],
+  exports: [KycService],
 })
 export class KycModule {}
