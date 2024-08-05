84a71f9484a4df9203ad3f7739134abb103e0b6f
KOSASIH <kosasihg88@gmail.com>
2024-08-05 17:33:45 +0700

    Create constants.js

---
+++ blockchain_integration/pi_network/PiLearn/utils/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+  COURSE_CREATED: 'Course created successfully',
+  COURSE_UPDATED: 'Course updated successfully',
+  COURSE_DELETED: 'Course deleted successfully',
+  COURSE_NOT_FOUND: 'Course not found',
+  INVALID_REQUEST: 'Invalid request'
+};
