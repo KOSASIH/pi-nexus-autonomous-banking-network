af1eb8ee0e866954c84f8208860742b86a1d5da4
KOSASIH <kosasihg88@gmail.com>
2024-08-05 21:44:34 +0700

    Create User.js

---
+++ blockchain_integration/pi_network/pifood/backend/models/User.js
@@ -0,0 +1,60 @@
+// User.js
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bcrypt = require('bcryptjs');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  googleId: {
+    type: String,
+  },
+  facebookId: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  const user = this;
+  return await bcrypt.compare(candidatePassword, user.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
