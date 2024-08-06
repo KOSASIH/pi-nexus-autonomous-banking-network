d122bd40df15c2347cc4d02f8a3860947ccba951
KOSASIH <kosasihg88@gmail.com>
2024-08-06 08:19:42 +0700

    Delete blockchain_integration/pi_network/PiHealth directory

--- blockchain_integration/pi_network/PiHealth/config/index.ts
+++
@@ -1,4 +0,0 @@
-import { config } from './environment.json';
-import { SMART_CONTRACT_ADDRESSES, DATA_FORMATS, API_ERROR_CODES, IOT_DEVICE_TYPES } from './constants';
-
-export { config, SMART_CONTRACT_ADDRESSES, DATA_FORMATS, API_ERROR_CODES, IOT_DEVICE_TYPES };
