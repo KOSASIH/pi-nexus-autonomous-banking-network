24f9658e01e3090cebf6a24b71b8acfff3e55b09
KOSASIH <kosasihg88@gmail.com>
2024-08-06 21:20:41 +0700

    Create index.js

---
+++ blockchain_integration/PiSure/client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppContainer from './containers/AppContainer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppContainer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
