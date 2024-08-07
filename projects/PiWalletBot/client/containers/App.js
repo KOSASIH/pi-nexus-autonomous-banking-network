b1f5bf22dbaa2c164931a2f300027ffb03260a8d
KOSASIH <kosasihg88@gmail.com>
2024-08-07 22:06:45 +0700

    Create App.js

---
+++ projects/PiWalletBot/client/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatWindow from '../components/ChatWindow';
+
+const App = () => {
+  return (
+    <div className="app">
+      <ChatWindow />
+    </div>
+  );
+};
+
+export default App;
