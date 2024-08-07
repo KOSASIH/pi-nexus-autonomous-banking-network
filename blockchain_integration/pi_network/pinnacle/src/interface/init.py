4cd5c14f4a891f813a082469b436b29c2e7fba55
KOSASIH <kosasihg88@gmail.com>
2024-08-07 10:39:44 +0700

    Create init.py

---
+++ blockchain_integration/pi_network/pinnacle/src/interface/init.py
@@ -0,0 +1,5 @@
+# Initialize the interfaces package
+from .liquidity_provider_interface import LiquidityProviderInterface
+from .cross_chain_bridge_interface import CrossChainBridgeInterface
+
+__all__ = ["LiquidityProviderInterface", "CrossChainBridgeInterface"]
