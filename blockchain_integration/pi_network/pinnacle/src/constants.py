da2157e6fb426009f79690d66220db70f27d31d6
KOSASIH <kosasihg88@gmail.com>
2024-08-07 09:48:40 +0700

    Create constants.py

---
+++ blockchain_integration/pi_network/pinnacle/src/constants.py
@@ -0,0 +1,22 @@
+class Constants:
+    # Blockchain constants
+    PI_NETWORK_CHAIN_ID = 1234
+    ETHEREUM_CHAIN_ID = 1
+    BSC_CHAIN_ID = 56
+
+    # Contract addresses
+    PI_NETWORK_TOKEN_ADDRESS = "0x..."
+    ETHEREUM_TOKEN_ADDRESS = "0x..."
+    BSC_TOKEN_ADDRESS = "0x..."
+
+    # API endpoints
+    PI_NETWORK_API_ENDPOINT = "https://api.pi.network/v1"
+    ETHEREUM_API_ENDPOINT = "https://api.ethereum.org/v1"
+    BSC_API_ENDPOINT = "https://api.bscscan.com/v1"
+
+    # Other constants
+    MAX_GAS_PRICE = 1000000000
+    MIN_GAS_PRICE = 1000000
+    GAS_PRICE_STEP = 1000000
+
+constants = Constants()
