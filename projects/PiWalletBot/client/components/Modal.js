4733877e8228819391a319e4bf1901ff0e81e89a
KOSASIH <kosasihg88@gmail.com>
2024-08-07 22:44:27 +0700

    Create Modal.js

---
+++ projects/PiWalletBot/client/components/Modal.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalContent = styled.div`
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+const Modal = ({ children, onClose }) => {
+  return (
+    <ModalContainer>
+      <ModalContent>
+        {children}
+        <Button onClick={onClose}>Close</Button>
+      </ModalContent>
+    </ModalContainer>
+  );
+};
+
+export default Modal;
