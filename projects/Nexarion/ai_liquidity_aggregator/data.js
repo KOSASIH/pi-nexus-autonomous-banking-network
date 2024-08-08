5702501a3fb889b1b742849bf4337b9481b7526e
KOSASIH <kosasihg88@gmail.com>
2024-08-08 09:38:57 +0700

    Create data.js

---
+++ projects/Nexarion/ai_liquidity_aggregator/data.js
@@ -0,0 +1,18 @@
+**data**
+* **exchanges.json**: A file containing a list of supported exchanges with their APIs and credentials.
+```json
+[
+  {
+    "name": "Binance",
+    "apiUrl": "https://api.binance.com/api/v3",
+    "apiKey": "YOUR_API_KEY",
+    "apiSecret": "YOUR_API_SECRET"
+  },
+  {
+    "name": "Kraken",
+    "apiUrl": "https://api.kraken.com/0/public",
+    "apiKey": "YOUR_API_KEY",
+    "apiSecret": "YOUR_API_SECRET"
+  },
+  ...
+]
