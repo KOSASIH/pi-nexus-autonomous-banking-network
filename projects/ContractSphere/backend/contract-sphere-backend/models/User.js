25d204362643a34529e752ad6cac700bc814cbbf
KOSASIH <kosasihg88@gmail.com>
2024-08-08 13:50:49 +0700

    Create User.js

---
+++ projects/ContractSphere/backend/contract-sphere-backend/models/User.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+export default mongoose.model('User', userSchema);
