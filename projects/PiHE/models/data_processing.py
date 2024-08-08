d4063bc81ccb1ce72f3f99b26817a7ddcf11cc3e
KOSASIH <kosasihg88@gmail.com>
2024-08-08 16:28:08 +0700

    Create data_processing.py

---
+++ projects/PiHE/models/data_processing.py
@@ -0,0 +1,10 @@
+# Data processing logic for PiHE
+from models.homomorphic_model import HomomorphicModel
+
+class DataProcessor:
+    def __init__(self, homomorphic_model):
+        self.homomorphic_model = homomorphic_model
+
+    def process_data(self, encrypted_data):
+        # Process encrypted data using homomorphic model for PiHE
+        pass
