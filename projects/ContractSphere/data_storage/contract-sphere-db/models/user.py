c95526a23c08841745fd9bacf43ddb88fe1a2057
KOSASIH <kosasihg88@gmail.com>
2024-08-08 14:13:53 +0700

    Create user.py

---
+++ projects/ContractSphere/data_storage/contract-sphere-db/models/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(255), nullable=False, unique=True)
+    email = Column(String(255), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    contracts = relationship('Contract', backref='user')
+
+    def __repr__(self):
+        return f'<User {self.username}>'
