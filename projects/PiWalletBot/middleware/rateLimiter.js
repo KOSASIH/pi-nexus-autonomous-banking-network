a8d5f8b0ef36bebb55a5bc8829252f60f7b35ecb
KOSASIH <kosasihg88@gmail.com>
2024-08-08 07:38:47 +0700

    Create rateLimiter.js

---
+++ projects/PiWalletBot/middleware/rateLimiter.js
@@ -0,0 +1,9 @@
+import { RateLimiter } from 'express-rate-limiter';
+
+const apiLimiter = new RateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  delayMs: 0, // disable delaying - full speed until the max limit is reached
+});
+
+export default apiLimiter;
