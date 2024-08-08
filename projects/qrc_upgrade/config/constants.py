a12fa42f309deba89b3b09224a047ed44b46de84
KOSASIH <kosasihg88@gmail.com>
2024-08-08 14:56:45 +0700

    Create constants.py

---
+++ projects/qrc_upgrade/config/constants.py
@@ -0,0 +1,4 @@
+# Constants for QRC Upgrade
+NTRU_N = 512  # NTRU parameter: degree of the polynomial ring
+NTRU_Q = 2048  # NTRU parameter: modulus
+NTRU_SIGMA = 3.2  # NTRU parameter: standard deviation for error distribution
