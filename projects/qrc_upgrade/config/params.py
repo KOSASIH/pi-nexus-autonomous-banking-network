ebb21210cde16b257c4bfba91d927bf327e64f94
KOSASIH <kosasihg88@gmail.com>
2024-08-08 14:57:34 +0700

    Create params.py

---
+++ projects/qrc_upgrade/config/params.py
@@ -0,0 +1,6 @@
+# QRC Upgrade parameters
+class QRCParams:
+    def __init__(self, ntru_n, ntru_q, ntru_sigma):
+        self.ntru_n = ntru_n
+        self.ntru_q = ntru_q
+        self.ntru_sigma = ntru_sigma
