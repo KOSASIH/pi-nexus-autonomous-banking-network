86240148246ac922fe7cc3f2c7d08fde3667c4f4
KOSASIH <kosasihg88@gmail.com>
2024-08-08 09:30:50 +0700

    Create util.js

---
+++ projects/Nexarion/cross_chain_bridge/utils/util.js
@@ -0,0 +1,8 @@
+**utils**
+* **constants.js**: A file containing constants used throughout the project.
+```javascript
+export const ETHEREUM_NETWORK_ID = 1;
+export const BINANCE_SMART_CHAIN_NETWORK_ID = 56;
+export const CROSS_CHAIN_BRIDGE_ADDRESS = '0x...CrossChainBridgeAddress...';
+export const ETHEREUM_BRIDGE_ADDRESS = '0x...EthereumBridgeAddress...';
+export const BINANCE_SMART_CHAIN_BRIDGE_ADDRESS = '0x...BinanceSmartChainBridgeAddress...';
