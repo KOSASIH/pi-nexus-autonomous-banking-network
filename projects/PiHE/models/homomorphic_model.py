5e74ef1745efe93caf5873b958aebafafc19bed2
KOSASIH <kosasihg88@gmail.com>
2024-08-08 16:27:28 +0700

    Create homomorphic_model.py

---
+++ projects/PiHE/models/homomorphic_model.py
@@ -0,0 +1,10 @@
+# Homomorphic model for data processing in PiHE
+from crypto.homomorphic import HomomorphicEncryption
+
+class HomomorphicModel:
+    def __init__(self, he):
+        self.he = he
+
+    def process_data(self, encrypted_data):
+        # Perform homomorphic operations on encrypted data for PiHE
+        pass
