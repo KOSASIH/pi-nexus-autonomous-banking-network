56bce0b49d0b8966b2f3865a01c4413172511406
KOSASIH <kosasihg88@gmail.com>
2024-08-10 15:27:56 +0700

    Create config.py

---
+++ blockchain_integration/pi_network/PiFusion/config/config.py
@@ -0,0 +1,18 @@
+# Configuration file for PiFusion
+
+# Blockchain integration settings
+BLOCKCHAIN_PLATFORM = 'Pi Network'
+BLOCKCHAIN_NODE_URL = 'https://node.pi.network'
+
+# AI and ML model settings
+AI_MODEL_TYPE = 'TensorFlow'
+AI_MODEL_PATH = 'models/tensorflow_model'
+
+# Data storage settings
+DATA_STORAGE_TYPE = 'Apache Cassandra'
+DATA_STORAGE_HOST = 'localhost'
+DATA_STORAGE_PORT = 9042
+
+# Cloud infrastructure settings
+CLOUD_PROVIDER = 'AWS'
+CLOUD_REGION = 'us-west-2'
