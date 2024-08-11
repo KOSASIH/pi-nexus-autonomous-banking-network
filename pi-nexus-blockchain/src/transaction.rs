3430172c5bc7599c1f549fa30cb828de4cc9469e
KOSASIH <kosasihg88@gmail.com>
2024-08-11 14:39:54 +0700

    Update transaction.rs

--- pi-nexus-blockchain/src/transaction.rs
+++ pi-nexus-blockchain/src/transaction.rs
@@ -1,65 +1,29 @@
-// transaction.rs
+// transaction.rs (new)
 
-use std::collections::HashSet;
-use std::hash::{Hash, Hasher};
+use crate::utils::{hash, verify_signature};
+use elliptic_curve::{KeyPair, PublicKey, Signature};
 
-// Transaction struct
 pub struct Transaction {
-    sender: String,
-    recipient: String,
-    amount: u64,
-    timestamp: u64,
-    hash: String,
+    pub sender: PublicKey,
+    pub recipient: PublicKey,
+    pub amount: u64,
+    pub signature: Signature,
 }
 
 impl Transaction {
-    pub fn new(sender: String, recipient: String, amount: u64) -> Self {
-        let timestamp = std::time::SystemTime::now()
-            .duration_since(std::time::UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-        let hash = Self::calculate_hash(sender.clone(), recipient.clone(), amount, timestamp);
+    pub fn new(sender: &KeyPair, recipient: &PublicKey, amount: u64) -> Self {
+        let data = format!("{}{}{}", sender.public_key, recipient, amount);
+        let signature = sender.sign(&data);
         Transaction {
-            sender,
-            recipient,
+            sender: sender.public_key.clone(),
+            recipient: recipient.clone(),
             amount,
-            timestamp,
-            hash,
+            signature,
         }
     }
 
-    fn calculate_hash(sender: String, recipient: String, amount: u64, timestamp: u64) -> String {
-        let mut hasher = sha3::Sha3_256::new();
-        hasher.write(sender.as_bytes());
-        hasher.write(recipient.as_bytes());
-        hasher.write(amount.to_le_bytes().as_ref());
-        hasher.write(timestamp.to_le_bytes().as_ref());
-        let hash = hasher.finish();
-        hex::encode(hash)
-    }
-
-    pub fn hash(&self) -> String {
-        self.hash.clone()
-    }
-}
-
-// TransactionPool struct
-pub struct TransactionPool {
-    transactions: HashSet<Transaction>,
-}
-
-impl TransactionPool {
-    pub fn new() -> Self {
-        TransactionPool {
-            transactions: HashSet::new(),
-        }
-    }
-
-    pub fn add_transaction(&mut self, transaction: Transaction) {
-        self.transactions.insert(transaction);
-    }
-
-    pub fn get_transactions(&self) -> Vec<Transaction> {
-        self.transactions.clone().into_iter().collect()
+    pub fn verify(&self) -> bool {
+        let data = format!("{}{}{}", self.sender, self.recipient, self.amount);
+        verify_signature(&self.sender, &data, &self.signature)
     }
 }
