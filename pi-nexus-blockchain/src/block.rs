962fd48e862192daf44fcaa946a04cae86045f7d
KOSASIH <kosasihg88@gmail.com>
2024-08-11 14:37:22 +0700

    Update block.rs

--- pi-nexus-blockchain/src/block.rs
+++ pi-nexus-blockchain/src/block.rs
@@ -1,68 +1,19 @@
-// block.rs
+// block.rs (updated)
 
-use std::collections::HashSet;
-use std::hash::{Hash, Hasher};
-
-use crate::transaction::Transaction;
-
-// BlockHeader struct
-pub struct BlockHeader {
-    index: u32,
-    previous_hash: String,
-    transactions: Vec<Transaction>,
-    timestamp: u64,
-    nonce: u64,
-    difficulty_target: u64,
-}
+// ...
 
 impl BlockHeader {
-    pub fn new(
-        index: u32,
-        previous_hash: String,
-        transactions: Vec<Transaction>,
-        difficulty_target: u64,
-    ) -> Self {
-        BlockHeader {
-            index,
-            previous_hash,
-            transactions,
-            timestamp: 0, // set timestamp later
-            nonce: 0, // set nonce later
-            difficulty_target,
+    // ...
+
+    pub fn mine(&mut self, difficulty_target: u64) {
+        let mut nonce = 0;
+        loop {
+            self.nonce = nonce;
+            let hash = self.hash();
+            if hash.starts_with(&format!("{:0>64}", difficulty_target)) {
+                break;
+            }
+            nonce += 1;
         }
-    }
-
-    pub fn hash(&self) -> String {
-        let mut hasher = sha3::Sha3_256::new();
-        hasher.write(self.index.to_le_bytes().as_ref());
-        hasher.write(self.previous_hash.as_bytes());
-        for transaction in &self.transactions {
-            hasher.write(transaction.hash().as_bytes());
-        }
-        hasher.write(self.timestamp.to_le_bytes().as_ref());
-        hasher.write(self.nonce.to_le_bytes().as_ref());
-        let hash = hasher.finish();
-        hex::encode(hash)
-    }
-}
-
-// Block struct
-pub struct Block {
-    header: BlockHeader,
-    transactions: Vec<Transaction>,
-}
-
-impl Block {
-    pub fn new(header: BlockHeader, transactions: Vec<Transaction>) -> Self {
-        Block { header, transactions }
-    }
-
-    pub fn new_genesis() -> Self {
-        let header = BlockHeader::new(0, "0".to_string(), vec![], 1000);
-        Block { header, transactions: vec![] }
-    }
-
-    pub fn hash(&self) -> String {
-        self.header.hash()
     }
 }
