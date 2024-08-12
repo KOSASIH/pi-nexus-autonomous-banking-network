d5b346398f13b1a793a5e2eae38e1ed33b9a906f
KOSASIH <kosasihg88@gmail.com>
2024-08-12 20:58:57 +0700

    Create config.js

---
+++ blockchain_integration/pi_network/PiRide/config.js
@@ -0,0 +1,16 @@
+export const MONGODB_URI = 'mongodb://localhost:27017/piride';
+export const MONGODB_DB_NAME = 'piride';
+
+export const JWT_SECRET_KEY = 'secret-key';
+export const JWT_EXPIRATION_TIME = '1h';
+
+export const API_URL = 'https://api.piride.io';
+export const API_VERSION = 'v1';
+
+export const BLOCKCHAIN_NETWORK = 'mainnet';
+export const BLOCKCHAIN_PROVIDER = 'infura';
+
+export const ETHEREUM_CONTRACT_ADDRESS = '0x...';
+export const ETHEREUM_CONTRACT_ABI = [...];
+
+export const IPFS_GATEWAY = 'https://ipfs.io/ipfs/';
