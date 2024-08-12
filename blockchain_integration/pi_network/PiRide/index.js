9f6e93a3dbaf5351c40092c492b3e389bfb276b1
KOSASIH <kosasihg88@gmail.com>
2024-08-12 20:30:19 +0700

    Create index.js

---
+++ blockchain_integration/pi_network/PiRide/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
