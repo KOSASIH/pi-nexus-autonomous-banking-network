b5e124588cfba5b5e0f38078af4e23f4ce7c8ac5
KOSASIH <kosasihg88@gmail.com>
2024-08-13 12:57:05 +0700

    Create package.js

---
+++ projects/pi-atlas/package.js
@@ -0,0 +1,28 @@
+{
+  "name": "pi-atlas",
+  "version": "1.0.0",
+  "description": "A Decentralized, AI-Powered, and Quantum-Resistant Network Cartography System",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js",
+    "test": "jest"
+  },
+  "keywords": [],
+  "author": "KOSASIH",
+  "license": "Apache 2.0",
+  "dependencies": {
+    "express": "^4.17.1",
+    "body-parser": "^1.19.0",
+    "mongoose": "^5.10.18",
+    "graphql": "^15.5.0",
+    "apollo-server-express": "^2.18.2",
+    "ai-lib": "^1.2.3",
+    "quantum-lib": "^0.5.1",
+    "network-cartography-lib": "^3.4.2"
+  },
+  "devDependencies": {
+    "jest": "^26.6.3",
+    "nodemon": "^2.0.7"
+  }
+}
