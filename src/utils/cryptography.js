83e2a5f733c489b369b374813e0cbd4e2509950f
KOSASIH <kosasihg88@gmail.com>
2024-08-16 14:11:21 +0700

    Create cryptography.js

---
+++ src/utils/cryptography.js
@@ -0,0 +1,19 @@
+const crypto = require('crypto');
+
+class Cryptography {
+    static encrypt(data, key) {
+        const cipher = crypto.createCipher('aes-256-cbc', key);
+        let encrypted = cipher.update(data, 'utf8', 'hex');
+        encrypted += cipher.final('hex');
+        return encrypted;
+    }
+
+    static decrypt(encrypted, key) {
+        const decipher = crypto.createDecipher('aes-256-cbc', key);
+        let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+        decrypted += decipher.final('utf8');
+        return decrypted;
+    }
+}
+
+module.exports = Cryptography;
