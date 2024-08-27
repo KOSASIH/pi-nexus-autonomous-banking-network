50ab1270ec1f6ce6436f32c06d631355f2ae7c40
KOSASIH <kosasihg88@gmail.com>
2024-08-27 18:35:53 +0700

    Create node app.js

---
+++ pi-nexus-api/node app.js
@@ -0,0 +1,5 @@
+This will start the API on port 3000. You can then use a tool like `curl` or a web browser to interact with the API.
+
+### Contributing
+
+Contributions are welcome! If you'd like to contribute to the Pi Nexus API, please fork this repository and submit a pull request.
