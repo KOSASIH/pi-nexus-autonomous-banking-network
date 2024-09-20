e0d38f7f7b733c901f1c39b29f2af4bfdbf2d368
KOSASIH <kosasihg88@gmail.com>
2024-09-20 17:13:02 +0700

    Update users_controller.rb

--- global_business/business/controllers/api/v1/users_controller.rb
+++ global_business/business/controllers/api/v1/users_controller.rb
@@ -1,44 +1,26 @@
-module Api
-  module V1
-    class UsersController < ApplicationController
-      before_action :authenticate_user!, except: [:create]
-      before_action :set_user, only: [:show, :update, :destroy]
+class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_user!
 
-      def create
-        @user = User.new(user_params)
-        if @user.save
-          render json: @user, status: :created
-        else
-          render json: { errors: @user.errors }, status: :unprocessable_entity
-        end
-      end
+  def show
+    render json: current_user, status: :ok
+  end
 
-      def show
-        render json: @user
-      end
-
-      def update
-        if @user.update(user_params)
-          render json: @user
-        else
-          render json: { errors: @user.errors }, status: :unprocessable_entity
-        end
-      end
-
-      def destroy
-        @user.destroy
-        render json: { message: "User deleted successfully" }, status: :ok
-      end
-
-      private
-
-      def user_params
-        params.require(:user).permit(:username, :email, :password, :first_name, :last_name, :role)
-      end
-
-      def set_user
-        @user = User.find(params[:id])
-      end
+  def update
+    if current_user.update(user_params)
+      render json: current_user, status: :ok
+    else
+      render json: { errors: current_user.errors }, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    current_user.destroy
+    render json: {}, status: :no_content
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
