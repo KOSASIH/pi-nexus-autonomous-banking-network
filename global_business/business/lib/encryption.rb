7fa9c6e387b3ae80a68758653fa72aead44f9127
KOSASIH <kosasihg88@gmail.com>
2024-09-20 17:05:04 +0700

    Update encryption.rb

--- global_business/business/lib/encryption.rb
+++ global_business/business/lib/encryption.rb
@@ -1,18 +1,22 @@
 require 'openssl'
+require 'base64'
 
 class Encryption
-  def self.encrypt(data)
-    cipher = OpenSSL::Cipher.new('AES-256-GCM')
+  def self.encrypt(data, key)
+    cipher = OpenSSL::Cipher.new('AES-256-CBC')
     cipher.encrypt
-    cipher.key = Rails.application.secrets.secret_key_base
+    cipher.key = key
+
     encrypted_data = cipher.update(data) + cipher.final
-    encrypted_data
+    Base64.encode64(encrypted_data)
   end
 
-  def self.decrypt(encrypted_data)
-    cipher = OpenSSL::Cipher.new('AES-256-GCM')
+  def self.decrypt(encrypted_data, key)
+    encrypted_data = Base64.decode64(encrypted_data)
+    cipher = OpenSSL::Cipher.new('AES-256-CBC')
     cipher.decrypt
-    cipher.key = Rails.application.secrets.secret_key_base
+    cipher.key = key
+
     decrypted_data = cipher.update(encrypted_data) + cipher.final
     decrypted_data
   end
