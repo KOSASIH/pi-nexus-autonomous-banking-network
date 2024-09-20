42e981f8a82eb3d236c2ccd0304ae8378f641c95
KOSASIH <kosasihg88@gmail.com>
2024-09-20 17:13:43 +0700

    Update accounts_controller.rb

--- global_business/business/controllers/api/v1/accounts_controller.rb
+++ global_business/business/controllers/api/v1/accounts_controller.rb
@@ -1,44 +1,43 @@
-module Api
-  module V1
-    class AccountsController < ApplicationController
-      before_action :authenticate_user!
-      before_action :set_account, only: [:show, :update, :destroy]
+class Api::V1::AccountsController < ApplicationController
+  before_action :authenticate_user!
 
-      def create
-        @account = Account.new(account_params)
-        if @account.save
-          render json: @account, status: :created
-        else
-          render json: { errors: @account.errors }, status: :unprocessable_entity
-        end
-      end
+  def index
+    accounts = current_user.accounts
+    render json: accounts, status: :ok
+  end
 
-      def show
-        render json: @account
-      end
+  def show
+    account = current_user.accounts.find(params[:id])
+    render json: account, status: :ok
+  end
 
-      def update
-        if @account.update(account_params)
-          render json: @account
-        else
-          render json: { errors: @account.errors }, status: :unprocessable_entity
-        end
-      end
-
-      def destroy
-        @account.destroy
-        render json: { message: "Account deleted successfully" }, status: :ok
-      end
-
-      private
-
-      def account_params
-        params.require(:account).permit(:account_number, :routing_number, :account_type, :user_id)
-      end
-
-      def set_account
-        @account = Account.find(params[:id])
-      end
+  def create
+    account = current_user.accounts.new(account_params)
+    if account.save
+      render json: account, status: :created
+    else
+      render json: { errors: account.errors }, status: :unprocessable_entity
     end
+  end
+
+  def update
+    account = current_user.accounts.find(params[:id])
+    if account.update(account_params)
+      render json: account, status: :ok
+    else
+      render json: { errors: account.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    account = current_user.accounts.find(params[:id])
+    account.destroy
+    render json: {}, status: :no_content
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:balance)
   end
 end
