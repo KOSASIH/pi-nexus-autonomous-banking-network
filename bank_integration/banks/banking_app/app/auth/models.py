e3250477e6e37aeea657d1ae3e93d5831bfd3f6e
KOSASIH <kosasihg88@gmail.com>
2024-09-22 14:10:51 +0700

    Create models.py

---
+++ bank_integration/banks/banking_app/app/auth/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
