43414addb1c516ffbaa0eaf0e53aaa5da19bd19f
KOSASIH <kosasihg88@gmail.com>
2024-09-22 14:12:16 +0700

    Delete bank_integration/banks/banking_app/app/auth/auth directory

--- bank_integration/banks/banking_app/app/auth/auth/schemas.py
+++
@@ -1,9 +0,0 @@
-from marshmallow import Schema, fields
-
-class UserSchema(Schema):
-    id = fields.Int(dump_only=True)
-    username = fields.Str(required=True)
-    password = fields.Str(load_only=True)
-
-    class Meta:
-        fields = ("id", "username", "password")
