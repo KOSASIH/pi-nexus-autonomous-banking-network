dc381715cc544752d1c50fcd0e994a0fb260c7f7
KOSASIH <kosasihg88@gmail.com>
2024-09-22 14:12:58 +0700

    Create schemas.py

---
+++ bank_integration/banks/banking_app/app/auth/schemas.py
@@ -0,0 +1,9 @@
+from marshmallow import Schema, fields
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(load_only=True)
+
+    class Meta:
+        fields = ("id", "username", "password")
