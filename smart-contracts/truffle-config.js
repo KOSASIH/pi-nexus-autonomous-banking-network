ffd2a321d7857c5801a4e7a983f132cf917a57b5
KOSASIH <kosasihg88@gmail.com>
2024-11-20 20:12:35 +0700

    Create truffle-config.js

---
+++ smart-contracts/truffle-config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { infuraKey, mnemonic } = require("./secrets.json");
+
+module.exports = {
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 4000000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${infuraKey}`),
+      network_id: 1,       // Mainnet's id
+      gas: 4000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {          // See solc-js docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+      }
+    }
+  }
+};
