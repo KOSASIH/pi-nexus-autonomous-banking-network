918777e52a8e6ebe1dbd11549f406c37f7412918
KOSASIH <kosasihg88@gmail.com>
2024-11-20 19:12:38 +0700

    Delete src/components directory

--- src/components/AIEngine/inference.js
+++
@@ -1,5 +0,0 @@
-export const makePrediction = async (model, input) => {
-    const inputTensor = tf.tensor2d([input]);
-    const prediction = model.predict(inputTensor);
-    return prediction.arraySync();
-};
--- src/components/AIEngine/utils.js
+++
@@ -1,7 +0,0 @@
-export const preprocessData = (data) => {
-    // Implement data preprocessing logic here
-    return data.map(item => ({
-        input: item.input,
-        label: item.label
-    }));
-};
