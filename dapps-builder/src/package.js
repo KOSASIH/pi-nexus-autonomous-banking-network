a32b34ad880bcbbe7a9a7b2d05421de9a32ed067
KOSASIH <kosasihg88@gmail.com>
2024-11-20 19:43:47 +0700

    Create package.js

---
+++ dapps-builder/src/package.js
@@ -0,0 +1,44 @@
+{
+  "name": "dapps-builder",
+  "version": "1.0.0",
+  "description": "A powerful dApp builder leveraging blockchain technology and AI.",
+  "main": "index.js",
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject",
+    "lint": "eslint src/**/*.{js,jsx}",
+    "format": "prettier --write src/**/*.{js,jsx,json,css,md}"
+  },
+  "dependencies": {
+    "react": "^18.0.0",
+    "react-dom": "^18.0.0",
+    "react-scripts": "5.0.0",
+    "axios": "^0.21.1",
+    "web3": "^1.6.0",
+    "dotenv": "^10.0.0",
+    "redux": "^4.1.0",
+    "redux-thunk": "^2.4.1",
+    "react-redux": "^8.0.0",
+    "ethers": "^5.4.0"
+  },
+  "devDependencies": {
+    "eslint": "^7.32.0",
+    "prettier": "^2.5.1"
+  },
+  "keywords": [
+    "dApp",
+    "Blockchain",
+    "Web3",
+    "AI",
+    "Decentralized Application"
+  ],
+  "author": "KOSASIH",
+  "license": "MIT",
+  "homepage": "https://github.com/KOSASIH/pi-nexus-autonomous-banking-network/dapps-builder#readme",
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/KOSASIH/pi-nexus-autonomous-banking-network/dapps-builder.git"
+  }
+}
