59c1764b09d1ea68f52704ff195fa05b1c91d941
KOSASIH <kosasihg88@gmail.com>
2024-12-01 20:22:28 +0700

    Create notification.py

---
+++ src/payment/notification.py
@@ -0,0 +1,4 @@
+class Notification:
+    def send_notification(self,recipient, message):
+        # Here you can implement the logic to send notifications via email, SMS, or any other method
+        print(f"Notification sent to {recipient}: {message}")
