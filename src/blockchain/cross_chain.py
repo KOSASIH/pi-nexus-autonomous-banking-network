e5ff987c82d21625e02254a756a1ce1494aee95f
KOSASIH <kosasihg88@gmail.com>
2024-12-01 17:39:44 +0700

    Create cross_chain.py

---
+++ src/blockchain/cross_chain.py
@@ -0,0 +1,27 @@
+import requests
+
+class CrossChain:
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
+
+    def send_transaction(self, recipient, amount):
+        # Send a transaction to another blockchain
+        transaction_data = {
+            'sender': str(uuid4()),  # Simulate sender address
+            'recipient': recipient,
+            'amount': amount,
+        }
+        response = requests.post('http://other-blockchain-url/transactions/new', json=transaction_data)
+        return response.json()
+
+    def fetch_chain(self, node):
+        response = requests.get(f'http://{node}/chain')
+        if response.status_code == 200:
+            return response.json()
+        return None
+
+    def synchronize_chains(self):
+        for node in self.blockchain.nodes:
+            chain = self.fetch_chain(node)
+            if chain and self.blockchain.valid_chain(chain):
+                self.blockchain.chain = chain
