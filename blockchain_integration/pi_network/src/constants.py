79414576fbbafc8801e92c3a6b122c2508c04b6b
KOSASIH <kosasihg88@gmail.com>
2025-01-04 14:54:05 +0700

    Create constants.py

---
+++ blockchain_integration/pi_network/src/constants.py
@@ -0,0 +1,20 @@
+# src/constants.py
+
+"""
+Pi Coin Configuration Constants
+This module contains constants related to the Pi Coin cryptocurrency.
+"""
+
+# Pi Coin Symbol
+PI_COIN_SYMBOL = "Pi"  # Symbol for Pi Coin
+
+# Pi Coin Value
+PI_COIN_VALUE = 314159  # Fixed value of Pi Coin in USD
+
+# Pi Coin Supply
+PI_COIN_SUPPLY = 100_000_000_000  # Total supply of Pi Coin
+
+# Additional constants can be added here as needed
+# Example:
+# PI_COIN_TRANSACTION_FEE = 0.01  # Transaction fee in USD
+# PI_COIN_BLOCK_TIME = 10  # Average block time in seconds
