9cbc87cf9653b20aea45d02854725f7da515663d
KOSASIH <kosasihg88@gmail.com>
2025-02-10 21:57:24 +0700

    Create swagger.js

---
+++ src/api/swagger.js
@@ -0,0 +1,25 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Pi Nexus API',
+            version: '1.0.0',
+            description: 'API documentation for the Pi Nexus Autonomous Banking Network',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['./src/api/routes/*.js'], // Path to the API docs
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+module.exports = (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+};
