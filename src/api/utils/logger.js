f1da1022c5dd4f97dbf1b611bab54d41ac529310
KOSASIH <kosasihg88@gmail.com>
2025-02-10 23:46:45 +0700

    Update logger.js

--- src/api/utils/logger.js
+++ src/api/utils/logger.js
@@ -2,7 +2,10 @@ const winston = require('winston');
 
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
     transports: [
         new winston.transports.Console(),
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
