2a8d76f40d6be9842a2ed28c05388fa49bbcf423
KOSASIH <kosasihg88@gmail.com>
2025-02-10 23:42:08 +0700

    Update authMiddleware.js

--- src/api/middleware/authMiddleware.js
+++ src/api/middleware/authMiddleware.js
@@ -2,18 +2,17 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config/serverConfig');
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1];
+    const token = req.header('Authorization')?.replace('Bearer ', '');
     if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({ success: false, message: 'Access denied' });
     }
-
-    jwt.verify(token, JWT_SECRET, (err, user) => {
-        if (err) {
-            return res.status(403).json({ message: 'Forbidden' });
-        }
-        req.user = user;
+    try {
+        const verified = jwt.verify(token, JWT_SECRET);
+        req.user = verified;
         next();
-    });
+    } catch (error) {
+        res.status(400).json({ success: false, message: 'Invalid token' });
+    }
 };
 
 module.exports = authMiddleware;
