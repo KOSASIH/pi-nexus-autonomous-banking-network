a6096cded323bb48bdc482d15f6e0f741d175ce9
KOSASIH <kosasihg88@gmail.com>
2025-02-10 21:32:19 +0700

    Create logger.js

---
+++ src/api/utils/logger.js
@@ -0,0 +1,13 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
+
+module.exports = logger;
