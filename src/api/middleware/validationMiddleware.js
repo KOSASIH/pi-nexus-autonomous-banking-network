c20eac2516d3a6d3d7f5612550f5fd9793f1a13c
KOSASIH <kosasihg88@gmail.com>
2025-02-10 21:54:53 +0700

    Create validationMiddleware.js

---
+++ src/api/middleware/validationMiddleware.js
@@ -0,0 +1,17 @@
+const Joi = require('joi');
+
+const userSchema = Joi.object({
+    username: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+});
+
+const validateUser = (req, res, next) => {
+    const { error } = userSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ success: false, message: error.details[0].message });
+    }
+    next();
+};
+
+module.exports = validateUser;
