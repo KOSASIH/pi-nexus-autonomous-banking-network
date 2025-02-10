a25a7744eef5f9608b729400e24effd82ecaa94f
KOSASIH <kosasihg88@gmail.com>
2025-02-10 23:09:31 +0700

    Update serverConfig.js

--- src/api/config/serverConfig.js
+++ src/api/config/serverConfig.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
-const JWT_SECRET = process.env.JWT_SECRET;
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
 module.exports = {
     PORT,
     MONGO_URI,
-    JWT_SECRET,
+    TELEGRAM_BOT_TOKEN,
 };
