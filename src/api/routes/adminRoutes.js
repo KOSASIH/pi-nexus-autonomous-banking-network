747ad117fc3bb3c298dc85b7ecbf240484b1a8b7
KOSASIH <kosasihg88@gmail.com>
2025-02-11 02:06:39 +0700

    Update adminRoutes.js

--- src/api/routes/adminRoutes.js
+++ src/api/routes/adminRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { getAllUsers, getAllTransactions } = require('../services/adminService');
 const authMiddleware = require('../middleware/authMiddleware');
+const authorizationMiddleware = require('../middleware/authorizationMiddleware');
 
 const router = express.Router();
 
 // Route to get all users
-router.get('/users', authMiddleware, getAllUsers);
+router.get('/users', authMiddleware, authorizationMiddleware(['admin']), getAllUsers);
 
 // Route to get all transactions
-router.get('/transactions', authMiddleware, getAllTransactions);
+router.get('/transactions', authMiddleware, authorizationMiddleware(['admin']), getAllTransactions);
 
 module.exports = router;
