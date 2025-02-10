7ab798b5ce1edcf25cb70393b72f8fd8b76b30cf
KOSASIH <kosasihg88@gmail.com>
2025-02-10 23:05:30 +0700

    Update validationMiddleware.js

--- src/api/middleware/validationMiddleware.js
+++ src/api/middleware/validationMiddleware.js
@@ -6,8 +6,8 @@ const userSchema = Joi.object({
     password: Joi.string().min(6).required(),
 });
 
-const validateUser = (req, res, next) => {
-    const { error } = userSchema.validate(req.body);
+const validateUser  = (req, res, next) => {
+    const { error} = userSchema.validate(req.body);
     if (error) {
         return res.status(400).json({ success: false, message: error.details[0].message });
     }
