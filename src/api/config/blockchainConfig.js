0903b4b08b0420e9b1069adc52f0a91f565085e3
KOSASIH <kosasihg88@gmail.com>
2025-02-10 21:45:39 +0700

    Create blockchainConfig.js

---
+++ src/api/config/blockchainConfig.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const CONTRACT_ABI = [
+    // Add your smart contract ABI here
+];
+
+const CONTRACT_BYTECODE = '0x...'; // Add your smart contract bytecode here
+
+module.exports = {
+    CONTRACT_ABI,
+    CONTRACT_BYTECODE,
+};
