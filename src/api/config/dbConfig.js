6a9251619702f062e042a499ad67b01a94772d3e
KOSASIH <kosasihg88@gmail.com>
2025-02-10 21:30:44 +0700

    Create dbConfig.js

---
+++ src/api/config/dbConfig.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./serverConfig');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error);
+        process.exit(1);
+    }
+};
+
+module.exports = { connectDB };
