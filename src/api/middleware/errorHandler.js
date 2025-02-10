6e75021b4f608573c4592770606bcde1a146529e
KOSASIH <kosasihg88@gmail.com>
2025-02-10 21:20:50 +0700

    Create errorHandler.js

---
+++ src/api/middleware/errorHandler.js
@@ -0,0 +1,9 @@
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({
+        success: false,
+        message: err.message || 'Internal Server Error',
+    });
+};
+
+module.exports = errorHandler;
