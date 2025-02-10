67a0d47006ddd2dcaba311aa2084c4de28fabe60
KOSASIH <kosasihg88@gmail.com>
2025-02-10 21:21:41 +0700

    Create authMiddleware.js

---
+++ src/api/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config/serverConfig');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Forbidden' });
+        }
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authMiddleware;
