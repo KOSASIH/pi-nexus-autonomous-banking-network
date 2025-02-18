fdd9f97365c3b307440d8d1201c9dcf628cd9370
KOSASIH <kosasihg88@gmail.com>
2025-02-18 21:10:22 +0700

    Create complianceService.ts

---
+++ src/compliance/complianceService.ts
@@ -0,0 +1,5 @@
+// src/compliance/complianceService.ts
+export const checkCompliance = (transaction: any) => {
+    // Logic to check if the transaction complies with regulations
+    return true; // or false based on checks
+};
