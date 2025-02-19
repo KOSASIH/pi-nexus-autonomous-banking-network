526b3124dd977d1ed54fa8d2d910e5b9b6b7fc66
KOSASIH <kosasihg88@gmail.com>
2025-02-19 10:53:50 +0700

    Create selfHealing.js

---
+++ src/utils/selfHealing.js
@@ -0,0 +1,7 @@
+const selfHeal = () => {
+    // Logic to restart services or clear caches
+    console.log('Attempting to self-heal the system...');
+    // Example: Restart a service or clear a cache
+};
+
+export default selfHeal;
