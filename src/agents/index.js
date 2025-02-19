eef45bc146e50de0c59fe534ea5634cbd675a9b7
KOSASIH <kosasihg88@gmail.com>
2025-02-19 08:16:45 +0700

    Update index.js

--- src/agents/index.js
+++ src/agents/index.js
@@ -2,6 +2,9 @@ import TransactionAgent from './TransactionAgent';
 import IdentityAgent from './IdentityAgent';
 import NotificationAgent from './NotificationAgent';
 import ComplianceAgent from './ComplianceAgent';
+import RiskAssessmentAgent from './RiskAssessmentAgent';
+import AnalyticsAgent from './AnalyticsAgent';
+import CustomerSupportAgent from './CustomerSupportAgent';
 import { exec } from 'child_process';
 
 // Initialize JavaScript agents
@@ -9,6 +12,9 @@ const transactionAgent = new TransactionAgent();
 const identityAgent = new IdentityAgent();
 const notificationAgent = new NotificationAgent();
 const complianceAgent = new ComplianceAgent();
+const riskAssessmentAgent = new RiskAssessmentAgent();
+const analyticsAgent = new AnalyticsAgent();
+const customerSupportAgent = new CustomerSupportAgent();
 
 // Function to run Python scripts
 const runPythonScript = (scriptName, args = []) => {
@@ -48,4 +54,7 @@ export {
     identityAgent,
     notificationAgent,
     complianceAgent,
+    riskAssessmentAgent,
+    analyticsAgent,
+    customerSupportAgent,
 };
