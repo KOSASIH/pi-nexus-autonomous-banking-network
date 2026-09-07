a9eb2fdf039fd711ca54d84d272837437d47e8c7
KOSASIH <kosasihg88@gmail.com>
2026-09-08 00:15:01 +0700

    Upgrade Pi-Nexus Autonomous Banking Network: secure cryptography, robust Solidity smart contracts, and modern dependency manifests.

--- security.py
+++ security.py
@@ -1,30 +1,51 @@
 import json
-import os
-
-from cryptography.hazmat.primitives import serialization
-from eth_sig_util import to_checksum_address
-from web3 import Web3
-
-
-def secure_send_transaction(web3, private_key, to_address, value):
-    # Securely send a transaction using Web3.py
-    tx_count = web3.eth.get_transaction_count(to_checksum_address(private_key))
-    tx = {
-        "from": to_checksum_address(private_key),
-        "to": to_address,
-        "value": value,
-        "gas": 20000,
-        "gasPrice": web3.eth.gas_price,
-        "nonce": tx_count,
-    }
-    signed_tx = web3.eth.account.sign_transaction(tx, private_key)
-    web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-
-
-def secure_generate_keypair():
-    # Securely generate a keypair using cryptography
-    private_key = serialization.generate_private_key(
-        algorithm=serialization.RSA(), key_size=2048
-    )
-    public_key = private_key.public_key()
-    return private_key, public_key
+    import os
+    from cryptography.hazmat.primitives.asymmetric import rsa
+    from cryptography.hazmat.primitives import serialization
+    from web3 import Web3
+    
+    def secure_send_transaction(web3_provider, private_key_hex, to_address, value_wei):
+        """
+        Securely sign and send a transaction with dynamic gas estimation and nonce tracking.
+        """
+        account = web3_provider.eth.account.from_key(private_key_hex)
+        nonce = web3_provider.eth.get_transaction_count(account.address)
+        
+        gas_estimate = web3_provider.eth.estimate_gas({
+            'from': account.address,
+            'to': to_address,
+            'value': value_wei
+        })
+        
+        tx = {
+            'chainId': web3_provider.eth.chain_id,
+            'from': account.address,
+            'to': to_address,
+            'value': value_wei,
+            'gas': gas_estimate,
+            'maxFeePerGas': web3_provider.to_wei('50', 'gwei'),
+            'maxPriorityFeePerGas': web3_provider.to_wei('2', 'gwei'),
+            'nonce': nonce,
+            'type': '0x2'
+        }
+        
+        signed_tx = web3_provider.eth.account.sign_transaction(tx, private_key_hex)
+        tx_hash = web3_provider.eth.send_raw_transaction(signed_tx.rawTransaction)
+        return web3_provider.to_hex(tx_hash)
+    
+    def secure_generate_keypair():
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=4096
+        )
+        private_pem = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+        public_pem = private_key.public_key().public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        return private_pem, public_pem
+    
